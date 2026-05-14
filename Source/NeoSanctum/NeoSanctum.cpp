6ebb82d999709ed6447677524bf7cffa05358da1
ChoiJunHyeok <optimal9744@gmail.com>
2026-05-14 11:07:45 +0900

    init: 프로젝트 생성

---
+++ Source/NeoSanctum/NeoSanctum.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NeoSanctum.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NeoSanctum, "NeoSanctum" );
