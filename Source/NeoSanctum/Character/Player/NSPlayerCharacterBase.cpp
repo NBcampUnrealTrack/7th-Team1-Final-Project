a60802c31d9f91f97c4edec76f5edad210562d69
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-15 21:51:44 +0900

    add: Player Character Base 추가
    카메라만 추가해둔 스켈레톤 코드

---
+++ Source/NeoSanctum/Character/Player/NSPlayerCharacterBase.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSPlayerCharacterBase.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ANSPlayerCharacterBase::ANSPlayerCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+void ANSPlayerCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ANSPlayerCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ANSPlayerCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/NeoSanctum/Character/Player/NSPlayerCharacterBase.h
@@ -0,0 +1,30 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "NSPlayerCharacterBase.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class NEOSANCTUM_API ANSPlayerCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ANSPlayerCharacterBase();
+	
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player")
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player")
+	TObjectPtr<UCameraComponent> CameraComp;
+};
