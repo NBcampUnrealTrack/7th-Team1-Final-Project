96e5800d3740ef84b146f784aab068ec84fc8318
ChoiJunHyeok <optimal9744@gmail.com>
2026-05-15 18:56:28 +0900

    chore: GitStatusBranch 추가

---
+++ Source/GitStatusBranch/UPEdEngine.h
@@ -0,0 +1,18 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Editor/UnrealEdEngine.h"
+#include "UPEdEngine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITSTATUSBRANCH_API UUPEdEngine : public UUnrealEdEngine
+{
+	GENERATED_BODY()
+	
+	virtual void Init(IEngineLoop* InEngineLoop) override;
+};
--- Source/NeoSanctum/NeoSanctum.Build.cs
+++ Source/NeoSanctum/NeoSanctum.Build.cs
@@ -47,8 +47,6 @@ public class NeoSanctum : ModuleRules
 			"PhysicsCore"
 		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
