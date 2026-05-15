602b5f965a3fca6a357b04d432ccb50b809e1ea5
Park WonJong <kidsmon@naver.com>
2026-05-15 20:35:18 +0900

    add(gas): NSGameplayTags_Input 작성

---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Input.cpp
@@ -0,0 +1,13 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#include "NSGameplayTags_Input.h"
+
+namespace NSGameplayTags
+{
+	// Input
+	UE_DEFINE_GAMEPLAY_TAG(Input_BaseAttack, "Input.BaseAttack");
+	UE_DEFINE_GAMEPLAY_TAG(Input_ActiveSkill1, "Input.ActiveSkill1");
+	UE_DEFINE_GAMEPLAY_TAG(Input_ActiveSkill2, "Input.ActiveSkill2");
+	UE_DEFINE_GAMEPLAY_TAG(Input_ActiveSkill3, "Input.ActiveSkill3");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Dodge, "Input.Dodge");
+}
---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Input.h
@@ -0,0 +1,15 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace NSGameplayTags
+{
+	// Input
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_BaseAttack);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_ActiveSkill1);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_ActiveSkill2);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_ActiveSkill3);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Dodge);
+}
