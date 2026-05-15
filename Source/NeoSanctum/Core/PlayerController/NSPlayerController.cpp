51fd3c1169492a3ae444b0391b6347f5ebd47a55
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-15 21:50:54 +0900

    add: NSPlayerController 추가
    기본적인 이동 및 카메라 무브에 대한 구현만 해둠
    NativeAction으로 분류하여 태그에 따라 바인딩하는 코드를 추후에 추가할 예정

---
+++ Source/NeoSanctum/Core/PlayerController/NSPlayerController.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSPlayerController.h"
+
+#include "NeoSanctum/Input/NSInputComponent.h"
+
+ANSPlayerController::ANSPlayerController()
+{
+}
+
+void ANSPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ANSPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UNSInputComponent* NSInputComponent = CastChecked<UNSInputComponent>(InputComponent);
+	if (!IsValid(NSInputComponent)) return;
+	
+	if (InputConfig)
+	{
+		// NativeAction 바인딩하는 부분
+	}
+}
+
+void ANSPlayerController::Input_Move(const FInputActionValue& Value)
+{
+	APawn* ControlledPawn = GetPawn();
+	if (!ControlledPawn) return;
+	
+	const FVector2D MoveValue = Value.Get<FVector2D>();
+	const FRotator MoveRotation(0.f, GetControlRotation().Yaw, 0.f);
+	
+	const FVector ForwardDirection = MoveRotation.RotateVector(FVector::ForwardVector);
+	const FVector RightDirection = MoveRotation.RotateVector(FVector::RightVector);
+	
+	ControlledPawn->AddMovementInput(ForwardDirection, MoveValue.Y);
+	ControlledPawn->AddMovementInput(RightDirection, MoveValue.X);
+}
+
+void ANSPlayerController::Input_Look(const FInputActionValue& Value)
+{
+	const FVector2D LookValue = Value.Get<FVector2D>();
+	
+	AddYawInput(LookValue.X);
+	AddPitchInput(LookValue.Y);
+}
+
+void ANSPlayerController::Input_AbilityPressed(FGameplayTag InputTag)
+{
+	
+}
+
+void ANSPlayerController::Input_AbilityReleased(FGameplayTag InputTag)
+{
+	
+}
---
+++ Source/NeoSanctum/Core/PlayerController/NSPlayerController.h
@@ -0,0 +1,43 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/PlayerController.h"
+#include "NSPlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputMappingContext;
+class UNSInputConfig;
+
+UCLASS()
+class NEOSANCTUM_API ANSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ANSPlayerController();
+	
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+protected:
+	void Input_Move(const FInputActionValue& Value);
+	
+	void Input_Look(const FInputActionValue& Value);
+	
+	void Input_AbilityPressed(FGameplayTag InputTag);
+	
+	void Input_AbilityReleased(FGameplayTag InputTag);
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UNSInputConfig> InputConfig;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultIMC;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UInputMappingContext> CurrentIMC;
+};
