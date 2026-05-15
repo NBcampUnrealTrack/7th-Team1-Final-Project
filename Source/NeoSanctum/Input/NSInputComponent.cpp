7f9e833e651ad653206e8cd6c06e43a8987e6a15
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-15 21:48:45 +0900

    add: InputComponent 추가
    EnhancedInputComponent를 대체하는 커스텀 InputComponent
    - InputConfig와 IMC를 소유
    - Input 태그에 따라서 액션을 동적으로 바인딩해서 실행

---
+++ Source/NeoSanctum/Input/NSInputComponent.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSInputComponent.h"
+
+UNSInputComponent::UNSInputComponent(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+void UNSInputComponent::AddInputMapping(const UNSInputConfig* InputConfig,
+	UEnhancedInputLocalPlayerSubsystem* InputSubSystem)
+{
+	check(InputConfig);
+	check(InputSubSystem);
+}
+
+void UNSInputComponent::RemoveInputMapping(const UNSInputConfig* InputConfig,
+	UEnhancedInputLocalPlayerSubsystem* InputSubSystem) const
+{
+	check(InputConfig);
+	check(InputSubSystem);
+}
+
+void UNSInputComponent::RemoveBinds(TArray<uint32>& BindHandles)
+{
+	for (uint32 Handle : BindHandles)
+	{
+		RemoveBindingByHandle(Handle);
+	}
+	
+	BindHandles.Reset();
+}
---
+++ Source/NeoSanctum/Input/NSInputComponent.h
@@ -0,0 +1,97 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "GameplayTagContainer.h"
+#include "NSInputConfig.h"
+#include "NSInputComponent.generated.h"
+
+class UEnhancedInputLocalPlayerSubsystem;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class NEOSANCTUM_API UNSInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	UNSInputComponent(const FObjectInitializer& ObjectInitializer);
+
+	void AddInputMapping(
+		const UNSInputConfig* InputConfig,
+		UEnhancedInputLocalPlayerSubsystem* InputSubSystem);
+
+	void RemoveInputMapping(
+		const UNSInputConfig* InputConfig,
+		UEnhancedInputLocalPlayerSubsystem* InputSubSystem) const;
+
+	void RemoveBinds(TArray<uint32>& BindHandles);
+
+public:
+	template <class UserClass, typename FuncType>
+	void BindNativeAction(
+		const UNSInputConfig* InputConfig,
+		const FGameplayTag& InputTag,
+		ETriggerEvent TriggerEvent,
+		UserClass* Object,
+		FuncType Func,
+		bool bLogIfNotFound
+	);
+
+	template <class UserClass, typename PressedFuncType, typename ReleasedFuncType>
+	void BindAbilityActions(
+		const UNSInputConfig* InputConfig,
+		UserClass* Object,
+		PressedFuncType PressedFunc,
+		ReleasedFuncType ReleasedFunc,
+		TArray<uint32>& BindHandles
+	);
+};
+
+template <class UserClass, typename FuncType>
+void UNSInputComponent::BindNativeAction(const UNSInputConfig* InputConfig, const FGameplayTag& InputTag,
+	ETriggerEvent TriggerEvent, UserClass* Object, FuncType Func, bool bLogIfNotFound)
+{
+	check(InputConfig);
+	
+	if (const UInputAction* IA = InputConfig->FindNativeInputActionForTag(InputTag, bLogIfNotFound))
+	{
+		BindAction(IA, TriggerEvent, Object, Func);	
+	}
+}
+
+template <class UserClass, typename PressedFuncType, typename ReleasedFuncType>
+void UNSInputComponent::BindAbilityActions(const UNSInputConfig* InputConfig, UserClass* Object,
+	PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, TArray<uint32>& BindHandles)
+{
+	check(InputConfig);
+	
+	for (const FNSInputAction& Action : InputConfig->AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			if (PressedFunc)
+			{
+				BindHandles.Add(BindAction(
+					Action.InputAction,
+					Action.PressedTriggerEvent,
+					Object,
+					PressedFunc, 
+					Action.InputTag
+				).GetHandle());
+			}
+			
+			if (ReleasedFunc)
+			{
+				BindHandles.Add(BindAction(
+					Action.InputAction, 
+					Action.ReleasedTriggerEvent,
+					Object,
+					ReleasedFunc,
+					Action.InputTag
+				).GetHandle());
+			}
+		}
+	}
+}
