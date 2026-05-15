a65ad48325ca53225012bcfd3f50172bbe24a127
Park WonJong <kidsmon@naver.com>
2026-05-15 20:51:05 +0900

    add(gas): NSGameplayTags_State 작성

---
+++ Source/NeoSanctum/Tag/NSGAmeplayTags_State.cpp
@@ -0,0 +1,10 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#include "NSGAmeplayTags_State.h"
+
+namespace NSGameplayTags
+{
+	// State
+	UE_DEFINE_GAMEPLAY_TAG(State_Dead, "State.Dead");
+	UE_DEFINE_GAMEPLAY_TAG(State_Invincible, "State.Invincible");
+}
---
+++ Source/NeoSanctum/Tag/NSGAmeplayTags_State.h
@@ -0,0 +1,12 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace NSGAmeplayTags
+{
+	// State
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Dead);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Invincible);
+}
