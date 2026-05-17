29bf03c6c98c71d891777fbc2975f5e95bb03668
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-17 10:19:14 +0900

    feat: Player Controller에서 InputAction 바인딩
    Player Controller에서 InputAction 바인딩
    - NativeAction
      - Move, Look
    - AbilityAction
      - Pressed/Released

--- Source/NeoSanctum/Core/PlayerController/NSPlayerController.cpp
+++ Source/NeoSanctum/Core/PlayerController/NSPlayerController.cpp
@@ -4,6 +4,7 @@
 #include "NSPlayerController.h"
 
 #include "NeoSanctum/Input/NSInputComponent.h"
+#include "NeoSanctum/Tag/NSGameplayTags_Input.h"
 
 ANSPlayerController::ANSPlayerController()
 {
@@ -17,27 +18,58 @@ void ANSPlayerController::BeginPlay()
 void ANSPlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
-	
+
 	UNSInputComponent* NSInputComponent = CastChecked<UNSInputComponent>(InputComponent);
-	if (!IsValid(NSInputComponent)) return;
-	
+	if (!IsValid(NSInputComponent))
+	{
+		return;
+	}
+
 	if (InputConfig)
 	{
-		// NativeAction 바인딩하는 부분
+		NSInputComponent->BindNativeAction(
+			InputConfig,
+			NSGameplayTags::Input_Native_Move,
+			ETriggerEvent::Triggered,
+			this,
+			&ThisClass::Input_Move,
+			true
+		);
+
+		NSInputComponent->BindNativeAction(
+			InputConfig,
+			NSGameplayTags::Input_Native_Look,
+			ETriggerEvent::Triggered,
+			this,
+			&ThisClass::Input_Look,
+			true
+		);
+
+		TArray<uint32> BindHandles;
+		NSInputComponent->BindAbilityActions(
+			InputConfig,
+			this,
+			&ThisClass::Input_AbilityPressed,
+			&ThisClass::Input_AbilityReleased,
+			BindHandles
+		);
 	}
 }
 
 void ANSPlayerController::Input_Move(const FInputActionValue& Value)
 {
 	APawn* ControlledPawn = GetPawn();
-	if (!ControlledPawn) return;
-	
+	if (!ControlledPawn)
+	{
+		return;
+	}
+
 	const FVector2D MoveValue = Value.Get<FVector2D>();
 	const FRotator MoveRotation(0.f, GetControlRotation().Yaw, 0.f);
-	
+
 	const FVector ForwardDirection = MoveRotation.RotateVector(FVector::ForwardVector);
 	const FVector RightDirection = MoveRotation.RotateVector(FVector::RightVector);
-	
+
 	ControlledPawn->AddMovementInput(ForwardDirection, MoveValue.Y);
 	ControlledPawn->AddMovementInput(RightDirection, MoveValue.X);
 }
@@ -45,17 +77,17 @@ void ANSPlayerController::Input_Move(const FInputActionValue& Value)
 void ANSPlayerController::Input_Look(const FInputActionValue& Value)
 {
 	const FVector2D LookValue = Value.Get<FVector2D>();
-	
+
 	AddYawInput(LookValue.X);
 	AddPitchInput(LookValue.Y);
 }
 
 void ANSPlayerController::Input_AbilityPressed(FGameplayTag InputTag)
 {
-	
+	// ASC 추가 이후 구현
 }
 
 void ANSPlayerController::Input_AbilityReleased(FGameplayTag InputTag)
 {
-	
+	// ASC 추가 이후 구현
 }
