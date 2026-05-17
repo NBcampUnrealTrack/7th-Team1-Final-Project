60edb36f32ce5682421632bc9b8020a54be704e0
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-17 10:13:08 +0900

    chore: Input Tag 카테고리 세분화
     Input Tag 카테고리를 Native와 Ability로 세분화하여 설정할 때 구분을 확실하게 할 수 있도록 변경

--- Source/NeoSanctum/Tag/NSGameplayTags_Input.cpp
+++ Source/NeoSanctum/Tag/NSGameplayTags_Input.cpp
@@ -5,15 +5,15 @@
 namespace NSGameplayTags
 {
 	// Native Input
-	UE_DEFINE_GAMEPLAY_TAG(Input_Move, "Input.Move");
-	UE_DEFINE_GAMEPLAY_TAG(Input_Look, "Input.Look");
-	UE_DEFINE_GAMEPLAY_TAG(Input_Jump, "Input.Jump");
-	UE_DEFINE_GAMEPLAY_TAG(Input_Sprint, "Input.Sprint");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Native_Move, "Input.Native.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Native_Look, "Input.Native.Look");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Native_Jump, "Input.Native.Jump");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Native_Sprint, "Input.Native.Sprint");
 	
 	// Ability Input
-	UE_DEFINE_GAMEPLAY_TAG(Input_BaseAttack, "Input.BaseAttack");
-	UE_DEFINE_GAMEPLAY_TAG(Input_ActiveSkill1, "Input.ActiveSkill1");
-	UE_DEFINE_GAMEPLAY_TAG(Input_ActiveSkill2, "Input.ActiveSkill2");
-	UE_DEFINE_GAMEPLAY_TAG(Input_ActiveSkill3, "Input.ActiveSkill3");
-	UE_DEFINE_GAMEPLAY_TAG(Input_Dodge, "Input.Dodge");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_BaseAttack, "Input.Ability.BaseAttack");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_ActiveSkill1, "Input.Ability.ActiveSkill1");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_ActiveSkill2, "Input.Ability.ActiveSkill2");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_ActiveSkill3, "Input.Ability.ActiveSkill3");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_Dodge, "Input.Ability.Dodge");
 }
