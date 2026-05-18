3927d618ab4cf69e0edd7df611d47b873a8814ed
YongHo <skymo6872@gmail.com>
2026-05-18 21:10:44 +0900

    add: Run 게임 내 진행과 판정을 위한 함수 추가

--- Source/NeoSanctum/Core/GameMode/NSRunGameMode.cpp
+++ Source/NeoSanctum/Core/GameMode/NSRunGameMode.cpp
@@ -2,3 +2,45 @@
 
 
 #include "NSRunGameMode.h"
+#include "NeoSanctum/Core/GameState/NSRunGameState.h"
+#include "NeoSanctum/Core/PlayerController/NSPlayerController.h"
+#include "NeoSanctum/Core/PlayerState/NSPlayerState.h"
+
+
+ANSRunGameMode::ANSRunGameMode()
+{
+	GameStateClass = ANSRunGameState::StaticClass();
+	PlayerControllerClass = ANSPlayerController::StaticClass();
+	PlayerStateClass = ANSPlayerState::StaticClass();
+}
+
+void ANSRunGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// TODO: 후에 StageManager 구현 후 연결해야함
+}
+
+void ANSRunGameMode::StartNextStage()
+{
+}
+
+void ANSRunGameMode::OnStageCleared()
+{
+}
+
+void ANSRunGameMode::NotifyPlayerDied(AController* DeadPlayer)
+{
+}
+
+void ANSRunGameMode::HandleRunOver(bool bIsClear)
+{
+	if (bIsClear)
+	{
+		// 클리어 시 로직 및 거점 귀환
+	}
+	else
+	{
+		// 전멸 시 런 종료 및 거점 귀환
+	}
+}
