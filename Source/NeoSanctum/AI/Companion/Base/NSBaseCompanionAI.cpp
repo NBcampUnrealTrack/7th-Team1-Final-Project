f12fb61b193d88c19027e658277c800bd9efd0a5
Daype <mj1527123@gmail.com>
2026-05-18 16:32:36 +0900

    add(ai) : CompanionAI 초기파일 세팅

---
+++ Source/NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBaseCompanionAI.h"
+#include "Components/SceneComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+// Sets default values
+ANSBaseCompanionAI::ANSBaseCompanionAI()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	RootScene = CreateDefaultSubobject<USceneComponent>("SceneComponent");
+	SetRootComponent(RootScene);
+	
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	MeshComponent->SetupAttachment(RootScene);
+	
+	MovementComp = CreateDefaultSubobject<UFloatingPawnMovement>("FloatingPawnMovement");
+	MovementComp->UpdatedComponent = RootScene;
+	
+}
+
+
---
+++ Source/NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.h
@@ -0,0 +1,43 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "NSBaseCompanionAI.generated.h"
+
+class UNSCompanionAttributeSet;
+class UNSCompanionAbilitySystemComponent;
+class UFloatingPawnMovement;
+class USceneComponent;
+
+UCLASS()
+class NEOSANCTUM_API ANSBaseCompanionAI : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ANSBaseCompanionAI();
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UFloatingPawnMovement> MovementComp;
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> RootScene;
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="AbilitySystemComponent")
+	TObjectPtr<UNSCompanionAbilitySystemComponent> CompanionAbilitySystemComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="AbilitySystemComponent")
+	TObjectPtr<UNSCompanionAttributeSet> CompanionAttributeSet;
+	
+public:
+	FORCEINLINE UNSCompanionAbilitySystemComponent* GetCompanionAbilitySystemComponent() const {return CompanionAbilitySystemComponent;}
+	
+	FORCEINLINE UNSCompanionAttributeSet* GetCompanionAttributeSet() const {return CompanionAttributeSet;}
+};
