4217c94c882e4b3bcc91a957e05f8d408193c677
Daype <mj1527123@gmail.com>
2026-05-18 16:34:22 +0900

    add(gas) : CompanionAI 어트리뷰트 셋 초기파일 세팅

---
+++ Source/NeoSanctum/GAS/AttributeSet/NSCompanionAttributeSet.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "NSCompanionAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSCompanionAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+};
