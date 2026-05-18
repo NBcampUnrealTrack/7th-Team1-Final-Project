b41e2a60c74ffecd115149d614b8f866a6b9dd91
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-18 09:03:16 +0900

    Add: AnimInstance Skeleton Code
    캐릭터용 AnimInstance 뼈대

---
+++ Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.h
@@ -0,0 +1,13 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NSCharacterAnimInstance.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API UNSCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
