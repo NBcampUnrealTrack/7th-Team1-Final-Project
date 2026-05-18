c728be5467d70503213fcc6d5f6a3e160f78c49d
YongHo <skymo6872@gmail.com>
2026-05-18 19:11:55 +0900

    Init: 런 내,외 게임스테이트 생성
    NSRunGameState, NSOutGameState 생성

---
+++ Source/NeoSanctum/Core/GameState/NSOutGameState.h
@@ -0,0 +1,16 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "NSOutGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API ANSOutGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
