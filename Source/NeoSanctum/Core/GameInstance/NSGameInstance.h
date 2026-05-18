47ad3923c9d6f6316759f45cd89406e5ce1f31ed
YongHo <skymo6872@gmail.com>
2026-05-18 21:09:55 +0900

    Init: GameInstance 파일 생성

---
+++ Source/NeoSanctum/Core/GameInstance/NSGameInstance.h
@@ -0,0 +1,14 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NSGameInstance.generated.h"
+
+
+UCLASS()
+class NEOSANCTUM_API UNSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
