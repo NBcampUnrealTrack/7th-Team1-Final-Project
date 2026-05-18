9aef644acce07dc68048b02b986e078faf8b1f60
YongHo <skymo6872@gmail.com>
2026-05-18 19:07:18 +0900

    Init: 런 내 전용 게임모드 추가

---
+++ Source/NeoSanctum/Core/GameMode/NSRunGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NSRunGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API ANSRunGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
