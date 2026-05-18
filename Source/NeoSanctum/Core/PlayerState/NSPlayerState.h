7539367718ac14693e285947bbc3145927c99d50
Park WonJong <kidsmon@naver.com>
2026-05-18 17:45:57 +0900

    add(gas): PlayerState 파일 생성

---
+++ Source/NeoSanctum/Core/PlayerState/NSPlayerState.h
@@ -0,0 +1,16 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "NSPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API ANSPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+};
