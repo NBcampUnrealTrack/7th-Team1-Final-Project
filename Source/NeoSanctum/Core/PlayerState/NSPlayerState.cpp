2ce60157a4406e7a1c6cb9e20c9627284195efae
Park WonJong <kidsmon@naver.com>
2026-05-18 19:25:40 +0900

    feat(GAS): AttributeSet과 PlayerState GAS 기반 구조 추가

--- Source/NeoSanctum/Core/PlayerState/NSPlayerState.cpp
+++ Source/NeoSanctum/Core/PlayerState/NSPlayerState.cpp
@@ -2,3 +2,29 @@
 
 
 #include "NSPlayerState.h"
+
+#include "NeoSanctum/GAS/NSAbilitySystemComponent.h"
+#include "NeoSanctum/GAS/AttributeSet/NSPlayerAttributeSet.h"
+
+ANSPlayerState::ANSPlayerState()
+{
+	// PlayerState의 기본 Frequency는 1Hz(매우 낮음)
+	SetNetUpdateFrequency(100.0f);
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UNSAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	
+	PlayerAttributeSet = CreateDefaultSubobject<UNSPlayerAttributeSet>(TEXT("PlayerAttributeSet"));
+}
+
+UAbilitySystemComponent* ANSPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UNSPlayerAttributeSet* ANSPlayerState::GetPlayerAttributeSet() const
+{
+	return PlayerAttributeSet;
+}
--- Source/NeoSanctum/Core/PlayerState/NSPlayerState.h
+++ Source/NeoSanctum/Core/PlayerState/NSPlayerState.h
@@ -3,14 +3,31 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/PlayerState.h"
 #include "NSPlayerState.generated.h"
 
+class UNSAbilitySystemComponent;
+class UNSPlayerAttributeSet;
 /**
  * 
  */
 UCLASS()
-class NEOSANCTUM_API ANSPlayerState : public APlayerState
+class NEOSANCTUM_API ANSPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
+	
+public:
+	ANSPlayerState();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+	UNSPlayerAttributeSet* GetPlayerAttributeSet() const;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UNSAbilitySystemComponent> AbilitySystemComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UNSPlayerAttributeSet> PlayerAttributeSet;
 };
