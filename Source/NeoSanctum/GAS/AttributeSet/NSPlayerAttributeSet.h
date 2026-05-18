40b14429e252445798c9ecef14cac4c1300c41d5
Park WonJong <kidsmon@naver.com>
2026-05-18 17:36:52 +0900

    feat(GAS): Player가 사용할 AttributeSet
    - 프로토 타입 기준으로 Shield, MaxShiled만 추가

---
+++ Source/NeoSanctum/GAS/AttributeSet/NSPlayerAttributeSet.h
@@ -0,0 +1,42 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NSBaseAttributeSet.h"
+#include "NSPlayerAttributeSet.generated.h"
+
+/**
+ * 플레이어 전용 AttributeSet
+ */
+UCLASS()
+class NEOSANCTUM_API UNSPlayerAttributeSet : public UNSBaseAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Shield, Category = "GAS|Attribute")
+	FGameplayAttributeData Shield;
+	ATTRIBUTE_ACCESSORS(UNSPlayerAttributeSet, Shield);
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxShield, Category = "GAS|Attribute")
+	FGameplayAttributeData MaxShield;
+	ATTRIBUTE_ACCESSORS(UNSPlayerAttributeSet, MaxShield);
+
+protected:
+	// Health 적용 전 Shield로 데미지를 흡수
+	virtual float HandlePreHealthDamage(float DamageAmount, const FGameplayEffectModCallbackData& Data) override;
+	
+private:
+	UFUNCTION()
+	void OnRep_Shield(const FGameplayAttributeData& OldShield);
+	
+	UFUNCTION()
+	void OnRep_MaxShield(const FGameplayAttributeData& OldMaxShield);
+};
