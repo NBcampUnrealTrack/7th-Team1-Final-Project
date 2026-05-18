2eb60f5d82e08bc71293d5b02b32a8ab2e421421
YongHo <skymo6872@gmail.com>
2026-05-18 19:05:51 +0900

    Init: 아웃 런 전용 게임모드 추가

---
+++ Source/NeoSanctum/Core/GameMode/NSOutGameMode.h
@@ -0,0 +1,14 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NSOutGameMode.generated.h"
+
+
+UCLASS()
+class NEOSANCTUM_API ANSOutGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
