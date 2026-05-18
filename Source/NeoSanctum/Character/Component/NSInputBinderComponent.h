e8f02c4e4545e1486a074dd648c9cfe09edc886b
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-18 15:16:13 +0900

    refactor(Input): NSInputBinderComponent로 Input 바인딩 로직 이전
    Player Controller에서 바인딩을 하는 것보다는 Character에서 바인딩을 하는 것이 Possess / Respawn 구조 면에서 좋다는 의견
    - 플레이어 Pawn은 런타임 중에 계속해서 바뀔 수 있는데, 이 때 새 Pawn에 Possess 될 때마다 해당 Pawn의 SetupPlayerInputComponent가 호출되면서 그에 맞게 바인딩될 수 있는 형태.
    - 하지만 캐릭터 교체가 Data기반으로 이루어진다는 점에서 바인딩 교체가 자연스럽게 SetupPlayerInputComponent 호출로 이루어질 것 같지는 않아서 Bind/Unbind 함수를 유지하기로 했음.

---
+++ Source/NeoSanctum/Character/Component/NSInputBinderComponent.h
@@ -0,0 +1,67 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameplayTagContainer.h"
+#include "NeoSanctum/Input/NSInputConfig.h"
+#include "NSInputBinderComponent.generated.h"
+
+class UEnhancedInputLocalPlayerSubsystem;
+class UInputComponent;
+class UNSInputComponent;
+class UNSInputConfig;
+struct FInputActionValue;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class NEOSANCTUM_API UNSInputBinderComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UNSInputBinderComponent();
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	void InitializePlayerInput(UInputComponent* PlayerInputComponent);
+	void SetInputConfig(UNSInputConfig* NewConfig);
+
+protected:
+	void ApplyInputConfig();
+	void RemoveInputConfig();
+	void BindInputActions();
+	void UnbindInputActions();
+
+protected:
+	void Input_Move(const FInputActionValue& Value);
+	void Input_Look(const FInputActionValue& Value);
+	void Input_Jump();
+
+	void Input_AbilityPressed(FGameplayTag InputTag);
+	void Input_AbilityReleased(FGameplayTag InputTag);
+
+protected:
+	UEnhancedInputLocalPlayerSubsystem* GetInputSubsystem() const;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UNSInputConfig> DefaultInputConfig;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UNSInputConfig> CurrentInputConfig;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UNSInputComponent> InputComponent;
+
+	UPROPERTY(Transient)
+	bool bHasAppliedInputConfig = false;
+
+	UPROPERTY(Transient)
+	TArray<uint32> NativeInputBindHandles;
+
+	UPROPERTY(Transient)
+	TArray<uint32> AbilityInputBindHandles;
+};
--- Source/NeoSanctum/Core/PlayerController/NSPlayerController.h
+++ Source/NeoSanctum/Core/PlayerController/NSPlayerController.h
@@ -3,12 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameplayTagContainer.h"
 #include "GameFramework/PlayerController.h"
-#include "NeoSanctum/Input/NSInputConfig.h"
 #include "NSPlayerController.generated.h"
-
-struct FInputActionValue;
 
 UCLASS()
 class NEOSANCTUM_API ANSPlayerController : public APlayerController
@@ -17,51 +13,4 @@ class NEOSANCTUM_API ANSPlayerController : public APlayerController
 	
 public:
 	ANSPlayerController();
-	
-	virtual void BeginPlay() override;
-	virtual void SetupInputComponent() override;
-	
-public:
-	void SetInputConfig(UNSInputConfig* NewInputConfig, ENSInputRoute NewInputRoute);
-
-	void SetInputRoute(ENSInputRoute NewInputRoute);
-	
-protected:
-	void BindInputActions();
-
-	void UnbindInputActions();
-
-protected:
-	void Input_Move(const FInputActionValue& Value);
-	
-	void Input_Look(const FInputActionValue& Value);
-	
-	void Input_Jump();
-	
-protected:
-	void Input_AbilityPressed(FGameplayTag InputTag);
-	
-	void Input_AbilityReleased(FGameplayTag InputTag);
-	
-protected:
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	TObjectPtr<UNSInputConfig> DefaultInputConfig;
-
-	UPROPERTY(Transient)
-	TObjectPtr<UNSInputConfig> CurrentInputConfig;
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	ENSInputRoute DefaultInputRoute = ENSInputRoute::KeyboardMouse;
-	
-	UPROPERTY(Transient)
-	ENSInputRoute CurrentInputRoute = ENSInputRoute::KeyboardMouse;
-
-	UPROPERTY(Transient)
-	bool bHasAppliedInputConfig = false;
-
-	UPROPERTY(Transient)
-	TArray<uint32> NativeInputBindHandles;
-
-	UPROPERTY(Transient)
-	TArray<uint32> AbilityInputBindHandles;
 };
