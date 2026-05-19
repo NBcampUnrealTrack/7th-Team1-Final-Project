3138be224088b19071b93f8f13e65cc09e74d14a
JongKyu <kongi7064@gmail.com>
2026-05-19 12:34:26 +0900

    feat : SaveRepository 인터페이스 추가

---
+++ Source/NeoSanctum/System/NSSaveRepository.cpp
@@ -0,0 +1,3 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#include "NSSaveRepository.h"
---
+++ Source/NeoSanctum/System/NSSaveRepository.h
@@ -0,0 +1,32 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "NSSaveRepository.generated.h"
+
+/**
+ * 바이트 배열 저장 완료 콜백
+ * @param bSuccess 저장 성공 여부
+ */
+DECLARE_DELEGATE_OneParam(FNSSaveBytesComplete, bool);
+
+/**
+ * 바이트 배열 로드 완료 콜백
+ * @param bSuccess 저장 성공 여부
+ * @param Bytes 로드된 바이너리 데이터
+ */
+DECLARE_DELEGATE_TwoParams(FNSLoadBytesComplete, bool, const TArray<uint8>&);
+
+UINTERFACE(MinimalAPI)
+class UNSSaveRepository : public UInterface { GENERATED_BODY() };
+
+class NEOSANCTUM_API INSSaveRepository
+{
+	GENERATED_BODY()
+
+public:
+	virtual void SaveBytesAsync(const TArray<uint8>& Data, FNSSaveBytesComplete OnComplete) = 0;
+	virtual void LoadBytesAsync(FNSLoadBytesComplete OnComplete) = 0;
+};
