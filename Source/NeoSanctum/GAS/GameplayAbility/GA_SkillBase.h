f08a653ed7137a13c3b2cccac23d4fc2b440bb81
Park WonJong <kidsmon@naver.com>
2026-05-19 19:23:55 +0900

    add(GAS): GA_SkillBase 생성

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_SkillBase.h
@@ -0,0 +1,16 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_SkillBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_SkillBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+};
