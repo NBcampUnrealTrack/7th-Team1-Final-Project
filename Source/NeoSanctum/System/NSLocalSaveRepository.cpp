4ee181b946681d7db6454f9e96b39266d674a9f5
JongKyu <kongi7064@gmail.com>
2026-05-19 19:51:52 +0900

    fix : 세이브, 로드를 비동기로 변경

--- Source/NeoSanctum/System/NSLocalSaveRepository.cpp
+++ Source/NeoSanctum/System/NSLocalSaveRepository.cpp
@@ -3,24 +3,41 @@
 #include "NSLocalSaveRepository.h"
 #include "Misc/FileHelper.h"
 #include "Misc/Paths.h"
+#include "Async/Async.h"
 
 const FString UNSLocalSaveRepository::SavePath = FPaths::ProjectSavedDir() / TEXT("SaveGames/NS_Permanent.sav");
 
 void UNSLocalSaveRepository::SaveBytesAsync(const TArray<uint8>& Data, FNSSaveBytesComplete OnComplete)
 {
-	const bool bSuccess = FFileHelper::SaveArrayToFile(Data, *SavePath);
-	OnComplete.ExecuteIfBound(bSuccess);
+	TArray<uint8> DataCopy = Data;
+	Async(EAsyncExecution::ThreadPool, [DataCopy = MoveTemp(DataCopy), OnComplete]()
+	{
+		const bool bSuccess = FFileHelper::SaveArrayToFile(DataCopy, *SavePath);
+		AsyncTask(ENamedThreads::GameThread, [bSuccess, OnComplete]()
+		{
+			OnComplete.ExecuteIfBound(bSuccess);
+		});
+	});
 }
 
 void UNSLocalSaveRepository::LoadBytesAsync(FNSLoadBytesComplete OnComplete)
 {
-	if (!FPaths::FileExists(SavePath))
+	Async(EAsyncExecution::ThreadPool, [OnComplete]()
 	{
-		OnComplete.ExecuteIfBound(false, TArray<uint8>{});
-		return;
-	}
+		if (!FPaths::FileExists(SavePath))
+		{
+			AsyncTask(ENamedThreads::GameThread, [OnComplete]()
+			{
+				OnComplete.ExecuteIfBound(false, TArray<uint8>{});
+			});
+			return;
+		}
 
-	TArray<uint8> Bytes;
-	const bool bSuccess = FFileHelper::LoadFileToArray(Bytes, *SavePath);
-	OnComplete.ExecuteIfBound(bSuccess, Bytes);
+		TArray<uint8> Bytes;
+		const bool bSuccess = FFileHelper::LoadFileToArray(Bytes, *SavePath);
+		AsyncTask(ENamedThreads::GameThread, [bSuccess, Bytes = MoveTemp(Bytes), OnComplete]()
+		{
+			OnComplete.ExecuteIfBound(bSuccess, Bytes);
+		});
+	});
 }
