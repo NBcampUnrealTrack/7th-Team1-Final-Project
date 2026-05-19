f224299a6912ef67512983db6ebd2092c5ce3912
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-19 16:51:02 +0900

    add(anim): 캐릭터 공통 AnimInstance PROPERTY 설정
    캐릭터 공통 AnimInstance PROPERTY 설정
    - 변수만 일단 설정하고 Anim BP에서 Motion Matching 진행

--- Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.cpp
+++ Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.cpp
@@ -1,3 +1,70 @@
-﻿// Copyright 2026 One Team. All rights reserved.
+// Copyright 2026 One Team. All rights reserved.
 
 #include "NSCharacterAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UNSCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	RefreshOwningCharacter();
+}
+
+void UNSCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!OwnerCharacter)
+	{
+		RefreshOwningCharacter();
+	}
+
+	UpdateMovementData();
+}
+
+void UNSCharacterAnimInstance::RefreshOwningCharacter()
+{
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	CharacterMovement = OwnerCharacter ? OwnerCharacter->GetCharacterMovement() : nullptr;
+}
+
+void UNSCharacterAnimInstance::UpdateMovementData()
+{
+	if (!OwnerCharacter)
+	{
+		Velocity = FVector::ZeroVector;
+		LocalVelocity = FVector::ZeroVector;
+		Acceleration = FVector::ZeroVector;
+		GroundSpeed = 0.f;
+		bHasAcceleration = false;
+		bShouldMove = false;
+		bIsFalling = false;
+		return;
+	}
+
+	if (!CharacterMovement)
+	{
+		CharacterMovement = OwnerCharacter->GetCharacterMovement();
+	}
+
+	Velocity = OwnerCharacter->GetVelocity();
+	LocalVelocity = OwnerCharacter->GetActorTransform().InverseTransformVectorNoScale(Velocity);
+	GroundSpeed = FVector(Velocity.X, Velocity.Y, 0.f).Size();
+
+	if (CharacterMovement)
+	{
+		Acceleration = CharacterMovement->GetCurrentAcceleration();
+		bHasAcceleration = Acceleration.SizeSquared2D() > UE_KINDA_SMALL_NUMBER;
+		bIsFalling = CharacterMovement->IsFalling();
+	}
+	else
+	{
+		Acceleration = FVector::ZeroVector;
+		bHasAcceleration = false;
+		bIsFalling = false;
+	}
+
+	bShouldMove = GroundSpeed > 3.f && bHasAcceleration;
+}
--- Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.h
+++ Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.h
@@ -1,4 +1,4 @@
-﻿// Copyright 2026 One Team. All rights reserved.
+// Copyright 2026 One Team. All rights reserved.
 
 #pragma once
 
@@ -6,8 +6,58 @@
 #include "Animation/AnimInstance.h"
 #include "NSCharacterAnimInstance.generated.h"
 
+class ACharacter;
+class UCharacterMovementComponent;
+
+UENUM(BlueprintType)
+enum class ENSAnimGait : uint8
+{
+	Walk,
+	Run,
+	Sprint
+};
+
 UCLASS()
 class NEOSANCTUM_API UNSCharacterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	void RefreshOwningCharacter();
+	void UpdateMovementData();
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Owner")
+	TObjectPtr<ACharacter> OwnerCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Owner")
+	TObjectPtr<UCharacterMovementComponent> CharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	FVector Velocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	FVector LocalVelocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	FVector Acceleration = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	float GroundSpeed = 0.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	bool bHasAcceleration = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	bool bShouldMove = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	bool bIsFalling = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
+	ENSAnimGait Gait = ENSAnimGait::Run;
 };
