967c097df353e50c4438c7a8cd423ecc8077e43a
Park WonJong <kidsmon@naver.com>
2026-05-19 23:57:48 +0900

    feat(gas): 플레이어 캐릭터가 사용할 GA_SkillBase 구현

--- Source/NeoSanctum/GAS/GameplayAbility/GA_SkillBase.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_SkillBase.cpp
@@ -2,3 +2,49 @@
 
 
 #include "GA_SkillBase.h"
+
+#include "NeoSanctum/Core/PlayerState/NSPlayerState.h"
+#include "NeoSanctum/GAS/NSAbilitySystemComponent.h"
+#include "NeoSanctum/Tag/NSGameplayTags_State.h"
+
+UGA_SkillBase::UGA_SkillBase()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+	ReplicationPolicy = EGameplayAbilityReplicationPolicy::ReplicateNo;
+	
+	ActivationBlockedTags.AddTag(NSGameplayTags::State_Dead);
+}
+
+UNSAbilitySystemComponent* UGA_SkillBase::GetNSASC() const
+{
+	return Cast<UNSAbilitySystemComponent>(GetAbilitySystemComponentFromActorInfo());
+}
+
+ANSPlayerState* UGA_SkillBase::GetNSPlayerState() const
+{
+	return Cast<ANSPlayerState>(GetOwningActorFromActorInfo());
+}
+
+FString UGA_SkillBase::GetCurrentPredictionKeyStatus()
+{
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	if (!ASC)
+	{
+		return TEXT("NSASC 없음");
+	}
+	
+	return ASC->ScopedPredictionKey.ToString() + TEXT("예측키가 아직 열려 있나: ")
+		+ (ASC->ScopedPredictionKey.IsValidForMorePrediction() ? TEXT("열려있음") : TEXT("닫힘"));
+}
+
+bool UGA_SkillBase::IsPredictionKeyValidForMorePrediction() const
+{
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	if (!ASC)
+	{
+		return false;
+	}
+	
+	return ASC->ScopedPredictionKey.IsValidForMorePrediction();
+}
