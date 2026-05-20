81caea6b6417ddb6c53ce4e291ec0c479b23f139
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-20 18:12:20 +0900

    chore(anim): 에임오프셋 + TurnInPlace 구현 중지
    에임오프셋 + TurnInPlace에 대해 학습이 필요해서 일단 구현을 중지하고 기존 상태로 초기화

--- Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.cpp
+++ Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.cpp
@@ -3,16 +3,13 @@
 #include "NSCharacterAnimInstance.h"
 
 #include "CharacterTrajectoryComponent.h"
-#include "Components/SkeletalMeshComponent.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "GameFramework/Controller.h"
 
 void UNSCharacterAnimInstance::NativeInitializeAnimation()
 {
 	Super::NativeInitializeAnimation();
 
-	// AnimInstance가 생성될 때 캐릭터와 필요한 컴포넌트들을 캐싱
 	RefreshOwningCharacter();
 }
 
@@ -49,7 +46,6 @@ void UNSCharacterAnimInstance::UpdateMovementData(float DeltaSeconds)
 {
 	if (!OwnerCharacter)
 	{
-		// 캐릭터가 없으면 ABP가 이전 프레임 값을 계속 쓰지 못하게 기본값으로 초기화
 		Velocity = FVector::ZeroVector;
 		LocalVelocity = FVector::ZeroVector;
 		Acceleration = FVector::ZeroVector;
@@ -83,7 +79,6 @@ void UNSCharacterAnimInstance::UpdateMovementData(float DeltaSeconds)
 	VerticalVelocity = Velocity.Z;
 	bWasFalling = bIsFalling;
 
-	// CharacterMovement에서 가속도와 공중 상태를 읽어 Locomotion 상태를 만듬
 	if (CharacterMovement)
 	{
 		Acceleration = CharacterMovement->GetCurrentAcceleration();
@@ -97,7 +92,6 @@ void UNSCharacterAnimInstance::UpdateMovementData(float DeltaSeconds)
 		bIsFalling = false;
 	}
 
-	// 착지 순간에는 직전 프레임의 낙하 속도를 저장해서 Light/Heavy Land로 상태 구분
 	bIsMovingUp = bIsFalling && VerticalVelocity > 0.f;
 	if (bWasFalling && !bIsFalling)
 	{
@@ -106,17 +100,13 @@ void UNSCharacterAnimInstance::UpdateMovementData(float DeltaSeconds)
 		bHasLandRequest = true;
 	}
 
-	// 계산 순서가 꼬이면 안되는데, Aim/Turn 값을 먼저 만든 뒤에 AnimState에서 Turn 상태를 선택하도록 해야함
 	bShouldMove = GroundSpeed > 3.f && bHasAcceleration;
-	UpdateAimData(DeltaSeconds);
-	UpdateTurnInPlaceData();
 	UpdateAnimState();
 	PreviousVerticalVelocity = VerticalVelocity;
 }
 
 void UNSCharacterAnimInstance::UpdateAnimState()
 {
-	// 착지는 한 프레임짜리 요청으로 처리
 	if (bHasLandRequest)
 	{
 		AnimState = bIsHeavyLand ? ENSAnimState::LandHeavy : ENSAnimState::LandLight;
@@ -124,25 +114,14 @@ void UNSCharacterAnimInstance::UpdateAnimState()
 		return;
 	}
 
-	// 공중에서는 상승 중이면 JumpStart, 하강 중이면 FallLoop
 	if (bIsFalling)
 	{
 		AnimState = bIsMovingUp ? ENSAnimState::JumpStart : ENSAnimState::FallLoop;
 		return;
 	}
 
-	// 정지 중 카메라-몸 방향 차이가 크면 Turn In Place PSD에 따라 하체를 회전
-	if (bShouldTurnInPlace)
-	{
-		AnimState = TurnInPlaceDirection == ENSTurnInPlaceDirection::Left
-			? ENSAnimState::TurnInPlaceLeft
-			: ENSAnimState::TurnInPlaceRight;
-		return;
-	}
-
 	PreviousGroundedAnimState = CurrentGroundedAnimState;
 
-	// 현재 속도에 따라 Idle/Walk/Run/Sprint 중 하나의 상태가 됨
 	if (GroundSpeed < IdleSpeedThreshold)
 	{
 		CurrentGroundedAnimState = ENSAnimState::Idle;
@@ -160,7 +139,6 @@ void UNSCharacterAnimInstance::UpdateAnimState()
 		CurrentGroundedAnimState = ENSAnimState::Sprint;
 	}
 
-	// 속도 단계가 바뀐 순간에는 전환 PSD에 따라 애니메이션 재생
 	if (PreviousGroundedAnimState == ENSAnimState::Walk && CurrentGroundedAnimState == ENSAnimState::Run)
 	{
 		AnimState = ENSAnimState::WalkToRun;
@@ -191,109 +169,4 @@ void UNSCharacterAnimInstance::UpdateAnimState()
 	}
 
 	bIsHeavyLand = false;
-}
-
-void UNSCharacterAnimInstance::UpdateAimData(float DeltaSeconds)
-{
-	if (!OwnerCharacter)
-	{
-		TargetAimYaw = 0.f;
-		AimYaw = 0.f;
-		TargetAimPitch = 0.f;
-		AimPitch = 0.f;
-		AimOffsetAlpha = 0.f;
-		return;
-	}
-
-	const FRotator AimRotation = GetViewRotation();
-	const FRotator AimBaseRotation = GetAimBaseRotation();
-
-	// Sample Project 방식에 가깝게 카메라 방향과 메쉬 root 기준 방향의 차이를 AimOffset 입력으로 사용
-	TargetAimYaw = FRotator::NormalizeAxis(AimRotation.Yaw - AimBaseRotation.Yaw);
-	TargetAimPitch = FRotator::NormalizeAxis(AimRotation.Pitch);
-
-	// Yaw값의 경우 180/-180 경계에서 튀지 않도록 각도 차이를 먼저 구한 뒤에 보간
-	const float InterpolationAlpha = FMath::Clamp(DeltaSeconds * AimInterpolationSpeed, 0.f, 1.f);
-	const float YawDelta = FMath::FindDeltaAngleDegrees(AimYaw, TargetAimYaw);
-	const float PitchDelta = FMath::FindDeltaAngleDegrees(AimPitch, TargetAimPitch);
-
-	AimYaw = FRotator::NormalizeAxis(AimYaw + YawDelta * InterpolationAlpha);
-	AimPitch = FRotator::NormalizeAxis(AimPitch + PitchDelta * InterpolationAlpha);
-	AimOffsetAlpha = CharacterAnimType == ENSCharacterAnimType::Ranged
-		? RangedAimOffsetAlpha
-		: MeleeAimOffsetAlpha;
-}
-
-void UNSCharacterAnimInstance::UpdateTurnInPlaceData()
-{
-	if (!OwnerCharacter)
-	{
-		ViewYawDelta = 0.f;
-		RootYawOffset = 0.f;
-		bShouldTurnInPlace = false;
-		TurnInPlaceDirection = ENSTurnInPlaceDirection::None;
-		return;
-	}
-
-	const FRotator AimRotation = GetViewRotation();
-	const FRotator AimBaseRotation = GetAimBaseRotation();
-
-	// Turn In Place도 AimOffset과 같은 기준을 사용해야 상체/하체 회전에 문제가 생기지 않는다고 함
-	ViewYawDelta = FRotator::NormalizeAxis(AimRotation.Yaw - AimBaseRotation.Yaw);
-	RootYawOffset = ViewYawDelta;
-
-	const float AbsYawDelta = FMath::Abs(ViewYawDelta);
-	const bool bCanTurnInPlace = GroundSpeed < IdleSpeedThreshold && !bIsFalling;
-	if (!bCanTurnInPlace)
-	{
-		bShouldTurnInPlace = false;
-		TurnInPlaceDirection = ENSTurnInPlaceDirection::None;
-		return;
-	}
-	
-	bShouldTurnInPlace = bShouldTurnInPlace
-		? AbsYawDelta > TurnInPlaceStopAngle
-		: AbsYawDelta >= TurnInPlaceStartAngle;
-
-	TurnInPlaceDirection = bShouldTurnInPlace
-		? (ViewYawDelta < 0.f ? ENSTurnInPlaceDirection::Left : ENSTurnInPlaceDirection::Right)
-		: ENSTurnInPlaceDirection::None;
-
-	if (bShouldTurnInPlace)
-	{
-		// 하체가 상체를 따라 도는 중에는 상체 AimOffset의 영향을 조금 줄여서 허리 꼬임 현상을 줄이는 부분
-		AimOffsetAlpha *= TurnInPlaceAimOffsetAlphaMultiplier;
-	}
-}
-
-FRotator UNSCharacterAnimInstance::GetViewRotation() const
-{
-	if (!OwnerCharacter)
-	{
-		return FRotator::ZeroRotator;
-	}
-
-	if (const AController* Controller = OwnerCharacter->GetController())
-	{
-		return Controller->GetControlRotation();
-	}
-
-	return OwnerCharacter->GetBaseAimRotation();
-}
-
-FRotator UNSCharacterAnimInstance::GetAimBaseRotation() const
-{
-	if (!OwnerCharacter)
-	{
-		return FRotator::ZeroRotator;
-	}
-
-	const USkeletalMeshComponent* MeshComponent = GetSkelMeshComponent();
-	if (MeshComponent && MeshComponent->GetBoneIndex(AimBaseBoneName) != INDEX_NONE)
-	{
-		// Motion Matching/Turn In Place로 메쉬 root가 캡슐과 다르게 돌 수 있음. 따라서 root bone을 우선함.
-		return MeshComponent->GetSocketTransform(AimBaseBoneName, RTS_World).Rotator();
-	}
-
-	return OwnerCharacter->GetActorRotation();
 }
--- Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.h
+++ Source/NeoSanctum/Character/Animation/NSCharacterAnimInstance.h
@@ -14,13 +14,13 @@ class UCharacterTrajectoryComponent;
 UENUM(BlueprintType)
 enum class ENSAnimState : uint8
 {
-	// 기본 이동속도 상태
+	// 기본 이동 상태
 	Idle,
 	Walk,
 	Run,
 	Sprint,
 
-	// 속도 단계가 바뀌는 순간의 상태
+	// 기본 이동 상태가 바뀌는 순간의 전환 상태
 	WalkToRun,
 	WalkToSprint,
 	RunToWalk,
@@ -28,30 +28,11 @@ enum class ENSAnimState : uint8
 	SprintToWalk,
 	SprintToRun,
 
-	// 제자리 회전 상태
-	TurnInPlaceLeft,
-	TurnInPlaceRight,
-
 	// 공중/착지 상태
 	JumpStart,
 	FallLoop,
 	LandLight,
 	LandHeavy
-};
-
-UENUM(BlueprintType)
-enum class ENSTurnInPlaceDirection : uint8
-{
-	None,
-	Left,
-	Right
-};
-
-UENUM(BlueprintType)
-enum class ENSCharacterAnimType : uint8
-{
-	Melee,
-	Ranged
 };
 
 UCLASS()
@@ -71,30 +52,17 @@ public:
 	void SetSelectedPoseSearchDatabase(UPoseSearchDatabase* NewSelectedPoseSearchDatabase);
 
 protected:
-	// 소유 캐릭터와 컴포넌트를 캐싱합니다. AnimGraph에서는 이 값을 읽기만 하는 구조를 유지
+	// 소유 캐릭터와 애니메이션에서 읽을 컴포넌트를 캐싱
 	void RefreshOwningCharacter();
 
-	// 매 프레임 CharacterMovement 값을 애니메이션용 변수로 복사하고 파생 상태를 갱신
+	// CharacterMovement 값을 애니메이션용 변수로 복사하고 Locomotion 상태를 갱신
 	void UpdateMovementData(float DeltaSeconds);
 
-	// 현재 이동/공중/회전 조건을 ENSAnimState로 정리합니다. Chooser Table의 핵심 입력값
+	// 현재 이동/공중/착지 조건을 Chooser Table에서 사용할 상태로 정리
 	void UpdateAnimState();
 
-	// 카메라 방향과 메쉬 기준 방향의 차이를 AimOffset 입력값으로 계산
-	void UpdateAimData(float DeltaSeconds);
-
-	// 정지 중 카메라와 몸 방향 차이가 커졌는지 확인해 Turn In Place 상태를 계산
-	void UpdateTurnInPlaceData();
-
-	// 컨트롤러가 있으면 ControlRotation, 없으면 BaseAimRotation을 시선 방향으로 사용
-	FRotator GetViewRotation() const;
-
-	// AimOffset의 기준 방향
-	FRotator GetAimBaseRotation() const;
-
 protected:
-	// 캐릭터 / 컴포넌트 캐싱
-	// 매 프레임 Cast나 FindComponent를 반복하는 것이 비효율이라 초기화 시점에 캐싱
+	// 캐릭터 / 컴포넌트 캐시
 	UPROPERTY(BlueprintReadOnly, Category = "Animation|Owner")
 	TObjectPtr<ACharacter> OwnerCharacter;
 
@@ -105,7 +73,6 @@ protected:
 	TObjectPtr<UCharacterTrajectoryComponent> CharacterTrajectoryComponent;
 
 	// 이동 입력 / 속도 값
-	// CharacterMovement에서 복사한 원본 값과 ABP에서 쓸 수 있도록 변환한 값
 	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
 	FVector Velocity = FVector::ZeroVector;
 
@@ -143,17 +110,15 @@ protected:
 	bool bIsHeavyLand = false;
 
 	// Locomotion 상태
-	// 속도 변화와 캐릭터 상태에 따른 Chooser Table을 통한 애니메이션 선택
 	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
 	ENSAnimState PreviousGroundedAnimState = ENSAnimState::Idle;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
 	ENSAnimState CurrentGroundedAnimState = ENSAnimState::Idle;
 
-	// Chooser Table이 읽고 애니메이션 데이터를 선택하게되는 상태
 	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
 	ENSAnimState AnimState = ENSAnimState::Idle;
-	
+
 	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
 	ENSAnimState SelectedAnimState = ENSAnimState::Idle;
 
@@ -166,8 +131,7 @@ protected:
 	UPROPERTY(BlueprintReadOnly, Category = "Animation|Movement")
 	float PreviousVerticalVelocity = 0.f;
 
-	// 이동 상태 판정 기준값
-	// 프로젝트의 실제 이동 속도와 애니메이션 풀에 맞춰 에디터에서 조정
+	// 이동 상태 기본값
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Movement")
 	float IdleSpeedThreshold = 10.f;
 
@@ -179,62 +143,4 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Movement")
 	float HeavyLandVelocityThreshold = 900.f;
-
-	// AimOffset
-	// 캐릭터 타입에 따라 같은 AimOffset을 얼마나 강하게 적용할지 조절. 기본은 원거리 캐릭터
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Aim")
-	ENSCharacterAnimType CharacterAnimType = ENSCharacterAnimType::Ranged;
-
-	// AimOffset 기준이 되는 본. 일반적으로 root를 쓰는 경우가 많다고 하고 없으면 ActorRotation으로 대체하도록 함
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Aim")
-	FName AimBaseBoneName = TEXT("root");
-
-	// 보간하기 전의 AimYaw
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Aim")
-	float TargetAimYaw = 0.f;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Aim")
-	float AimYaw = 0.f;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Aim")
-	float TargetAimPitch = 0.f;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Aim")
-	float AimPitch = 0.f;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Aim")
-	float AimInterpolationSpeed = 12.f;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Aim")
-	float MeleeAimOffsetAlpha = 0.4f;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Aim")
-	float RangedAimOffsetAlpha = 1.0f;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Aim")
-	float AimOffsetAlpha = 0.4f;
-
-	// Turn In Place 중에는 하체가 도는 중이므로 상체 AimOffset 영향력을 어느 정도 줄여야 허리가 안 꼬임
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Aim")
-	float TurnInPlaceAimOffsetAlphaMultiplier = 0.5f;
-
-	// Turn In Place
-	// 정지 상태에서 카메라와 메쉬 기준 방향 차이가 커지면 하체를 회전하는 애니메이션
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Turn In Place")
-	float ViewYawDelta = 0.f;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Turn In Place")
-	float RootYawOffset = 0.f;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Turn In Place")
-	bool bShouldTurnInPlace = false;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Animation|Turn In Place")
-	ENSTurnInPlaceDirection TurnInPlaceDirection = ENSTurnInPlaceDirection::None;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Turn In Place")
-	float TurnInPlaceStartAngle = 60.f;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Turn In Place")
-	float TurnInPlaceStopAngle = 10.f;
 };
