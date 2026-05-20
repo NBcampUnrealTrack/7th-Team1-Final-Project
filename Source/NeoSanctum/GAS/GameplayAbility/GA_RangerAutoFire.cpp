cc9ac679f6755b0a28ecd72370a54e9cff225c47
Park WonJong <kidsmon@naver.com>
2026-05-20 21:33:42 +0900

    feat(gas): 사운드 및 애니메이션이 없는 연발 사격 토대 구현

--- Source/NeoSanctum/GAS/GameplayAbility/GA_RangerAutoFire.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_RangerAutoFire.cpp
@@ -2,3 +2,183 @@
 
 
 #include "GA_RangerAutoFire.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/Tasks/AbilityTask_WaitInputRelease.h"
+
+UGA_RangerAutoFire::UGA_RangerAutoFire()
+{
+}
+
+void UGA_RangerAutoFire::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!ActorInfo || !ActorInfo->AvatarActor.IsValid())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	// 입력 즉시 첫 발을 발사한 뒤, 이후로는 타이머로 반복
+	FireOnce();
+	StartAutoFire();
+	
+	// Input.BaseAttack이 Release되면 연사 종료
+	UAbilityTask_WaitInputRelease* ReleaseTask = 
+		UAbilityTask_WaitInputRelease::WaitInputRelease(this, true);
+	ReleaseTask->OnRelease.AddDynamic(this, &ThisClass::OnInputReleased);
+	ReleaseTask->ReadyForActivation();
+}
+
+void UGA_RangerAutoFire::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
+{
+	StopAutoFire();
+	
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UGA_RangerAutoFire::OnInputReleased(float TimeHeld)
+{
+	EndAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true, false);
+}
+
+void UGA_RangerAutoFire::StartAutoFire()
+{
+	if (FireInterval <= 0.0f)
+	{
+		return;
+	}
+	
+	UWorld* World = GetWorld();
+	
+	if (!World)
+	{
+		return;
+	}
+	
+	// 실제 발사 판정은 FireOnce 내부에서 서버 권한으로 제한
+	World->GetTimerManager().SetTimer(
+		AutoFireTimerHandle,
+		this,
+		&ThisClass::FireOnce,
+		FireInterval,
+		true
+	);
+}
+
+void UGA_RangerAutoFire::StopAutoFire()
+{
+	UWorld* World = GetWorld();
+	
+	if (!World)
+	{
+		return;
+	}
+	
+	World->GetTimerManager().ClearTimer(AutoFireTimerHandle);
+}
+
+void UGA_RangerAutoFire::FireOnce()
+{
+	AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	
+	if (!AvatarActor)
+	{
+		return;
+	}
+	
+	// 데미지 판정은 서버에서만 처리
+	if (!AvatarActor->HasAuthority())
+	{
+		return;
+	}
+	
+	PerformHitscan();
+}
+
+void UGA_RangerAutoFire::PerformHitscan()
+{
+	AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	
+	if (!AvatarActor)
+	{
+		return;
+	}
+	
+	UWorld* World = GetWorld();
+	
+	if (!World)
+	{
+		return;
+	}
+	
+	APawn* Pawn = Cast<APawn>(AvatarActor);
+	AController* Controller = Pawn ? Pawn->GetController() : nullptr;
+	
+	const FVector TraceStart = Pawn ? Pawn->GetPawnViewLocation() : AvatarActor->GetActorLocation();
+	
+	const FRotator AimRotation = Controller ? Controller->GetControlRotation() : AvatarActor->GetActorRotation();
+	
+	const FVector TraceDirection = AimRotation.Vector();
+	const FVector TraceEnd = TraceStart + TraceDirection * TraceRange;
+	
+	FHitResult HitResult;
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(AvatarActor);
+	
+	const bool bHit = World->LineTraceSingleByChannel(
+		HitResult,
+		TraceStart,
+		TraceEnd,
+		TraceChannel,
+		QueryParams
+	);
+	
+	if (!bHit)
+	{
+		return;
+	}
+	
+	ApplyDamageToActor(HitResult.GetActor());
+}
+
+void UGA_RangerAutoFire::ApplyDamageToActor(AActor* TargetActor)
+{
+	if (!TargetActor || !DamageEffectClass)
+	{
+		return;
+	}
+	
+	UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo();
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	
+	if (!SourceASC || TargetASC)
+	{
+		return;
+	}
+	
+	FGameplayEffectSpecHandle DamageSpecHandle = MakeOutgoingGameplayEffectSpec(DamageEffectClass, GetAbilityLevel());
+	
+	if (!DamageSpecHandle.IsValid())
+	{
+		return;
+	}
+	
+	// GE_Damage -> GEC_DamageExecution -> Damage Meta Attribute 흐름으로 데미지 전달
+	SourceASC->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(), TargetASC);
+}
--- Source/NeoSanctum/GAS/GameplayAbility/GA_RangerAutoFire.h
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_RangerAutoFire.h
@@ -7,10 +7,56 @@
 #include "GA_RangerAutoFire.generated.h"
 
 /**
- * 
+ * 원거리 캐릭터 기본 공격
+ * 입력 유지 중 히트스캔 공격 반복
  */
 UCLASS()
 class NEOSANCTUM_API UGA_RangerAutoFire : public UGA_SkillBase
 {
 	GENERATED_BODY()
+
+public:
+	UGA_RangerAutoFire();
+
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+	
+	UFUNCTION()
+	void OnInputReleased(float TimeHeld);
+	
+	void StartAutoFire();
+	void StopAutoFire();
+	
+	void FireOnce();
+	void PerformHitscan();
+	void ApplyDamageToActor(AActor* TargetActor);
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Ranger")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Ranger")
+	float FireInterval = 0.15f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Ranger")
+	float TraceRange = 1000.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Ranger")
+	TEnumAsByte<ECollisionChannel> TraceChannel = ECC_Visibility;
+	
+private:
+	FTimerHandle AutoFireTimerHandle;
 };
