cbed966acf29e8927d2a065236d4ba4e3b463365
Park WonJong <kidsmon@naver.com>
2026-05-20 17:13:46 +0900

    add(gas): 연발 사격 파일 생성

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_RangerAutoFire.h
@@ -0,0 +1,16 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_SkillBase.h"
+#include "GA_RangerAutoFire.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_RangerAutoFire : public UGA_SkillBase
+{
+	GENERATED_BODY()
+};
