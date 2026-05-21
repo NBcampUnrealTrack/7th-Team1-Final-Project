7a8dce05a9dac241bb516d1ed5c2469fb9f7dda3
Park WonJong <kidsmon@naver.com>
2026-05-21 17:47:38 +0900

    add(gas): 원거리 캐릭터의 기본 발사용 Cue처리를 위한 태그 파일 생성

---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Cue.h
@@ -0,0 +1,11 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace NSGameplayTags
+{
+	// Ranger
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayCue_Ranger_MuzzleFire);
+}
