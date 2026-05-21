f9a0face469d861ab4ef726ac1953fa1d1dfbce8
Daype <mj1527123@gmail.com>
2026-05-21 14:59:11 +0900

    add(ai) : 테스트 코인 클래스 추가

---
+++ Source/NeoSanctum/AI/Companion/Pawn/ANSTestCoin.h
@@ -0,0 +1,25 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ANSTestCoin.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API AANSTestCoin : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AANSTestCoin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
