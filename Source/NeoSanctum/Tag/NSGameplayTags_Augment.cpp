43efcd641ded709859161e4f3f226c127318ff7f
JongKyu <kongi7064@gmail.com>
2026-05-21 20:49:48 +0900

    feat(system): 증강 태그 추가

---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Augment.cpp
@@ -0,0 +1,10 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#include "NSGameplayTags_Augment.h"
+
+namespace NSGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Augment_Pool_Normal,         "Augment.Pool.Normal");
+	UE_DEFINE_GAMEPLAY_TAG(Augment_Pool_HighGrade,      "Augment.Pool.HighGrade");
+	UE_DEFINE_GAMEPLAY_TAG(Augment_SetByCaller_Stack,   "Augment.SetByCaller.Stack");
+}
