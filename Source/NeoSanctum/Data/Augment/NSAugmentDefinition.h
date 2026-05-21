40d9cd5b51614c62b984dff3c37d9f1a43d70341
JongKyu <kongi7064@gmail.com>
2026-05-21 21:25:15 +0900

    feat(system) : 증강 Definition정의

---
+++ Source/NeoSanctum/Data/Augment/NSAugmentDefinition.h
@@ -0,0 +1,55 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "NSAugmentTypes.h"
+#include "NSAugmentDefinition.generated.h"
+
+class UTexture2D;
+class UGameplayEffect;
+class UGameplayAbility;
+
+// 증강 카드 한장의 내용 (이름, 아이콘, 어떤 효과인지)
+UCLASS(BlueprintType)
+class NEOSANCTUM_API UNSAugmentDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meta")
+	ENSAugmentRarity Rarity = ENSAugmentRarity::Common;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meta")
+	FGameplayTag AugmentTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meta", meta = (ClampMin = "1"))
+	int32 MaxStack = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Display")
+	TSoftObjectPtr<UTexture2D> Icon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Display")
+	FText DisplayName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Display", meta = (MultiLine = true))
+	FText Description;
+	
+	/**
+	 * Common / Rare / Epic / Legendary (수치 강화) 같은 스택형 GE저장
+	 * SetByCaller로 현재 스택 카운터 전달
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect")
+	TSoftClassPtr<UGameplayEffect> StackEffectClass;
+	
+	/**
+	 * 기믹 변경 GA저장 -> Legendary
+	 * 가질수 있는 최대개수제한이 있음 
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect")
+	TSoftClassPtr<UGameplayAbility> GrantedAbilityClass;
+};
