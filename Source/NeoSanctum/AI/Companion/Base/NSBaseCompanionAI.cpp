8015b18f68684946d44c63f152f79bb944cf2ff9
Daype <mj1527123@gmail.com>
2026-05-22 15:13:00 +0900

    refactor(ai) : 내부 컴포넌트 기본제공 Character 사용 및 Pawn->Character 구조변경 리팩토링

--- Source/NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.cpp
+++ Source/NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.cpp
@@ -3,25 +3,17 @@
 
 #include "NSBaseCompanionAI.h"
 #include "Components/SceneComponent.h"
+#include "Perception/AIPerceptionComponent.h"
 #include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
-#include "GameFramework/FloatingPawnMovement.h"
+
 
 // Sets default values
 ANSBaseCompanionAI::ANSBaseCompanionAI()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	RootScene = CreateDefaultSubobject<USceneComponent>("SceneComponent");
-	SetRootComponent(RootScene);
-	
-	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
-	MeshComponent->SetupAttachment(RootScene);
-	
-	MovementComp = CreateDefaultSubobject<UFloatingPawnMovement>("FloatingPawnMovement");
-	MovementComp->UpdatedComponent = RootScene;
 	
 	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
-	
 	AttributeSet = CreateDefaultSubobject<UAttributeSet>("AttributeSet");
 }
 
