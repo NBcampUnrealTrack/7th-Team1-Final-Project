ef63b126a9e01c6837a6aa3ba0239f8bd933934d
Youngwoong <afsd3330@gmail.com>
2026-05-22 21:00:26 +0900

    refactor: HP/Shield UI TODO 정리

--- Source/NeoSanctum/UI/HUD/NSHUDWidget.cpp
+++ Source/NeoSanctum/UI/HUD/NSHUDWidget.cpp
@@ -14,8 +14,6 @@ void UNSHUDWidget::UpdateHealthAndShield(
 	)
 {
 	//HP / Shield 위젯이 없으면 갱신X
-	// TODO(영웅): 게임 시작시 플레의어의 체력/실드 값 연동
-	// TODO(영웅): 현재 체력/ 실드 값 변경시 자동 갱신 연결
 	if (!HPShieldWidget)
 	{
 		return;
