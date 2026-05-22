ca440d023e09105e7dc3d1ab4b4d022bb03b88af
Daype <mj1527123@gmail.com>
2026-05-22 17:23:11 +0900

    refactor(ai) : 파일 이름 수정 및 AIController 코인 바인딩, 컨트롤러 캐시 추가

---
+++ Source/NeoSanctum/AI/Companion/Pawn/NSTestCoin.cpp
@@ -0,0 +1,50 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSTestCoin.h"
+#include "EngineUtils.h"
+#include "NeoSanctum/AI/Controller/DroneAI/NSDroneAIController.h"
+#include "NeoSanctum/AI/Companion/Pawn/NSDroneAI.h"
+
+
+ANSTestCoin::ANSTestCoin()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ANSTestCoin::RegisterPriorityActor()
+{
+	for (const TWeakObjectPtr<ANSDroneAIController>& DroneAIController : CashecDroneAIControllers)
+	{
+		if (!DroneAIController.IsValid()) continue;
+		
+		ANSDroneAIController* DroneAIC = DroneAIController.Get();
+		if (!IsValid(DroneAIC)) continue;
+		
+		DroneAIC->SetPriorityActor(this);
+	}
+}
+
+
+void ANSTestCoin::BeginPlay()
+{
+	Super::BeginPlay();
+	RegisterPriorityActor();
+}
+
+void ANSTestCoin::Destroyed()
+{
+	for (const TWeakObjectPtr<ANSDroneAIController>& DroneAIController : CashecDroneAIControllers)
+	{
+		if (!DroneAIController.IsValid()) continue;
+		
+		ANSDroneAIController* DroneAIC = DroneAIController.Get();
+		if (!IsValid(DroneAIC)) continue;
+		
+		DroneAIC->RemoveCoinActor(this);
+	}
+	
+	Super::Destroyed();
+}
+
+
---
+++ Source/NeoSanctum/AI/Companion/Pawn/NSTestCoin.h
@@ -0,0 +1,30 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NSTestCoin.generated.h"
+
+class ANSDroneAIController;
+
+UCLASS()
+class NEOSANCTUM_API ANSTestCoin : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANSTestCoin();
+
+	void RegisterPriorityActor();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	virtual void Destroyed() override;
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TArray<TWeakObjectPtr<ANSDroneAIController>> CashecDroneAIControllers;
+
+};
