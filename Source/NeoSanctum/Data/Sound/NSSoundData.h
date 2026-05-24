d8719a2829dfb057b9a26097ea6df21890cfb7fc
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-24 13:49:45 +0900

    feat(sound): SoundDataTable 세팅
    SoundDataTable 세팅

--- Source/NeoSanctum/Data/Sound/NSSoundData.h
+++ Source/NeoSanctum/Data/Sound/NSSoundData.h
@@ -6,11 +6,46 @@
 #include "Engine/DataTable.h"
 #include "NSSoundData.generated.h"
 
-/**
- * 
- */
+UENUM(BLueprintType)
+enum class ENSSoundCategory : uint8
+{
+	BGM,
+	SFX,
+	UI
+};
+
 UCLASS()
 class NEOSANCTUM_API UNSSoundData : public UDataTable
 {
 	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	ENSSoundCategory SoundCategory = ENSSoundCategory::BGM;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	TObjectPtr<USoundBase> Sound;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float Volume = 1.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float Pitch = 1.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	bool bLoop = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	bool bAllowMultiple = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float FadeInTime = 0.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	float FadeOutTime = 0.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	TObjectPtr<USoundAttenuation> Attenuation;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SoundData")
+	TObjectPtr<USoundConcurrency> Concurrency;
 };
