b05274d8f7a8d77339a8857ef29a0919239490fa
JongKyu <kongi7064@gmail.com>
2026-05-24 19:20:26 +0900

    feat(system): 증강 인벤토리 컴포넌트 구현
    - PlayerState에 부착됨
    - GE/GA적용 및 플레이어가 들고있는 증강(Owned) 갱신
    - 델리게이트(OnInventoryChanged)로 알림

--- Source/NeoSanctum/Progression/Augment/NSAugmentInventoryComponent.h
+++ Source/NeoSanctum/Progression/Augment/NSAugmentInventoryComponent.h
@@ -4,8 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "NeoSanctum/Data/Augment/NSAugmentTypes.h"
 #include "NSAugmentInventoryComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAugmentInventoryChanged);
+
+class UNSAugmentDefitnition;
+class UAbilitySystemComponent;
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class NEOSANCTUM_API UNSAugmentInventoryComponent : public UActorComponent
@@ -14,12 +19,46 @@ class NEOSANCTUM_API UNSAugmentInventoryComponent : public UActorComponent
 
 public:
 	UNSAugmentInventoryComponent();
+	
+	UPROPERTY(BlueprintAssignable, Category="NS|Augment")
+	FOnAugmentInventoryChanged OnInventoryChanged;
 
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-							   FActorComponentTickFunction* ThisTickFunction) override;
+	// 서버 권한, 증강 적용, 이미 있으면 Stack증가 없으면 GE/GA Grant
+	void ApplyAugment(const FPrimaryAssetId& DefId);
+	
+	// 서버 권한, 인런 종료시 모든 GE/GA 제거
+	void ClearAll();
+	
+public:
+	UFUNCTION(BlueprintPure, Category="NS|Augment")
+	const TArray<FNSAugmentInstance>& GetOwned() const { return Owned; }
+	
+	UFUNCTION(BlueprintPure, Category = "NS|Augment")
+	int32 GetStackCount(const FPrimaryAssetId& DefId) const;
+	
+	UFUNCTION(BlueprintPure, Category = "NS|Augment")
+	int32 GetLegendaryCount() const;
+	
+	UFUNCTION(BlueprintPure, Category="NS|Augment")	
+	bool IsLegendaryFull() const {return GetLegendaryCount() >= MaxLegendarySlots; }
+
+	UAbilitySystemComponent* GetOwnerASC();
 protected:
-	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="NS|Augment", meta=(ClampMin="0"))
+	int32 MaxLegendarySlots = 3;
 	
 private:
+	UFUNCTION()
+	void OnRep_Owned();
 	
+	// 스택형 GE 적용
+	void ApplyStackEffect(FNSAugmentInstance& Inst, UNSAugmentDefinition* Def, UAbilitySystemComponent* ASC);
+
+	// 기믹 Legendary GA 부여
+	void GrantMechanicAbility(FNSAugmentInstance& Inst, UNSAugmentDefinition* Def, UAbilitySystemComponent* ASC);
+	
+	UPROPERTY(ReplicatedUsing=OnRep_Owned)
+	TArray<FNSAugmentInstance> Owned;
 };
