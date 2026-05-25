e7454f95fc2a8475708f9361d203a500632d1f35
ChoiJunHyeok <optimal9744@gmail.com>
2026-05-25 17:46:09 +0900

    add(ai): 지상 Enemy 클래스 뼈대 생성

---
+++ Source/NeoSanctum/Character/Enemy/NSGroundEnemyBase.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NSEnemyCharacterBase.h"
+#include "NSGroundEnemyBase.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSGroundEnemyBase : public ANSEnemyCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	ANSGroundEnemyBase();
+};
