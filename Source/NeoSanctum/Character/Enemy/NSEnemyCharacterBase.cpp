306bf7af3e6f70bc4c181668ac12d4edc8c71e5e
ChoiJunHyeok <optimal9744@gmail.com>
2026-05-25 16:35:15 +0900

    feat(ai): EnemyCharacter 클래스 ASC 이식

---
+++ Source/NeoSanctum/Character/Enemy/NSEnemyCharacterBase.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSEnemyCharacterBase.h"
+
+ANSEnemyCharacterBase::ANSEnemyCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ANSEnemyCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/NeoSanctum/Character/Enemy/NSEnemyCharacterBase.h
@@ -0,0 +1,30 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "NSEnemyCharacterBase.generated.h"
+
+class UNSMonsterAttributeSet;
+
+UCLASS(Abstract)
+class NEOSANCTUM_API ANSEnemyCharacterBase : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ANSEnemyCharacterBase();
+	virtual void BeginPlay() override;
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override { return ASC; }
+
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UAbilitySystemComponent> ASC;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UNSMonsterAttributeSet> AttributeSet;
+};
