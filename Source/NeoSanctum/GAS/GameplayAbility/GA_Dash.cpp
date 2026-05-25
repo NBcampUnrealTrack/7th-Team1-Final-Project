4ae6668586adb128c587fcb166126f9c28fbdc8e
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-25 22:08:11 +0900

    add(ability): GA_Dash 선언

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_Dash.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "GA_Dash.h"
---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_Dash.h
@@ -0,0 +1,36 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Dash.generated.h"
+
+/**
+ * 캐릭터 대시 어빌리티
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_Dash : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+	
+private:
+	float DashDistance = 450.f;
+	float DashDuration = 0.2f;
+};
