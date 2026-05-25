e100a3067fa59b023ff09f7baca9eb94c0b3189a
ChoiJunHyeok <optimal9744@gmail.com>
2026-05-25 16:54:09 +0900

    feat(ai): EnemyAnimInstance 클래스 이동 구현

---
+++ Source/NeoSanctum/Character/Animation/NSEnemyAnimInstance.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSEnemyAnimInstance.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "NeoSanctum/Character/Enemy/NSEnemyCharacterBase.h"
+
+void UNSEnemyAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	EnemyCharacter = Cast<ANSEnemyCharacterBase>(TryGetPawnOwner());
+	if (EnemyCharacter)
+	{
+		MovementComponent = EnemyCharacter->GetCharacterMovement();
+	}
+}
+
+void UNSEnemyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	// 초기화 실패 시 혹은 런타임 예외 발생 시 다시 캐싱
+	if (!EnemyCharacter || !MovementComponent)
+	{
+		EnemyCharacter = Cast<ANSEnemyCharacterBase>(TryGetPawnOwner());
+		if (EnemyCharacter)
+		{
+			MovementComponent = EnemyCharacter->GetCharacterMovement();
+		}
+	}
+
+	if (MovementComponent && EnemyCharacter)
+	{
+		GroundSpeed = EnemyCharacter->GetVelocity().Size2D();
+		bIsMoving = (GroundSpeed > MovingSpeedThreshold) 
+				&& (MovementComponent->GetCurrentAcceleration().Size2D() > 0.0f);
+	}
+}
