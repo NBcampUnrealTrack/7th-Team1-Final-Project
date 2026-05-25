12752ab9bf54cae6ad709ad03e24beb58f8938dc
ChoiJunHyeok <optimal9744@gmail.com>
2026-05-25 18:15:52 +0900

    feat(ai): EnemyCharacterBase에 몬스터 진영 반환 함수 추가
    - AIPerception 센서가 시야 반경 내 액터를 포착했을 때 플레이어 진영인지, 몬스터 진영인지 엔진 내부에서 OtherActor->GetGenericTeamId()를 자동으로 호출하는데 Enemy 간 공격 시 피아 식별을 위해 사용.

--- Source/NeoSanctum/Character/Enemy/NSEnemyCharacterBase.h
+++ Source/NeoSanctum/Character/Enemy/NSEnemyCharacterBase.h
@@ -4,27 +4,34 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemInterface.h"
+#include "GenericTeamAgentInterface.h"
 #include "GameFramework/Character.h"
 #include "NSEnemyCharacterBase.generated.h"
 
 class UNSMonsterAttributeSet;
 
 UCLASS(Abstract)
-class NEOSANCTUM_API ANSEnemyCharacterBase : public ACharacter, public IAbilitySystemInterface
+class NEOSANCTUM_API ANSEnemyCharacterBase : public ACharacter, public IAbilitySystemInterface,
+                                             public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
 
 public:
 	ANSEnemyCharacterBase();
 	virtual void BeginPlay() override;
-	
+
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override { return ASC; }
 
+	/*
+	 * 0번을 플레이어 진영
+	 * 1번을 몬스터 진영으로 구성할 예정.
+	 */
+	virtual FGenericTeamId GetGenericTeamId() const override { return FGenericTeamId(1); }
 
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
 	TObjectPtr<UAbilitySystemComponent> ASC;
-	
+
 	UPROPERTY(Transient)
 	TObjectPtr<UNSMonsterAttributeSet> AttributeSet;
 };
