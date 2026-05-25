7b45888255be390ae31cd98d68d6877f4974696a
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-26 00:44:33 +0900

    add(ability): GA_Dash 초기세팅

--- Source/NeoSanctum/GAS/GameplayAbility/GA_Dash.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_Dash.cpp
@@ -2,3 +2,24 @@
 
 
 #include "GA_Dash.h"
+
+void UGA_Dash::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData
+)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGA_Dash::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled
+)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
