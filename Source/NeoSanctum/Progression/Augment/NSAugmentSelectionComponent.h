f261db97ad73e460765947b5ac94e65a50774037
JongKyu <kongi7064@gmail.com>
2026-05-26 09:35:21 +0900

    feat(system): 증강 선택 컴포넌트 구현

--- Source/NeoSanctum/Progression/Augment/NSAugmentSelectionComponent.h
+++ Source/NeoSanctum/Progression/Augment/NSAugmentSelectionComponent.h
@@ -4,7 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GameplayTagContainer.h"
+#include "NeoSanctum/Data/Augment/NSAugmentTypes.h"
 #include "NSAugmentSelectionComponent.generated.h"
+
+class UNSAugmentPoolDefinition;
+class UNSAugmentDefinition;
+class UNSRunGameDataComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAugmentOfferPresented, const TArray<FPrimaryAssetId>&, OfferIds, int32, RerollCost);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAugmentOfferClosed);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class NEOSANCTUM_API UNSAugmentSelectionComponent : public UActorComponent
@@ -14,11 +23,61 @@ class NEOSANCTUM_API UNSAugmentSelectionComponent : public UActorComponent
 public:
 	UNSAugmentSelectionComponent();
 
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-							   FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(BlueprintAssignable, Category = "NS|Augment")
+	FOnAugmentOfferPresented OnOfferPresented;
+
+	UPROPERTY(BlueprintAssignable, Category = "NS|Augment")
+	FOnAugmentOfferClosed OnOfferClosed;
+
+	// 트리거(레벨업/엘리트킬/보물상자)에서 호출
+	UFUNCTION(Server, Reliable, BlueprintCallable, Category = "NS|Augment")
+	void Server_RequestOffer(FGameplayTag PoolTag);
+
+	UFUNCTION(Server, Reliable, BlueprintCallable, Category = "NS|Augment")
+	void Server_Reroll();
+
+	UFUNCTION(Server, Reliable, BlueprintCallable, Category = "NS|Augment")
+	void Server_Choose(int32 Index);
+	
 protected:
-	virtual void BeginPlay() override;
+	UPROPERTY(EditDefaultsOnly, Category = "NS|Augment", meta = (ClampMin = "1"))
+	int32 CardsCount = 3;
 	
 private:
+	UFUNCTION(Client, Reliable)
+	void Client_PresentOffer(const TArray<FPrimaryAssetId>& OfferIds, int32 RerollCost);
+
+	UFUNCTION(Client, Reliable)
+	void Client_CloseOffer();
 	
+	void RollAndPresent();
+	UNSAugmentPoolDefinition* FindPool(const FGameplayTag& PoolTag) const;
+	TArray<FPrimaryAssetId> RollCards(UNSAugmentPoolDefinition* Pool, int32 N) const;
+
+	UNSAugmentDefinition* ResolveDefinition(
+		UNSRunGameDataComponent* Run,
+		const TSoftObjectPtr<UNSAugmentDefinition>& SoftDef) const;
+
+	void CollectInventoryFilter(
+		bool& bOutLegendaryFull,
+		TSet<FPrimaryAssetId>& OutOwnedMechanicIds) const;
+
+	void BuildRarityBuckets(
+		UNSRunGameDataComponent* Run,
+		const UNSAugmentPoolDefinition* Pool,
+		bool bLegendaryFull,
+		const TSet<FPrimaryAssetId>& OwnedMechanicIds,
+		TMap<ENSAugmentRarity, TArray<UNSAugmentDefinition*>>& OutByRarity) const;
+
+	TArray<FPrimaryAssetId> DrawCards(
+		const UNSAugmentPoolDefinition* Pool,
+		const TMap<ENSAugmentRarity, TArray<UNSAugmentDefinition*>>& ByRarity,
+		int32 N) const;
+	
+	UPROPERTY()
+	TObjectPtr<UNSAugmentPoolDefinition> CurrentPool;
+
+	TArray<FPrimaryAssetId> PendingOffer;
+
+	int32 CurrentRerollCost = 0;
 };
