08bd98c17e65c7f02db66543b3428ef73bf0dd04
Park WonJong <kidsmon@naver.com>
2026-05-26 09:11:12 +0900

    add(data): 캐릭터 데이터 파일 생성

---
+++ Source/NeoSanctum/Data/Character/NSCharacterData.h
@@ -0,0 +1,23 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "NSCharacterData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FNSCharacterAbilityEntry
+{
+	GENERATED_BODY()
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSCharacterData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
