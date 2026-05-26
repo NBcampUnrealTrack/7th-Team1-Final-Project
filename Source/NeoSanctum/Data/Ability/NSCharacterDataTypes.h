cab8d3830adbb5990e67b050bac9d76de1369462
Park WonJong <kidsmon@naver.com>
2026-05-26 11:41:42 +0900

    add(data): 게임 중에서 캐릭터가 사용하는 전반적인 구조체 데이터를 관리하는 파일 생성
    NSCharacterDataTypes.h
     ├─ FNSCharacterAbilityData
     ├─ FNSCharacterWeaponData       // 필요해지면 추가
     └─ FNSCharacterVisualData       // 필요해지면 추가

---
+++ Source/NeoSanctum/Data/Ability/NSCharacterDataTypes.h
@@ -0,0 +1,28 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "NSCharacterDataTypes.generated.h"
+
+class UGameplayAbility;
+
+USTRUCT(BlueprintType)
+struct FNSCharacterAbilityData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character|Ability")
+	TSoftClassPtr<UGameplayAbility> AbilityClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character|Ability")
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character|Ability")
+	FGameplayTag AbilityTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character|Ability", meta = (ClampMin = "1"))
+	int32 AbilityLevel = 1;
+};
