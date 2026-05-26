ce97a1642fe832be8eb2492d1c04fca69ff44972
JRoLee <cnsfw123@gmail.com>
2026-05-26 20:13:33 +0900

    add(level) : 파괴가능 오브젝트 데미지 처리를 위한 어트리뷰트 셋 추가
     파괴가능 오브젝트 데미지 처리를 위한 어트리뷰트 셋 추가

---
+++ Source/NeoSanctum/GAS/AttributeSet/NSDestructibleAttributeSet.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSDestructibleAttributeSet.h"
+#include "GameplayEffectExtension.h"
+#include "Net/UnrealNetwork.h"
+
+void UNSDestructibleAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UNSDestructibleAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UNSDestructibleAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void UNSDestructibleAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+	
+	if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		float DmgDone = GetDamage();
+		SetDamage(0.f);
+		SetHealth(FMath::Max(GetHealth() - DmgDone, 0.f));
+	}
+	else if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}
+}
+
+void UNSDestructibleAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNSDestructibleAttributeSet, Health, OldHealth);
+}
+
+void UNSDestructibleAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNSDestructibleAttributeSet, MaxHealth, OldMaxHealth);
+}
---
+++ Source/NeoSanctum/GAS/AttributeSet/NSDestructibleAttributeSet.h
@@ -0,0 +1,46 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "NSDestructibleAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)           \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)               \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)               \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ *  Destructible Object를 위한 AttributeSet
+ */
+UCLASS()
+class NEOSANCTUM_API UNSDestructibleAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health, Category="Destructible")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UNSDestructibleAttributeSet, Health)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth, Category="Destructible")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UNSDestructibleAttributeSet, MaxHealth)
+	
+	UPROPERTY(BlueprintReadOnly, Category="Destructible")
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UNSDestructibleAttributeSet, Damage)
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+protected:
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+};
