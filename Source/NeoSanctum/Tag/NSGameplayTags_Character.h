b5ef1af611fcdad999099021132bc5f8c54aed3e
Park WonJong <kidsmon@naver.com>
2026-05-26 12:18:21 +0900

    add(data): 캐릭터 클래스 선택 시 어떤 클래스 인지 구분 하는 태그 추가

---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Character.h
@@ -0,0 +1,15 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace NSGameplayTags
+{
+	// PlayerCharacter
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Character_Player);
+	
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Character_Player_Ranger);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Character_Player_Engineer);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Character_Player_Warrior);
+}
