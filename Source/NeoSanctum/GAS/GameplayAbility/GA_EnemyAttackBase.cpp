8062336d9ca57152d3bda83ebbc10fc63dfb7d5d
ChoiJunHyeok <optimal9744@gmail.com>
2026-05-26 16:35:33 +0900

    feat(ai): 몬스터 GA에서 AnimMontage 실행

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_EnemyAttackBase.cpp
@@ -0,0 +1,82 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "GA_EnemyAttackBase.h"
+
+#include "AbilitySystemComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+
+UGA_EnemyAttackBase::UGA_EnemyAttackBase()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UGA_EnemyAttackBase::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                          const FGameplayAbilityActorInfo* ActorInfo,
+                                          const FGameplayAbilityActivationInfo ActivationInfo,
+                                          const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+		return;
+	}
+
+	if (!AttackMontage)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+		return;
+	}
+
+	UAbilityTask_WaitGameplayEvent* EventTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this, HitCheckEventTag, nullptr, false, false);
+
+	if (EventTask)
+	{
+		EventTask->EventReceived.AddDynamic(this, &UGA_EnemyAttackBase::OnHitCheckEventReceived);
+		EventTask->ReadyForActivation();
+	}
+
+	UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+		this,
+		TEXT("AttackMontageTask"),
+		AttackMontage,
+		1.0f,
+		NAME_None,
+		false,
+		1.0f,
+		0.0f);
+
+	if (MontageTask)
+	{
+		MontageTask->OnCompleted.AddDynamic(this, &UGA_EnemyAttackBase::OnMontageCompleted);
+		MontageTask->OnInterrupted.AddDynamic(this, &UGA_EnemyAttackBase::OnMontageInterrupted);
+		MontageTask->OnCancelled.AddDynamic(this, &UGA_EnemyAttackBase::OnMontageInterrupted);
+
+		MontageTask->ReadyForActivation();
+	}
+}
+
+void UGA_EnemyAttackBase::OnMontageCompleted()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UGA_EnemyAttackBase::OnMontageInterrupted()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+void UGA_EnemyAttackBase::OnHitCheckEventReceived(FGameplayEventData Payload)
+{
+	UE_LOG(LogTemp, Log, TEXT("몽타주 노티파이 실행"));
+
+	// TODO: 데미지 부여
+	if (GameplayCueTag.IsValid())
+	{
+		GetAbilitySystemComponentFromActorInfo()->ExecuteGameplayCue(GameplayCueTag, FGameplayCueParameters());
+	}
+}
---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_EnemyAttackBase.h
@@ -0,0 +1,49 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_EnemyAttackBase.generated.h"
+
+/**
+ * 모든 적 AI 공격 어빌리티의 최상위 부모 C++ 클래스
+ * 멀티플레이어 애니메이션 동기화 및 BT 제어권 연동 흐름을 통제합니다.
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_EnemyAttackBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UGA_EnemyAttackBase();
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	UFUNCTION()
+	void OnMontageCompleted();
+
+	UFUNCTION()
+	void OnMontageInterrupted();
+
+	UFUNCTION()
+	void OnHitCheckEventReceived(FGameplayEventData Payload);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Attack|Assets")
+	TObjectPtr<UAnimMontage> AttackMontage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Attack|Assets")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Attack|Assets")
+	FGameplayTag GameplayCueTag;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Attack|Config")
+	FGameplayTag HitCheckEventTag;
+};
