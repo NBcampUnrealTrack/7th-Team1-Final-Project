bc1c256c5036a718ccb8304385e308ed0078ae77
JRoLee <cnsfw123@gmail.com>
2026-05-26 20:34:36 +0900

    add(level) : 파괴가능 오브젝트 베이스 클래스 추가
    파괴가능 오브젝트 베이스 클래스 추가

---
+++ Source/NeoSanctum/Interaction/Prop/NSDestructibleObjectBase.cpp
@@ -0,0 +1,78 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSDestructibleObjectBase.h"
+#include "AbilitySystemComponent.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
+#include "NeoSanctum/GAS/AttributeSet/NSDestructibleAttributeSet.h"
+
+
+ANSDestructibleObjectBase::ANSDestructibleObjectBase()
+{
+	ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	ASC->SetIsReplicated(true);
+	ASC->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+	DestructibleAttrSet = CreateDefaultSubobject<UNSDestructibleAttributeSet>(TEXT("DestructibleAttributeSet"));
+
+	GCComp = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GeometryCollection"));
+	RootComponent = GCComp;
+}
+
+void ANSDestructibleObjectBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	LastHitLocation = GetActorLocation();
+
+	ASC->InitAbilityActorInfo(this, this);
+	DestructibleAttrSet->InitHealth(MaxHealth);
+	DestructibleAttrSet->InitMaxHealth(MaxHealth);
+
+	ASC->GetGameplayAttributeValueChangeDelegate(
+		UNSDestructibleAttributeSet::GetHealthAttribute())
+		.AddUObject(this, &ANSDestructibleObjectBase::OnHealthChanged);
+
+	ASC->OnGameplayEffectAppliedDelegateToSelf.AddUObject(
+		this, &ANSDestructibleObjectBase::OnGEApplied);
+}
+
+UAbilitySystemComponent* ANSDestructibleObjectBase::GetAbilitySystemComponent() const
+{
+	return ASC;
+}
+
+void ANSDestructibleObjectBase::TriggerDestruction()
+{
+	if (!HasAuthority()) return;
+
+	Multicast_PlayDestruction(LastHitLocation);
+}
+
+void ANSDestructibleObjectBase::Multicast_PlayDestruction_Implementation(FVector HitLocation)
+{
+	GCComp->SetCollisionProfileName(TEXT("DestructedFragment"));
+
+	GCComp->SetSimulatePhysics(true);
+
+	FVector ImpulseDir = (GetActorLocation() - HitLocation).GetSafeNormal();
+	GCComp->AddImpulseAtLocation(ImpulseDir * ImpulseStrength, HitLocation);
+}
+
+void ANSDestructibleObjectBase::OnHealthChanged(const FOnAttributeChangeData& Data)
+{
+	if (!bIsDestroyed && Data.NewValue <= 0.f)
+	{
+		bIsDestroyed = true;
+		TriggerDestruction();
+	}
+}
+
+void ANSDestructibleObjectBase::OnGEApplied(UAbilitySystemComponent* Source, const FGameplayEffectSpec& Spec,
+	FActiveGameplayEffectHandle Handle)
+{
+	if (const FHitResult* Hit = Spec.GetContext().GetHitResult())
+	{
+		LastHitLocation = Hit->ImpactPoint;
+	}
+}
---
+++ Source/NeoSanctum/Interaction/Prop/NSDestructibleObjectBase.h
@@ -0,0 +1,60 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
+#include "NSDestructibleObjectBase.generated.h"
+
+class UAbilitySystemComponent;
+class UNSDestructibleAttributeSet;
+class UGeometryCollectionComponent;
+
+/*
+ * 파괴가능 오브젝트 베이스 클래스
+ * 어트리뷰트 셋에 데미지를 받아 체력이 0이 될 경우 카오스 디스트럭션으로 파괴
+ * 마지막 데미지를 받은 위치를 저장하여 해당 위치에서 파괴 시작
+ * 서버에서 실행하여 클라이언트에 복제되는 형식
+ **/
+UCLASS()
+class NEOSANCTUM_API ANSDestructibleObjectBase : public AActor , public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ANSDestructibleObjectBase();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void TriggerDestruction();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_PlayDestruction(FVector HitLocation);
+
+	UPROPERTY(VisibleAnywhere, Category="GAS")
+	UAbilitySystemComponent* ASC;
+
+	UPROPERTY(VisibleAnywhere, Category="GAS")
+	UNSDestructibleAttributeSet* DestructibleAttrSet;
+
+	UPROPERTY(VisibleAnywhere, Category="Destructible")
+	UGeometryCollectionComponent* GCComp;
+
+	UPROPERTY(EditAnywhere, Category="Destructible")
+	float MaxHealth = 100.f;
+
+	UPROPERTY(EditAnywhere, Category="Destructible")
+	float ImpulseStrength = 300000.f;
+
+private:
+	FVector LastHitLocation;
+	bool bIsDestroyed = false;
+
+	void OnHealthChanged(const FOnAttributeChangeData& Data);
+	void OnGEApplied(UAbilitySystemComponent* Source, const FGameplayEffectSpec& Spec, FActiveGameplayEffectHandle Handle);
+
+};
