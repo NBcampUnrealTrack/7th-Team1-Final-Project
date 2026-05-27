c899687de925a6b3a723e1c5b030a95ae6852a95
JRoLee <cnsfw123@gmail.com>
2026-05-27 12:38:00 +0900

    feat(level) : 데미지 처리를 위해 어트리뷰트 셋 상속구조 변경
    데미지 처리를 위해 어트리뷰트 셋 상속구조 변경

--- Source/NeoSanctum/GAS/AttributeSet/NSDestructibleAttributeSet.h
+++ Source/NeoSanctum/GAS/AttributeSet/NSDestructibleAttributeSet.h
@@ -3,44 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AttributeSet.h"
-#include "AbilitySystemComponent.h"
+#include "NSBaseAttributeSet.h"
 #include "NSDestructibleAttributeSet.generated.h"
-
-#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)           \
-	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
-	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)               \
-	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)               \
-	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  *  Destructible Object를 위한 AttributeSet
  */
 UCLASS()
-class NEOSANCTUM_API UNSDestructibleAttributeSet : public UAttributeSet
+class NEOSANCTUM_API UNSDestructibleAttributeSet : public UNSBaseAttributeSet
 {
 	GENERATED_BODY()
-	
-public:
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health, Category="Destructible")
-	FGameplayAttributeData Health;
-	ATTRIBUTE_ACCESSORS(UNSDestructibleAttributeSet, Health)
-
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth, Category="Destructible")
-	FGameplayAttributeData MaxHealth;
-	ATTRIBUTE_ACCESSORS(UNSDestructibleAttributeSet, MaxHealth)
-	
-	UPROPERTY(BlueprintReadOnly, Category="Destructible")
-	FGameplayAttributeData Damage;
-	ATTRIBUTE_ACCESSORS(UNSDestructibleAttributeSet, Damage)
-
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
-
-protected:
-	UFUNCTION()
-	void OnRep_Health(const FGameplayAttributeData& OldHealth);
-
-	UFUNCTION()
-	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
 };
