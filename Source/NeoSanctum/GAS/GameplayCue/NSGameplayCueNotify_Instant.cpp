fe908fa78893c1d62bcc5868b4fbdc5169a460d7
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-29 19:03:15 +0900

    feat(gameplaycue): GameplayCueNotify_Instant 구현
    Instant Cue의 경우 즉발형이기 때문에 하나의 Sound, VFX를 1회 재생하는 간결한 방식.
    다만 재생 소켓을 지정할 수 있게함.

--- Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Instant.cpp
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Instant.cpp
@@ -3,3 +3,86 @@
 
 #include "NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Instant.h"
 
+#include "Components/SkeletalMeshComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NeoSanctum/Core/GameInstance/Subsystem/NSSoundSubsystem.h"
+
+bool UNSGameplayCueNotify_Instant::OnExecute_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters
+) const
+{
+	Super::OnExecute_Implementation(MyTarget, Parameters);
+	
+	PlayAttachedSound(MyTarget, ExecuteSoundID, SoundAttachSocketName);
+	SpawnAttachedVFX(MyTarget, ExecuteVFX, VFXAttachSocketName);
+	
+	return true;
+}
+
+USceneComponent* UNSGameplayCueNotify_Instant::GetAttachComponent(AActor* MyTarget, FName SocketName) const
+{
+	if (!MyTarget)
+	{
+		return nullptr;
+	}
+	
+	if (USkeletalMeshComponent* MeshComponent = MyTarget->FindComponentByClass<USkeletalMeshComponent>())
+	{
+		if (SocketName.IsNone() || MeshComponent->DoesSocketExist(SocketName))
+		{
+			return MeshComponent;
+		}
+	}
+	
+	return MyTarget->GetRootComponent();
+}
+
+UAudioComponent* UNSGameplayCueNotify_Instant::PlayAttachedSound(
+	AActor* MyTarget,
+	FName SoundID,
+	FName SocketName
+) const
+{
+	if (SoundID.IsNone())
+	{
+		return nullptr;
+	}
+	
+	UNSSoundSubsystem* SoundSubsystem = UNSSoundSubsystem::Get(MyTarget);
+	USceneComponent* AttachComponent = GetAttachComponent(MyTarget, SocketName);
+	if (!SoundSubsystem || !AttachComponent)
+	{
+		return nullptr;
+	}
+	
+	return SoundSubsystem->PlaySoundAttached(SoundID, AttachComponent, SocketName);
+}
+
+void UNSGameplayCueNotify_Instant::SpawnAttachedVFX(
+	AActor* MyTarget,
+	UNiagaraSystem* NiagaraSystem,
+	FName SocketName
+) const
+{
+	if (!NiagaraSystem)
+	{
+		return;
+	}
+	
+	USceneComponent* AttachComponent = GetAttachComponent(MyTarget, SocketName);
+	if (!AttachComponent)
+	{
+		return;
+	}
+	
+	UNiagaraFunctionLibrary::SpawnSystemAttached(
+		NiagaraSystem,
+		AttachComponent,
+		SocketName,
+		FVector::ZeroVector,
+		FRotator::ZeroRotator,
+		EAttachLocation::SnapToTarget,
+		true
+	);
+}
--- Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Instant.h
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Instant.h
@@ -6,12 +6,47 @@
 #include "GameplayCueNotify_Static.h"
 #include "NSGameplayCueNotify_Instant.generated.h"
 
+class UNiagaraSystem;
+class UAudioComponent;
+class USceneComponent;
+
 /**
- * 
+ * 즉발형 GameplayCue 연출
+ * 사운드는 NSSoundSubsystem의 SoundID 기반으로 일단 PlaySoundAttached만 사용
  */
-UCLASS()
+UCLASS(Blueprintable)
 class NEOSANCTUM_API UNSGameplayCueNotify_Instant : public UGameplayCueNotify_Static
 {
 	GENERATED_BODY()
+
+public:
+	virtual bool OnExecute_Implementation(
+		AActor* MyTarget,
+		const FGameplayCueParameters& Parameters
+	) const override;
 	
+protected:
+	USceneComponent* GetAttachComponent(AActor* MyTarget, FName SocketName) const;
+	
+	// 사운드 재생
+	UAudioComponent* PlayAttachedSound(AActor* MyTarget, FName SoundID, FName SocketName) const;
+	// VFX 재생
+	void SpawnAttachedVFX(AActor* MyTarget, UNiagaraSystem* NiagaraSystem, FName SocketName) const;
+	
+protected:
+	// 사운드 ID : DataTable상의 RowName
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|Sound")
+	FName ExecuteSoundID = NAME_None;
+
+	// 사운드를 붙힐 소켓 : None이면 그냥 MeshComponent root로 고정임
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|Sound")
+	FName SoundAttachSocketName = NAME_None;
+
+	// VFX
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|VFX")
+	TObjectPtr<UNiagaraSystem> ExecuteVFX;
+
+	// VFX를 붙힐 소켓 : None이면 그냥 MeshComponent root로 고정임
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|VFX")
+	FName VFXAttachSocketName = NAME_None;
 };
