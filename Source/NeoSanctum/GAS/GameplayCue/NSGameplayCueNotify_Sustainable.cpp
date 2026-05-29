18aebf127524ac43071dfef7dec80fcb850cf2e0
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-05-29 19:01:06 +0900

    feat(gameplaycue): GameplayCueNotify_Sustainable 구현
    사실상 GameplayCueNotify_Actor의 기능 흐름을 그대로 유지하면서
    우리 프로젝트의 SoundSubsystem과 필요한 연출 설정을 커스텀할 수 있게 함
    - OnActive : Start 시점의 연출이 있다면 재생. Loop 연출이 있다면 재생 시작
    - WhileActive : Loop 연출이 있다면 재생 유지
    - OnRemove : Stop 시점의 연출이 있다면 재생하고 연출 종료

--- Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Sustainable.cpp
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Sustainable.cpp
@@ -3,3 +3,156 @@
 
 #include "NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Sustainable.h"
 
+#include "Components/AudioComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NeoSanctum/Core/GameInstance/Subsystem/NSSoundSubsystem.h"
+
+ANSGameplayCueNotify_Sustainable::ANSGameplayCueNotify_Sustainable()
+{
+	bAutoDestroyOnRemove = true;
+	bAllowMultipleOnActiveEvents = false;
+	bAllowMultipleWhileActiveEvents = false;
+}
+
+bool ANSGameplayCueNotify_Sustainable::OnActive_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters
+)
+{
+	Super::OnActive_Implementation(MyTarget, Parameters);
+	
+	PlayAttachedSound(MyTarget, StartSoundID, SoundAttachSocketName);
+	SpawnAttachedVFX(MyTarget, StartVFX, VFXAttachSocketName, true);
+	
+	LoopPresentation(MyTarget);
+	
+	return true;
+}
+
+bool ANSGameplayCueNotify_Sustainable::WhileActive_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters
+)
+{
+	Super::WhileActive_Implementation(MyTarget, Parameters);
+	
+	LoopPresentation(MyTarget);
+	
+	return true;
+}
+
+bool ANSGameplayCueNotify_Sustainable::OnRemove_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters
+)
+{
+	Super::OnRemove_Implementation(MyTarget, Parameters);
+	
+	if (LoopAudioComponent)
+	{
+		if (UNSSoundSubsystem* SoundSubsystem = UNSSoundSubsystem::Get(MyTarget))
+		{
+			SoundSubsystem->StopSound(LoopAudioComponent, LoopSoundFadeOutTime);
+		}
+		else
+		{
+			LoopAudioComponent->Stop();
+		}
+		
+		LoopAudioComponent = nullptr;
+	}
+	
+	if (LoopVFXComponent)
+	{
+		LoopVFXComponent->Deactivate();
+		LoopVFXComponent = nullptr;
+	}
+	
+	SpawnAttachedVFX(MyTarget, EndVFX, VFXAttachSocketName, true);
+	PlayAttachedSound(MyTarget, EndSoundID, SoundAttachSocketName);
+	
+	return true;
+}
+
+void ANSGameplayCueNotify_Sustainable::LoopPresentation(AActor* MyTarget)
+{
+	if (!LoopAudioComponent)
+	{
+		LoopAudioComponent = PlayAttachedSound(MyTarget, LoopSoundID, SoundAttachSocketName);
+	}
+	
+	if (!LoopVFXComponent)
+	{
+		LoopVFXComponent = SpawnAttachedVFX(MyTarget, LoopVFX, VFXAttachSocketName, false);
+	}
+}
+
+USceneComponent* ANSGameplayCueNotify_Sustainable::GetAttachComponent(AActor* MyTarget, FName SocketName) const
+{
+	if (!MyTarget)
+	{
+		return nullptr;
+	}
+	
+	if (USkeletalMeshComponent* MeshComponent = MyTarget->FindComponentByClass<USkeletalMeshComponent>())
+	{
+		if (SocketName.IsNone() || MeshComponent->DoesSocketExist(SocketName))
+		{
+			return MeshComponent;
+		}
+	}
+	
+	return MyTarget->GetRootComponent();
+}
+
+UAudioComponent* ANSGameplayCueNotify_Sustainable::PlayAttachedSound(
+	AActor* MyTarget,
+	FName SoundID,
+	FName SocketName
+) const
+{
+	if (SoundID.IsNone())
+	{
+		return nullptr;
+	}
+	
+	UNSSoundSubsystem* SoundSubsystem = UNSSoundSubsystem::Get(MyTarget);
+	USceneComponent* AttachComponent = GetAttachComponent(MyTarget, SocketName);
+	if (!SoundSubsystem || !AttachComponent)
+	{
+		return nullptr;
+	}
+	
+	return SoundSubsystem->PlaySoundAttached(SoundID, AttachComponent, SocketName);
+}
+
+UNiagaraComponent* ANSGameplayCueNotify_Sustainable::SpawnAttachedVFX(
+	AActor* MyTarget,
+	UNiagaraSystem* NiagaraSystem,
+	FName SocketName,
+	bool bAutoDestroy
+) const
+{
+	if (!NiagaraSystem)
+	{
+		return nullptr;
+	}
+	
+	USceneComponent* AttachComponent = GetAttachComponent(MyTarget, SocketName);
+	if (!AttachComponent)
+	{
+		return nullptr;
+	}
+	
+	return UNiagaraFunctionLibrary::SpawnSystemAttached(
+		NiagaraSystem,
+		AttachComponent,
+		SocketName,
+		FVector::ZeroVector,
+		FRotator::ZeroRotator,
+		EAttachLocation::SnapToTarget,
+		bAutoDestroy
+	);
+}
--- Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Sustainable.h
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_Sustainable.h
@@ -6,12 +6,91 @@
 #include "GameplayCueNotify_Actor.h"
 #include "NSGameplayCueNotify_Sustainable.generated.h"
 
+class UAudioComponent;
+class UNiagaraComponent;
+class UNiagaraSystem;
+class USceneComponent;
+
 /**
- * 
+ * 지속형 GameplayCue 연출
+ * OnActive에서 시작, OnRemove에서 정리
+ * 사운드는 NSSoundSubsystem의 SoundID 기반으로 일단 PlaySoundAttached만 사용
  */
-UCLASS()
+UCLASS(Blueprintable)
 class NEOSANCTUM_API ANSGameplayCueNotify_Sustainable : public AGameplayCueNotify_Actor
 {
 	GENERATED_BODY()
+
+public:
+	ANSGameplayCueNotify_Sustainable();
+
+	virtual bool OnActive_Implementation(
+		AActor* MyTarget,
+		const FGameplayCueParameters& Parameters
+	) override;
+
+	virtual bool WhileActive_Implementation(
+		AActor* MyTarget,
+		const FGameplayCueParameters& Parameters
+	) override;
+
+	virtual bool OnRemove_Implementation(
+		AActor* MyTarget,
+		const FGameplayCueParameters& Parameters
+	) override;
 	
+protected:
+	USceneComponent* GetAttachComponent(AActor* MyTarget, FName SocketName) const;
+	UAudioComponent* PlayAttachedSound(AActor* MyTarget, FName SoundID, FName SocketName) const;
+	UNiagaraComponent* SpawnAttachedVFX(
+		AActor* MyTarget,
+		UNiagaraSystem* NiagaraSystem,
+		FName SocketName,
+		bool bAutoDestroy
+	) const;
+	void LoopPresentation(AActor* MyTarget);
+
+protected:
+	// 시작 시점에 1회 재생될 사운드 ID : DataTable상의 RowName
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|Sound")
+	FName StartSoundID = NAME_None;
+
+	// 종료 시점까지 계속 재생될 사운드 ID : DataTable상의 RowName
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|Sound")
+	FName LoopSoundID = NAME_None;
+
+	// 종료 시점에 1회 재생될 사운드 ID : DataTable상의 RowName
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|Sound")
+	FName EndSoundID = NAME_None;
+
+	// 사운드를 붙힐 소켓 : None이면 그냥 MeshComponent root로 고정임
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|Sound")
+	FName SoundAttachSocketName = NAME_None;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|Sound", meta = (ClampMin = "0.0"))
+	float LoopSoundFadeOutTime = -1.f;
+	
+protected:
+	// Cue가 Add되는 시점에 1회 재생할 VFX
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|VFX")
+	TObjectPtr<UNiagaraSystem> StartVFX;
+
+	// End 시점까지 계속 재생할 루프 VFX
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|VFX")
+	TObjectPtr<UNiagaraSystem> LoopVFX;
+
+	// Cue가 End되는 시점에 1회 재생할 VFX
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|VFX")
+	TObjectPtr<UNiagaraSystem> EndVFX;
+
+	// VFX를 붙힐 소켓 : None이면 그냥 MeshComponent root로 고정임
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CustomCue|VFX")
+	FName VFXAttachSocketName = NAME_None;
+	
+protected:
+	UPROPERTY(Transient)
+	TObjectPtr<UAudioComponent> LoopAudioComponent;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UNiagaraComponent> LoopVFXComponent;
 };
