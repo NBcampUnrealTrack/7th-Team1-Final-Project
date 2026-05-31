6b59a3e1b655d7d48007c7be02ed6d8034931091
Daype <mj1527123@gmail.com>
2026-05-31 22:32:07 +0900

    add(ai) : Drone 커스텀 MoveTask 추가

---
+++ Source/NeoSanctum/AI/Companion/Task/NSBTTask_DroneMoveTo.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "NSBTTask_DroneMoveTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSBTTask_DroneMoveTo : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+};
