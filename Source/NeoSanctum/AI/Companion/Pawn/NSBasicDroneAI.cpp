5de8c061f1113ecb4291fea1ed3f421fd567b7f0
Daype <mj1527123@gmail.com>
2026-05-31 19:04:17 +0900

    refactor(ai) : 공통로직 Base 이동 리팩토링

--- Source/NeoSanctum/AI/Companion/Pawn/NSBasicDroneAI.cpp
+++ Source/NeoSanctum/AI/Companion/Pawn/NSBasicDroneAI.cpp
@@ -2,41 +2,16 @@
 
 
 #include "NSBasicDroneAI.h"
-#include "NeoSanctum/AI/Companion/Controller/DroneAI/NSDroneAIController.h"
 #include "Components/SkeletalMeshComponent.h"
-#include "Components/CapsuleComponent.h"
 
 ANSBasicDroneAI::ANSBasicDroneAI()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	
-	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
-	AIControllerClass = ANSDroneAIController::StaticClass();
-}
-
-void ANSBasicDroneAI::GetActorEyesViewPoint(FVector& Location, FRotator& Rotation) const
-{
-	Super::GetActorEyesViewPoint(Location, Rotation);
-	Rotation = GetActorRotation();
-}
-
-void ANSBasicDroneAI::SetOwnerPlayer(AActor* Actor)
-{
-	if (!IsValid(Actor)) return;
-	
-	OwnerPlayer = Actor;
 }
 
 void ANSBasicDroneAI::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	CachedAIController = Cast<ANSDroneAIController>(GetController());
-	
-	if (CachedAIController)
-	{
-		DroneAIBBComponent = CachedAIController->GetBlackboardComponent();
-	}
 }
 
 
--- Source/NeoSanctum/AI/Companion/Pawn/NSBasicDroneAI.h
+++ Source/NeoSanctum/AI/Companion/Pawn/NSBasicDroneAI.h
@@ -4,10 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.h"
-#include "NeoSanctum/AI/Companion/Controller/DroneAI/NSDroneAIController.h"
 #include "NSBasicDroneAI.generated.h"
-
-class USphereComponent;
 
 UCLASS()
 class NEOSANCTUM_API ANSBasicDroneAI : public ANSBaseCompanionAI
@@ -16,24 +13,7 @@ class NEOSANCTUM_API ANSBasicDroneAI : public ANSBaseCompanionAI
 
 public:
 	ANSBasicDroneAI();
-	
-	// AI Perceptin 시야 회전 동기화
-	virtual void GetActorEyesViewPoint( FVector& Location, FRotator& Rotation ) const override;
-	
-	// SpawnOwner 소유자 지정
-	void SetOwnerPlayer(AActor* Actor);
-	AActor* GetOwnerPlayer() { return OwnerPlayer;};
-	
+
 protected:
 	virtual void BeginPlay() override;
-
-private:
-	
-	
-	UPROPERTY(VisibleAnywhere)
-	UBlackboardComponent* DroneAIBBComponent;
-	
-public:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="OwnerPlayer")
-	TObjectPtr<AActor> OwnerPlayer;
 };
