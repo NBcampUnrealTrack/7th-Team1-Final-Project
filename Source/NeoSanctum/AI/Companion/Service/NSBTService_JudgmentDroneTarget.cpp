ef75554c9d594f311350d8691a3032ed94c11c85
Daype <mj1527123@gmail.com>
2026-05-31 21:07:47 +0900

    feat(ai) : 틱마다 가장가까운 엑터 찾아주는 기능 추가

--- Source/NeoSanctum/AI/Companion/Service/NSBTService_JudgmentDroneTarget.cpp
+++ Source/NeoSanctum/AI/Companion/Service/NSBTService_JudgmentDroneTarget.cpp
@@ -2,3 +2,107 @@
 
 
 #include "NSBTService_JudgmentDroneTarget.h"
+#include "NeoSanctum/AI/Companion/Controller/DroneAI/NSDroneAIController.h"
+#include "NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+UNSBTService_JudgmentDroneTarget::UNSBTService_JudgmentDroneTarget()
+{
+	NodeName = "Judgment Drone Target";
+	
+	Interval = 0.15f;
+	RandomDeviation = 0.02f;
+	bNotifyTick = true;
+	
+	MoveTargetKey.AddVectorFilter(
+		this, 
+		GET_MEMBER_NAME_CHECKED(UNSBTService_JudgmentDroneTarget,MoveTargetKey)
+		);
+	TargetActorKey.AddObjectFilter(
+		this, 
+		GET_MEMBER_NAME_CHECKED(UNSBTService_JudgmentDroneTarget, TargetActorKey),
+		AActor::StaticClass()
+		);
+}
+
+void UNSBTService_JudgmentDroneTarget::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+	
+	if (UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		MoveTargetKey.ResolveSelectedKey(*BBAsset);
+		TargetActorKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+void UNSBTService_JudgmentDroneTarget::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory,
+	float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent();
+	if (!AIController || !BB) return;
+	
+	ANSBaseCompanionAI* DronePawn = Cast<ANSBaseCompanionAI>(AIController->GetPawn());
+	ANSDroneAIController* DroneController = Cast<ANSDroneAIController>(AIController);
+	if (!DroneController || !DronePawn) return;
+	
+	if (AActor* Currency = FindNearestActor(DronePawn, CurrencyClass, CurrencyDetectionRadius))
+	{
+		BB->SetValueAsObject(TargetActorKey.SelectedKeyName, Currency);
+		BB->SetValueAsVector(MoveTargetKey.SelectedKeyName, Currency->GetActorLocation());
+		return;
+	}
+	
+	const FVector FollowPos = 
+		DronePawn->GetOwnerPlayer()->GetActorLocation() + 
+		DronePawn->GetOwnerPlayer()->GetActorRotation().RotateVector(FollowOffset);
+	
+	BB->ClearValue(TargetActorKey.SelectedKeyName);
+	BB->SetValueAsVector(MoveTargetKey.SelectedKeyName, FollowPos);
+}
+
+
+AActor* UNSBTService_JudgmentDroneTarget::FindNearestActor(AActor* InActor, TSubclassOf<AActor> FilterClass, float Radius) const
+{
+	if (!InActor || !FilterClass) return nullptr;
+	
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_WorldDynamic));
+	
+	TArray<AActor*> IgnoreActors;
+	IgnoreActors.Add(InActor);
+	
+	TArray<AActor*> FoundActors;
+	UKismetSystemLibrary::SphereOverlapActors(
+		InActor->GetWorld(),
+		InActor->GetActorLocation(),
+		Radius,
+		ObjectTypes,
+		FilterClass,
+		IgnoreActors,
+		FoundActors
+		);
+	
+	AActor* NearestActor = nullptr;
+	float BestDistSq = TNumericLimits<float>::Max();
+	const FVector Origin = InActor->GetActorLocation();
+	
+	for (AActor* CandiateActor : FoundActors)
+	{
+		if (!CandiateActor) continue;
+		
+		const float DistSq = FVector::DistSquared(Origin, CandiateActor->GetActorLocation());
+		if (DistSq < BestDistSq)
+		{
+			BestDistSq = DistSq;
+			NearestActor = CandiateActor;
+		}
+	}
+	
+	return NearestActor;
+}
--- Source/NeoSanctum/AI/Companion/Service/NSBTService_JudgmentDroneTarget.h
+++ Source/NeoSanctum/AI/Companion/Service/NSBTService_JudgmentDroneTarget.h
@@ -6,11 +6,49 @@
 #include "BehaviorTree/BTService.h"
 #include "NSBTService_JudgmentDroneTarget.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class NEOSANCTUM_API UNSBTService_JudgmentDroneTarget : public UBTService
 {
 	GENERATED_BODY()
+public:
+	UNSBTService_JudgmentDroneTarget();
+	
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
+protected:
+	// @민재 : 블랙보드 키 변수 에디터 할당
+	UPROPERTY(EditAnywhere, Category="Blackboard")
+	FBlackboardKeySelector MoveTargetKey;
+	
+	UPROPERTY(EditAnywhere, Category="Blackboard")
+	FBlackboardKeySelector TargetActorKey;
+	
+	// @민재 : 재화 관련 변수
+	UPROPERTY(EditAnywhere, Category = "DroneAI|Currency")
+	TSubclassOf<AActor> CurrencyClass;
+	
+	UPROPERTY(EditAnywhere, Category="DroneAI|Currency")
+	float CurrencyDetectionRadius = 1500.f;
+	
+	// @민재 : 전투 관련 변수
+	UPROPERTY(EditAnywhere, Category = "DroneAI|Combat")
+	TSubclassOf<AActor> EnemyClass;
+	
+	UPROPERTY(EditAnywhere, Category="DroneAI|Combat")
+	float CombatDetectionRadius = 2000.f;
+	
+	UPROPERTY(EditAnywhere, Category="DroneAI|Combat")
+	float EnemyDistance = 350.f;
+	
+	// @민재 : 오너 거리
+	UPROPERTY(EditAnywhere, Category="DroneAI|Follow")
+	FVector FollowOffset = FVector(-100.f, 0.f, 100.f);
+	
+private:
+	// @민재 : 감지범위구체
+	AActor* FindNearestActor(AActor* InActor, TSubclassOf<AActor> FilterClass, float Radius) const;
+	
 };
