368a053933f92f154c325b05234fa1a504bb914c
Daype <mj1527123@gmail.com>
2026-05-31 22:32:40 +0900

    feat(ai) : Task Move 기능구현

--- Source/NeoSanctum/AI/Companion/Task/NSBTTask_DroneMoveTo.cpp
+++ Source/NeoSanctum/AI/Companion/Task/NSBTTask_DroneMoveTo.cpp
@@ -2,3 +2,65 @@
 
 
 #include "NSBTTask_DroneMoveTo.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.h"
+
+UNSBTTask_DroneMoveTo::UNSBTTask_DroneMoveTo()
+{
+	NodeName = TEXT("Drone Move To");
+	bNotifyTick = true;
+	
+	MoveTargetKey.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UNSBTTask_DroneMoveTo, MoveTargetKey));
+}
+
+void UNSBTTask_DroneMoveTo::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+	
+	if (UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		MoveTargetKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+EBTNodeResult::Type UNSBTTask_DroneMoveTo::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	ANSBaseCompanionAI* DroneAI = AIController ? Cast<ANSBaseCompanionAI>(AIController->GetPawn()) : nullptr;
+	if (!DroneAI)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	return EBTNodeResult::InProgress;
+}
+
+void UNSBTTask_DroneMoveTo::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	ANSBaseCompanionAI* DroneAI = AIController ? Cast<ANSBaseCompanionAI>(AIController->GetPawn()) : nullptr;
+	if (!BBComp || !DroneAI)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+		return;
+	}
+	
+	const FVector MoveTarget = BBComp->GetValueAsVector(MoveTargetKey.SelectedKeyName);
+	
+	DroneAI->MoveTowards(MoveTarget);
+	
+	if (bFinishOnArrival)
+	{
+		const FVector Delta = MoveTarget - DroneAI->GetActorLocation();
+		const float HorizontalDistSq = FVector(Delta.X, Delta.Y, 0.0f).SizeSquared();
+		if (HorizontalDistSq <= FMath::Square(AcceptanceRadius))
+		{
+			FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		}
+	}
+}
+
+
--- Source/NeoSanctum/AI/Companion/Task/NSBTTask_DroneMoveTo.h
+++ Source/NeoSanctum/AI/Companion/Task/NSBTTask_DroneMoveTo.h
@@ -13,4 +13,22 @@ UCLASS()
 class NEOSANCTUM_API UNSBTTask_DroneMoveTo : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
+public:
+	UNSBTTask_DroneMoveTo();
+	
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	
+	UPROPERTY(EditAnywhere, Category="Blackboard")
+	FBlackboardKeySelector MoveTargetKey;
+	
+	UPROPERTY(EditAnywhere, Category="DroneAI")
+	float MoveSpeedOverride = 0.f;
+	UPROPERTY(EditAnywhere, Category="DroneAI")
+	bool bFinishOnArrival = false;
+	
+	UPROPERTY(EditAnywhere, Category="DroneAI")
+	float AcceptanceRadius = 120.f;
 };
