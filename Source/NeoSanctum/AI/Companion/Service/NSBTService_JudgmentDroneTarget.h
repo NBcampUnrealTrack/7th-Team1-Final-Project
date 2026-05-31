f54b848df259403a7038464c06121eed71b0981b
Daype <mj1527123@gmail.com>
2026-05-31 19:39:15 +0900

    add(ai) : BTService 상태체크 TickNode 서비스 추가

---
+++ Source/NeoSanctum/AI/Companion/Service/NSBTService_JudgmentDroneTarget.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "NSBTService_JudgmentDroneTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSBTService_JudgmentDroneTarget : public UBTService
+{
+	GENERATED_BODY()
+};
