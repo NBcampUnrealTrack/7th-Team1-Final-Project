63fc7551a59335be682a70afbaf9a94bd4ac5727
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-01 17:19:55 +0900

    add(Engineer): GA_Engineer_SpawnTurret 어빌리티 생성

---
+++ Source/NeoSanctum/GAS/GameplayAbility/Engineer/GA_Engineer_SpawnTurret.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NeoSanctum/GAS/GameplayAbility/GA_SkillBase.h"
+#include "GA_Engineer_SpawnTurret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_Engineer_SpawnTurret : public UGA_SkillBase
+{
+	GENERATED_BODY()
+};
