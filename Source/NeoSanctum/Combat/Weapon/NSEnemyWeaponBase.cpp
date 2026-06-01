ca8c36e0ccaae7e6b57b4e3df96914e1ae8548a5
ChoiJunHyeok <optimal9744@gmail.com>
2026-06-01 15:00:45 +0900

    feat(ai): EnemyWeaponBase 추가

---
+++ Source/NeoSanctum/Combat/Weapon/NSEnemyWeaponBase.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSEnemyWeaponBase.h"
+
+
+ANSEnemyWeaponBase::ANSEnemyWeaponBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	RootComponent = WeaponMesh;
+
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
---
+++ Source/NeoSanctum/Combat/Weapon/NSEnemyWeaponBase.h
@@ -0,0 +1,47 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NSEnemyWeaponBase.generated.h"
+
+class UGameplayAbility;
+class UAnimInstance;
+class UStaticMeshComponent;
+
+USTRUCT(BlueprintType)
+struct FWeaponConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon Config")
+	TSubclassOf<UGameplayAbility> WeaponAbility;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon Config")
+	TSubclassOf<UAnimInstance> AnimBlueprintClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon Config")
+	FName EquipSocketName = TEXT("WeaponSocket_R");
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon Config")
+	FTransform RelativeTransform;
+};
+
+UCLASS()
+class NEOSANCTUM_API ANSEnemyWeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANSEnemyWeaponBase();
+
+	FORCEINLINE const FWeaponConfig& GetWeaponConfig() const { return WeaponConfig; }
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Visual")
+	TObjectPtr<UStaticMeshComponent> WeaponMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon|Data")
+	FWeaponConfig WeaponConfig;
+};
