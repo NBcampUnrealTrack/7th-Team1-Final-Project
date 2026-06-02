a9045a241e66520602c213cae2a4b20bda0e00b9
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-02 12:01:24 +0900

    refactor(gas): GA_ThrowProjectile이라는 공통 베이스부터 시작하는 방식으로 변경

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_ThrowProjectile.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_SkillBase.h"
+#include "GA_ThrowProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_ThrowProjectile : public UGA_SkillBase
+{
+	GENERATED_BODY()
+};
