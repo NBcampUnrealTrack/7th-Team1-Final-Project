04efd6cdfa740a1f64af25362905255b2169ba86
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-02 15:58:34 +0900

    add(gas): GA_ThrowProjectile 코드 뼈대 구성

--- Source/NeoSanctum/GAS/GameplayAbility/GA_ThrowProjectile.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_ThrowProjectile.cpp
@@ -2,3 +2,59 @@
 
 
 #include "GA_ThrowProjectile.h"
+
+UGA_ThrowProjectile::UGA_ThrowProjectile()
+{
+	ActivationPolicy = ENSAbilityActivationPolicy::OnInputTriggered;
+}
+
+void UGA_ThrowProjectile::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!ActorInfo || !ActorInfo->AvatarActor.IsValid())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	if (ActorInfo->IsLocallyControlled())
+	{
+		// TODO : 던지기 궤적 + 탄착 지점 프리뷰
+	}
+}
+
+void UGA_ThrowProjectile::InputReleased(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
+
+	if (ActorInfo && ActorInfo->IsLocallyControlled())
+	{
+		// TODO : 실제 던지기 = Projectile 액터를 스폰
+	}
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
+
+void UGA_ThrowProjectile::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
+{
+	// TODO : 프리뷰 종료
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
--- Source/NeoSanctum/GAS/GameplayAbility/GA_ThrowProjectile.h
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_ThrowProjectile.h
@@ -7,10 +7,37 @@
 #include "GA_ThrowProjectile.generated.h"
 
 /**
- * 
+ * Projectile을 포물선 형태로 던지기 위한 GA
+ * Input이 Hold되는 동안 궤적 + 탄착지점 프리뷰를 보여주고 
+ * Input이 Release 되는 순간 프리뷰를 제거하고 Projectile을 스폰
  */
-UCLASS()
+UCLASS(Abstract, Blueprintable)
 class NEOSANCTUM_API UGA_ThrowProjectile : public UGA_SkillBase
 {
 	GENERATED_BODY()
+
+public:
+	UGA_ThrowProjectile();
+
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void InputReleased(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
 };
