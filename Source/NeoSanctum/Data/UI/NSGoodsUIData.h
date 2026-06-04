8826919cd553fe2e089210f44ca22b68960d32e9
Youngwoong <afsd3330@gmail.com>
2026-06-04 16:22:04 +0900

    fix(ui): UI 데이터 구조 include 오류 수정

--- Source/NeoSanctum/Data/UI/NSGoodsUIData.h
+++ Source/NeoSanctum/Data/UI/NSGoodsUIData.h
@@ -3,7 +3,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataTable.h"
-#include "GamePlayTagContainer.h"
+#include "GameplayTagContainer.h"
 #include "NSGoodsUIData.generated.h"
 
 class UTexture2D;
