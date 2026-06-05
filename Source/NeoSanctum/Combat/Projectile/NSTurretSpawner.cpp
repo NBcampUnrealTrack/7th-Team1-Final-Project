f94bca7181a0385b4097fe0e0514813f5bff5f7a
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-05 15:10:00 +0900

    add(character): TurretSpawner 액터 뼈대

---
+++ Source/NeoSanctum/Combat/Projectile/NSTurretSpawner.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSTurretSpawner.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ANSTurretSpawner::ANSTurretSpawner()
+{
+	UProjectileMovementComponent* Movement = GetProjectileMovementComponent();
+	if (Movement)
+	{
+		Movement->bShouldBounce = true;
+		// 탄력
+		Movement->Bounciness = 0.55f;
+		// 마찰력
+		Movement->Friction = 0.2f;
+	}
+}
+
+void ANSTurretSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UProjectileMovementComponent* Movement = GetProjectileMovementComponent();
+	if (Movement)
+	{
+		Movement->OnProjectileBounce.AddDynamic(this, &ThisClass::OnProjectileBounce)
+	}
+}
+
+void ANSTurretSpawner::OnProjectileBounce(const FHitResult& ImpactResult, const FVector& ImpactVelocity)
+{
+	// TODO : Projectile이 바운스 되는 순간 경사면의 Z축 경사 정도를 파악해서 Turret Spawn
+}
---
+++ Source/NeoSanctum/Combat/Projectile/NSTurretSpawner.h
@@ -0,0 +1,27 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NSThrowProjectileBase.h"
+#include "NSTurretSpawner.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSTurretSpawner : public ANSThrowProjectileBase
+{
+	GENERATED_BODY()
+
+public:
+	ANSTurretSpawner();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UFUNCTION()
+	void OnProjectileBounce(const FHitResult& ImpactResult, const FVector& ImpactVelocity);
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "TurretSpawner")
+	TSubclassOf<AActor> TurretClass;
+};
