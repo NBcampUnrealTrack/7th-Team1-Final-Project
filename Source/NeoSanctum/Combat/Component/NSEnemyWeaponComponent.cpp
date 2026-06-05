95fe9354fd7c67011436e9169ad66b3d1c3bad81
YongHo <skymo6872@gmail.com>
2026-06-05 17:09:26 +0900

    feat(ai): 적 재사용 시 무기가 이미 있으면 없애는 로직 추가
    적 캐릭터를 풀링 후 재사용할 때 CurrentWeapon이 이미 있으면 없애고 다시 만들고 붙이도록 로직 추가

--- Source/NeoSanctum/Combat/Component/NSEnemyWeaponComponent.cpp
+++ Source/NeoSanctum/Combat/Component/NSEnemyWeaponComponent.cpp
@@ -31,6 +31,13 @@ void UNSEnemyWeaponComponent::EquipWeapon()
 	ANSEnemyCharacterBase* Owner = Cast<ANSEnemyCharacterBase>(GetOwner());
 	if (!Owner || !Owner->HasAuthority()) return;
 
+	// 재장착할 때 대비용
+	if (CurrentWeapon)
+	{
+		CurrentWeapon->Destroy();
+		CurrentWeapon = nullptr;
+	}
+	
 	UNSEnemyData* EnemyData = Owner->GetEnemyData();
 	if (!EnemyData || !EnemyData->DefaultWeaponClass) return;
 
