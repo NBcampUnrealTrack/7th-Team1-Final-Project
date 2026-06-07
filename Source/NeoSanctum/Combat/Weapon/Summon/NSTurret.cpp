fa9c45c9810059053dce4325906ae5edb8c9fdfa
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-08 08:12:08 +0900

    add(gas): GA_NS_Engineer_SpawnTurret을 통해 스폰할 Turret 액터

---
+++ Source/NeoSanctum/Combat/Weapon/Summon/NSTurret.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSTurret.h"
+
+ANSTurret::ANSTurret()
+{
+}
+
+void ANSTurret::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/NeoSanctum/Combat/Weapon/Summon/NSTurret.h
@@ -0,0 +1,19 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NSTurret.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSTurret : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANSTurret();
+
+protected:
+	virtual void BeginPlay() override;
+};
