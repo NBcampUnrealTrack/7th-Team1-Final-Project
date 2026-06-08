63b3eb6242c1d5bc99b08133b6c00c0f88ace710
Daype <mj1527123@gmail.com>
2026-06-08 16:50:53 +0900

    add(ai) : Companion GAS 세팅

---
+++ Source/NeoSanctum/GAS/AttributeSet/NSCompanionAttributeSet.cpp
@@ -0,0 +1,30 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSCompanionAttributeSet.h"
+
+void UNSCompanionAttributeSet::OnRep_AttackDamage(const FGameplayAttributeData& OldAttackDamage)
+{
+}
+
+void UNSCompanionAttributeSet::OnRep_FireRate(const FGameplayAttributeData& OldFireRate)
+{
+}
+
+void UNSCompanionAttributeSet::OnRep_ProjectileSpeed(const FGameplayAttributeData& OldProjectileSpeed)
+{
+}
+
+void UNSCompanionAttributeSet::OnRep_AttackRange(const FGameplayAttributeData& OldAttackRange)
+{
+}
+
+void UNSCompanionAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
+
+void UNSCompanionAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+}
---
+++ Source/NeoSanctum/GAS/AttributeSet/NSCompanionAttributeSet.h
@@ -0,0 +1,57 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "NSCompanionAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSCompanionAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_AttackDamage , Category="GAS|DroneAttributes")
+	FGameplayAttributeData AttackDamage;
+	ATTRIBUTE_ACCESSORS(UNSCompanionAttributeSet, AttackDamage);
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_FireRate, Category="GAS|DroneAttributes")
+	FGameplayAttributeData FireRate;
+	ATTRIBUTE_ACCESSORS(UNSCompanionAttributeSet, FireRate);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_ProjectileSpeed, Category="GAS|DroneAttributes")
+	FGameplayAttributeData ProjectileSpeed;
+	ATTRIBUTE_ACCESSORS(UNSCompanionAttributeSet, ProjectileSpeed);
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_AttackRange, Category="GAS|DroneAttributes")
+	FGameplayAttributeData AttackRange;
+	ATTRIBUTE_ACCESSORS(UNSCompanionAttributeSet, AttackRange);
+	
+protected:
+	UFUNCTION()
+	void OnRep_AttackDamage(const FGameplayAttributeData& OldAttackDamage);
+	
+	UFUNCTION()
+	void OnRep_FireRate(const FGameplayAttributeData& OldFireRate);
+	
+	UFUNCTION()
+	void OnRep_ProjectileSpeed(const FGameplayAttributeData& OldProjectileSpeed);
+	
+	UFUNCTION()
+	void OnRep_AttackRange(const FGameplayAttributeData& OldAttackRange);
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+};
