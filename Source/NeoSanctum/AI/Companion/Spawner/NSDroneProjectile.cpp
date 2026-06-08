d3b6e17efa15ea494ea7f9a42e40c8f11c468590
Daype <mj1527123@gmail.com>
2026-06-08 16:55:07 +0900

    add(ai) : Drone 발사체 추가

---
+++ Source/NeoSanctum/AI/Companion/Spawner/NSDroneProjectile.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSDroneProjectile.h"
+
+
+// Sets default values
+ANSDroneProjectile::ANSDroneProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSDroneProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSDroneProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/NeoSanctum/AI/Companion/Spawner/NSDroneProjectile.h
@@ -0,0 +1,22 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NSDroneProjectile.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSDroneProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANSDroneProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
