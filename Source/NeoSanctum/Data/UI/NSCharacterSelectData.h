5a0a0873c79d80e0547e7fca26f30c2cbdbc7468
Youngwoong <afsd3330@gmail.com>
2026-06-09 14:51:43 +0900

    add(data): FNSCharacterSelectData 구조체 추가

---
+++ Source/NeoSanctum/Data/UI/NSCharacterSelectData.h
@@ -0,0 +1,27 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "NeoSanctum/Data/UI/NSCharacterSelectData.h"
+#include "NeoSanctum/Data/Character/NSCharacterData.h"
+#include "Engine/DataTable.h"
+#include "NSCharacterSelectData.generated.h"
+
+class UNSCharacterData;
+
+USTRUCT(BlueprintType)
+struct FNSCharacterSelectData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	//캐릭터 이름
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CharacterSelect")
+	FText CharacterName;
+	//캐릭터 설명
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CharacterSelect")
+	FText CharacterDescription;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CharacterSelect")
+	TSoftObjectPtr<UNSCharacterData> CharacterData;
+};
+
