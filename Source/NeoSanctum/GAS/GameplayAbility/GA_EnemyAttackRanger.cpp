3c194ab905347fcb529b8bbb8895fe3adbcd2142
ChoiJunHyeok <optimal9744@gmail.com>
2026-06-09 16:30:31 +0900

    feat(ai): 원거리 몬스터 공격 GA 추가

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_EnemyAttackRanger.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "GA_EnemyAttackRanger.h"
+
+#include "NeoSanctum/Tag/NSGameplayTags_Enemy.h"
+#include "NeoSanctum/Tag/NSGameplayTags_State.h"
+
+UGA_EnemyAttackRanger::UGA_EnemyAttackRanger()
+{
+	// Tags 세팅 설정
+	FGameplayTagContainer AssetTags = GetAssetTags();
+	AssetTags.AddTag(NSGameplayTags::Ability_Enemy_RangerAttack);
+	SetAssetTags(AssetTags);
+
+	ActivationOwnedTags.AddTag(NSGameplayTags::State_Enemy_Combat);
+	ActivationBlockedTags.AddTag(NSGameplayTags::State_Dead);
+}
---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_EnemyAttackRanger.h
@@ -0,0 +1,19 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_EnemyAttackBase.h"
+#include "GA_EnemyAttackRanger.generated.h"
+
+/**
+ * 몬스터의 원거리 공격 처리
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_EnemyAttackRanger : public UGA_EnemyAttackBase
+{
+	GENERATED_BODY()
+
+public:
+	UGA_EnemyAttackRanger();
+};
