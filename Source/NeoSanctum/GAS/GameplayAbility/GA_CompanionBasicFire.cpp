913e430a463b6345880201edf952670fd4f3eb62
Daype <mj1527123@gmail.com>
2026-06-09 15:07:55 +0900

    feat(GAS) : GA_Companion_Basic_Fire 드론 기초공격 생성자 세팅

--- Source/NeoSanctum/GAS/GameplayAbility/GA_CompanionBasicFire.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_CompanionBasicFire.cpp
@@ -2,3 +2,31 @@
 
 
 #include "GA_CompanionBasicFire.h"
+
+#include "NeoSanctum/Tag/NSGameplayTags_Ability.h"
+#include "NeoSanctum/Tag/NSGameplayTags_Companion.h"
+
+UGA_CompanionBasicFire::UGA_CompanionBasicFire()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerOnly;
+	
+	AbilityTags.AddTag(NSGameplayTags::Ability_Companion_Fire);
+	ActivationBlockedTags.AddTag(NSGameplayTags::State_Companion_Disable);
+	
+	DamageSetTag = NSGameplayTags::Data_Companion_Damage;
+	CoolDownTag = NSGameplayTags::Data_Companion_CoolDown;
+}
+
+void UGA_CompanionBasicFire::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGA_CompanionBasicFire::ApplyCooldown(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+{
+	Super::ApplyCooldown(Handle, ActorInfo, ActivationInfo);
+}
