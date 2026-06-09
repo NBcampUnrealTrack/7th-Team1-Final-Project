0e1b6f9c31dece9b9070b6b7704f9c35a74aadfa
Daype <mj1527123@gmail.com>
2026-06-09 15:07:00 +0900

    add(GAS) : Companion GAS 태그 추가
    어빌리티 , 상태, 공격데이터

--- Source/NeoSanctum/Tag/NSGameplayTags_Companion.h
+++ Source/NeoSanctum/Tag/NSGameplayTags_Companion.h
@@ -1,7 +1,17 @@
 ﻿#pragma once
 
-class NSGameplayTags_Companion
+#include "NativeGameplayTags.h"
+
+namespace NSGameplayTags
 {
-public:
+	// 어빌리티 태그
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Companion_Fire);
 	
-};
+	// 스테이트 태그
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Companion_Disable);
+	
+	// 데이터 태그
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Companion_Damage);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Companion_CoolDown);
+	
+}
