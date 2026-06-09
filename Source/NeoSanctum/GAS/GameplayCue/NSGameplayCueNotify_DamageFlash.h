a4f1edda3c46e66e35fd28fe6ca5dc4cfcf57c67
JRoLee <cnsfw123@gmail.com>
2026-06-09 21:29:38 +0900

    add(fx) : DamageFlash를 재생 다리 역할을 할 GamePlayCueNotify 클래스 생성 및 구현
    DamageFlash를 재생 다리 역할을 할 GamePlayCueNotify 클래스 생성 및 구현

---
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_DamageFlash.h
@@ -0,0 +1,21 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Static.h"
+#include "NSGameplayCueNotify_DamageFlash.generated.h"
+
+/**
+ * 데미지 플래시 GameplayCue.
+ * MyTarget의 UNSDamageFlashComponent를 찾아 PlayFlash() 호출.
+ * 상태는 컴포넌트가 보유하므로 이 클래스는 무상태(Static).
+ */
+UCLASS()
+class NEOSANCTUM_API UNSGameplayCueNotify_DamageFlash : public UGameplayCueNotify_Static
+{
+	GENERATED_BODY()
+	
+public:
+	virtual bool OnExecute_Implementation(AActor* MyTarget,	const FGameplayCueParameters& Parameters) const override;
+};
