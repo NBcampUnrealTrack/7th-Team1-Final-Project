4f0920b200e7fa32e8537f878197428418588f74
Daype <mj1527123@gmail.com>
2026-06-09 17:27:14 +0900

    feat(GAS) : Companion GA_BasicFire GetCombatTarget() 애너미 타겟 반환 함수 기능 구현

--- Source/NeoSanctum/GAS/GameplayAbility/GA_CompanionBasicFire.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_CompanionBasicFire.cpp
@@ -3,9 +3,10 @@
 
 #include "GA_CompanionBasicFire.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
 #include "NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.h"
 #include "NeoSanctum/GAS/AttributeSet/NSCompanionAttributeSet.h"
-#include "NeoSanctum/Tag/NSGameplayTags_Ability.h"
+#include "NeoSanctum/AI/Companion/Controller/DroneAI/NSDroneAIController.h"
 #include "NeoSanctum/Tag/NSGameplayTags_Companion.h"
 
 UGA_CompanionBasicFire::UGA_CompanionBasicFire()
@@ -32,10 +33,6 @@ void UGA_CompanionBasicFire::ActivateAbility(const FGameplayAbilitySpecHandle Ha
 		return;
 	}
 	
-	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
-	{
-		
-	}
 	
 }
 
@@ -43,6 +40,28 @@ void UGA_CompanionBasicFire::ApplyCooldown(const FGameplayAbilitySpecHandle Hand
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
 {
 	Super::ApplyCooldown(Handle, ActorInfo, ActivationInfo);
+}
+
+const UNSCompanionAttributeSet* UGA_CompanionBasicFire::GetCompanionSet() const
+{
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	if (!ASC) return nullptr;
+	
+	return ASC->GetSet<UNSCompanionAttributeSet>();
+}
+
+AActor* UGA_CompanionBasicFire::GetCombatTarget() const
+{
+	ANSBaseCompanionAI* DronePawn = Cast<ANSBaseCompanionAI>(GetAvatarActorFromActorInfo());
+	if (!DronePawn) return nullptr;
+	
+	ANSDroneAIController* DroneAIController = Cast<ANSDroneAIController>(DronePawn->GetController());
+	if (!DroneAIController) return nullptr;
+	
+	UBlackboardComponent* BB = DroneAIController->GetBlackboardComponent();
+	if (!BB) return nullptr;
+	
+	return Cast<AActor>(BB->GetValueAsObject(CombatTargetKeyName));
 }
 
 bool UGA_CompanionBasicFire::CanFireAt(AActor* Target, FVector& OutMuzzle, FVector& OutDir) const
