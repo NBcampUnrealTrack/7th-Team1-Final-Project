d9e355e6e769d834faf46c0f21394d59a49f0e14
Daype <mj1527123@gmail.com>
2026-06-10 12:56:02 +0900

    feat(GAS) : Companion GA_BasicFire CoolDown오버라이드 기능, Active어빌리티 , FireProjectile구현

--- Source/NeoSanctum/GAS/GameplayAbility/GA_CompanionBasicFire.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_CompanionBasicFire.cpp
@@ -7,6 +7,7 @@
 #include "NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.h"
 #include "NeoSanctum/GAS/AttributeSet/NSCompanionAttributeSet.h"
 #include "NeoSanctum/AI/Companion/Controller/DroneAI/NSDroneAIController.h"
+#include "NeoSanctum/AI/Companion/Spawner/NSDroneProjectile.h"
 #include "NeoSanctum/Tag/NSGameplayTags_Companion.h"
 
 UGA_CompanionBasicFire::UGA_CompanionBasicFire()
@@ -27,19 +28,65 @@ void UGA_CompanionBasicFire::ActivateAbility(const FGameplayAbilitySpecHandle Ha
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 	
-	if (!IsValid(GetCombatTarget()))
+	AActor* Target = GetCombatTarget();
+	
+	if (!IsValid(Target))
 	{
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 		return;
 	}
 	
+	ANSBaseCompanionAI* AvatarActor = Cast<ANSBaseCompanionAI>(ActorInfo->AvatarActor.Get());
+	if (!AvatarActor)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;	
+	}
+	
+	USkeletalMeshComponent* SkeletalMesh = AvatarActor->GetSkeletalMeshComponent();
+	if (!SkeletalMesh)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	FVector MuzzleSocket;
+	FVector OutDir;
+	
+	if (!CanFireAt(Target, MuzzleSocket, OutDir))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	FireProjectile(MuzzleSocket, OutDir, Target);
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 	
 }
 
 void UGA_CompanionBasicFire::ApplyCooldown(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
 {
-	Super::ApplyCooldown(Handle, ActorInfo, ActivationInfo);
+	const UGameplayEffect* CoolDownEffect = GetCooldownGameplayEffect();
+	if (!CoolDownEffect) return;
+	
+	FGameplayEffectSpecHandle CoolDownSpecHandle = 
+		MakeOutgoingGameplayEffectSpec(CoolDownEffect->GetClass(), GetAbilityLevel());
+	
+	const UNSCompanionAttributeSet* Set = GetCompanionSet();
+	if (!Set) return;
+	
+	float CoolDownInterval = (1.f / Set->GetFireRate());
+	
+	CoolDownSpecHandle.Data->SetSetByCallerMagnitude(CoolDownTag, CoolDownInterval);
+	
+	ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, CoolDownSpecHandle);
 }
 
 const UNSCompanionAttributeSet* UGA_CompanionBasicFire::GetCompanionSet() const
@@ -101,4 +148,33 @@ bool UGA_CompanionBasicFire::CanFireAt(AActor* Target, FVector& OutMuzzle, FVect
 
 void UGA_CompanionBasicFire::FireProjectile(const FVector& Muzzle, const FVector& Dir, AActor* Target)
 {
+	const UNSCompanionAttributeSet* Set = GetCompanionSet();
+	if (!Set) return;
+	
+	FGameplayEffectSpecHandle DamageSpec = MakeOutgoingGameplayEffectSpec(DamageEffectClass);
+	if (!DamageSpec.IsValid()) return;
+	
+	DamageSpec.Data->SetSetByCallerMagnitude(DamageSetTag, Set->GetAttackDamage());
+	
+	const float Speed = Set->GetProjectileSpeed();
+	if (Speed <= 0.f) return;
+	
+	APawn* InstigatorPawn = Cast<APawn>(GetAvatarActorFromActorInfo());
+	
+	FTransform MuzzleTransform(Dir.Rotation(), Muzzle);
+	
+	ANSDroneProjectile* Projectile = GetWorld()->SpawnActorDeferred<ANSDroneProjectile>(
+		ProjectileClass,
+		MuzzleTransform,
+		GetAvatarActorFromActorInfo(), InstigatorPawn,
+		ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+		);
+	
+	if (Projectile)
+	{
+		Projectile->InitProjectile(Dir, InstigatorPawn, DamageSpec, Speed);
+		
+		Projectile->FinishSpawning(MuzzleTransform);
+	}
+	
 }
