49140af3512cd68bfa3611b4094be48643ab49e6
Park WonJong <kidsmon@naver.com>
2026-06-11 09:10:57 +0900

    feat(gas): CombatStat modifier DataTable row 추가

--- Source/NeoSanctum/Data/Combat/NSCombatStatTypes.h
+++ Source/NeoSanctum/Data/Combat/NSCombatStatTypes.h
@@ -24,3 +24,35 @@ struct FNSAbilityBaseStatRow : public FTableRowBase
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NS|CombatStat")
 	bool bModifiable = true;
 };
+
+UENUM(BlueprintType)
+enum class ENSCombatStatModifierOperation : uint8
+{
+	Add,
+	Multiply
+};
+
+USTRUCT(BlueprintType)
+struct FNSCombatStatModifierRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NS|CombatStat",
+		meta = (AllowedTypes = "NSAugmentData"))
+	FPrimaryAssetId SourceDefId;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NS|CombatStat")
+	FGameplayTag TargetAbilityTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NS|CombatStat")
+	FGameplayTag StatTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NS|CombatStat")
+	ENSCombatStatModifierOperation Operation = ENSCombatStatModifierOperation::Add;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NS|CombatStat")
+	float Value = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NS|CombatStat")
+	bool bEnabled = true;
+};
