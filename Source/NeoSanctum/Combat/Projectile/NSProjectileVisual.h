634550dc67a43ee3625dc7bf6480a2bf86928aa7
ChoiJunHyeok <optimal9744@gmail.com>
2026-06-12 10:59:41 +0900

    feat(system): 시각 투사체 오브젝트 및 이동 기능 구현

---
+++ Source/NeoSanctum/Combat/Projectile/NSProjectileVisual.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NSProjectileVisual.generated.h"
+
+/**
+ * 클라이언트에서만 생성되는 투사체의 시각 표현
+ * 충돌, 데미지, 네트워크 복제 하지 않음
+ */
+UCLASS()
+class NEOSANCTUM_API ANSProjectileVisual : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANSProjectileVisual();
+
+	// 시각 탄환의 위치와 진행 방향을 갱신
+	void SetVisualTransform(
+		const FVector& Location,
+		const FVector& Direction);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile")
+	TObjectPtr<USceneComponent> SceneRoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile")
+	TObjectPtr<UStaticMeshComponent> ProjectileMesh;
+};
