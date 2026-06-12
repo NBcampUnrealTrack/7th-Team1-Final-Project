f1e3ac71a4eef9ad0b9284c5c33004a8323a2982
Daype <mj1527123@gmail.com>
2026-06-12 12:56:28 +0900

    add(ai) : 어빌리티 작동 커스텀 Task 파일 및 변수 함수 추가

---
+++ Source/NeoSanctum/AI/Companion/Task/NSBTTask_CompanionActivateAbility.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBTTask_CompanionActivateAbility.h"
+
+UNSBTTask_CompanionActivateAbility::UNSBTTask_CompanionActivateAbility()
+{
+}
+
+EBTNodeResult::Type UNSBTTask_CompanionActivateAbility::ExecuteTask(UBehaviorTreeComponent& OwnerComp,
+                                                                    uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
+
+void UNSBTTask_CompanionActivateAbility::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory,
+	float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+}
---
+++ Source/NeoSanctum/AI/Companion/Task/NSBTTask_CompanionActivateAbility.h
@@ -0,0 +1,26 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "NSBTTask_CompanionActivateAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSBTTask_CompanionActivateAbility : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+public:
+	UNSBTTask_CompanionActivateAbility();
+	
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, Category="Companion|Ability")
+	FGameplayTagContainer ActivateAbility;
+};
