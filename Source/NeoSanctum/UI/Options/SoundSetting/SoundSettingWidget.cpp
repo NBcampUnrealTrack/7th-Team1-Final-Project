752c02ff1aab100cc1b595fa9e9afd5349df789e
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-12 15:55:50 +0900

    add(ui): SoundSettingUI 샘플 코드 뼈대 구성

---
+++ Source/NeoSanctum/UI/Options/SoundSetting/SoundSettingWidget.cpp
@@ -0,0 +1,62 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "SoundSettingWidget.h"
+
+#include "Components/Button.h"
+#include "Components/Slider.h"
+
+void USoundSettingWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (MasterVolumeSlider)
+	{
+		MasterVolumeSlider->OnValueChanged.AddDynamic(this, &USoundSettingWidget::OnMasterVolumeChanged);
+	}
+	if (BGMVolumeSlider)
+	{
+		BGMVolumeSlider->OnValueChanged.AddDynamic(this, &USoundSettingWidget::OnBGMVolumeChanged);
+	}
+	if (SFXVolumeSlider)
+	{
+		SFXVolumeSlider->OnValueChanged.AddDynamic(this, &USoundSettingWidget::OnSFXVolumeChanged);
+	}
+	if (UIVolumeSlider)
+	{
+		UIVolumeSlider->OnValueChanged.AddDynamic(this, &USoundSettingWidget::OnUIVolumeChanged);
+	}
+	
+	if (ApplyButton)
+	{
+		ApplyButton->OnClicked.AddDynamic(this, &USoundSettingWidget::OnApplyClicked);
+	}
+	if (BackButton)
+	{
+		BackButton->OnClicked.AddDynamic(this, &USoundSettingWidget::OnBackClicked);
+	}
+}
+
+void USoundSettingWidget::OnMasterVolumeChanged(float Value)
+{
+}
+
+void USoundSettingWidget::OnBGMVolumeChanged(float Value)
+{
+}
+
+void USoundSettingWidget::OnSFXVolumeChanged(float Value)
+{
+}
+
+void USoundSettingWidget::OnUIVolumeChanged(float Value)
+{
+}
+
+void USoundSettingWidget::OnApplyClicked()
+{
+}
+
+void USoundSettingWidget::OnBackClicked()
+{
+}
---
+++ Source/NeoSanctum/UI/Options/SoundSetting/SoundSettingWidget.h
@@ -0,0 +1,74 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SoundSettingWidget.generated.h"
+
+class UButton;
+class UTextBlock;
+class USlider;
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API USoundSettingWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+	
+protected:
+	UFUNCTION()
+	void OnMasterVolumeChanged(float Value);
+	
+	UFUNCTION()
+	void OnBGMVolumeChanged(float Value);
+	
+	UFUNCTION()
+	void OnSFXVolumeChanged(float Value);
+	
+	UFUNCTION()
+	void OnUIVolumeChanged(float Value);
+	
+	UFUNCTION()
+	void OnApplyClicked();
+	
+	UFUNCTION()
+	void OnBackClicked();
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USlider> MasterVolumeSlider;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USlider> BGMVolumeSlider;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USlider> SFXVolumeSlider;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USlider> UIVolumeSlider;
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> MasterVolumeText;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> BGMVolumeText;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> SFXVolumeText;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> UIVolumeText;
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ApplyButton;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> BackButton;
+};
