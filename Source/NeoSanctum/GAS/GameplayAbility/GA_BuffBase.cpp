3e8646956d8e048d58060e72ca472187464e72aa
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-15 20:05:46 +0900

    add(gas): GA_BuffBase 설정 구조체 및 프로퍼티 추가
    버프를 주는 Ability를 최대한 공통화할 생각으로 `GA_BuffBase로 네이밍 함.
    
    버프의 타입을 지정
    - Self : 개인
    - SingleTarget : 특정 타겟 지정
    - Radius : 범위 지정
    
    FNSBuffTargetFilter를 통해 버프 대상의 필터를 지정
    - bIncludeSelf : 자신 포함 여부
    - bIncludeOwnedTurrets  : 자신이 소환한 Turret 포함 여부
    - bIncludeOtherPlayers : 다른 플레이어 포함 여부
    
    FNSBuffEffectEntry
    - 액터마다 다른 GE를 적용해야할 가능성이 있어서 액터마다 EffectClass를 지정하는게 좋아보임

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_BuffBase.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "GA_BuffBase.h"
+
+UGA_BuffBase::UGA_BuffBase()
+{
+}
+
+void UGA_BuffBase::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                                   const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+}
+
+void UGA_BuffBase::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_BuffBase.h
@@ -0,0 +1,101 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_SkillBase.h"
+#include "NeoSanctum/Data/Combat/NSCombatStatTypes.h"
+#include "GA_BuffBase.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ENSBuffTargetType : uint8
+{
+	Self,
+	SingleTarget,
+	Radius
+};
+
+USTRUCT(BlueprintType)
+struct FNSBuffTargetFilter
+{
+	GENERATED_BODY()
+
+	// 자신 포함
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff|Target")
+	bool bIncludeSelf = true;
+
+	// 자신이 소환한 Turret 포함
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff|Target")
+	bool bIncludeOwnedTurrets = false;
+
+	// 다른 플레이어 포함
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff|Target")
+	bool bIncludeOtherPlayers = false;
+};
+
+USTRUCT(BlueprintType)
+struct FNSBuffEffectEntry
+{
+	GENERATED_BODY()
+
+	// 적용 대상 Actor 타입
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff|Effect")
+	TSubclassOf<AActor> TargetActorClass;
+
+	// 대상에게 적용할 GameplayEffect
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff|Effect")
+	TSubclassOf<UGameplayEffect> EffectClass;
+};
+
+/**
+ * 버프를 줄 수 있는 Ability
+ * 
+ * 어떤 대상에게 어떤 GE를 적용할 것인지를 결정하는 간단한 Ability
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_BuffBase : public UGA_SkillBase
+{
+	GENERATED_BODY()
+
+public:
+	UGA_BuffBase();
+	
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+
+protected:
+	// 버프 대상 수집 방식
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Buff|Target")
+	ENSBuffTargetType TargetType = ENSBuffTargetType::Self;
+
+	// 버프 대상 필터
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Buff|Target")
+	FNSBuffTargetFilter TargetFilter;
+
+	// 대상 타입별 적용 GE
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Buff|Effect")
+	TArray<FNSBuffEffectEntry> BuffEffectEntries;
+
+	// CombatStat 값을 GameplayEffect SetByCaller 값으로 넘기기 위한 매핑
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Buff|SetByCaller")
+	TArray<FNSSetByCallerFromCombatStat> SetByCallerMappings;
+
+	// CombatStat 조회에 사용할 Ability 태그
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Buff|SetByCaller")
+	FGameplayTag CombatStatAbilityTag;
+};
