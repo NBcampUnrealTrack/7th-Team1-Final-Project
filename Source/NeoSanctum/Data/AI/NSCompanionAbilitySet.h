9b8fad843637e256bf84dbc89aeeed3cdef88b13
Daype <mj1527123@gmail.com>
2026-06-15 16:57:50 +0900

    add(ai) : Companion AbilitySet 추가

---
+++ Source/NeoSanctum/Data/AI/NSCompanionAbilitySet.h
@@ -0,0 +1,21 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "NSCompanionAbilitySet.generated.h"
+
+UCLASS(BlueprintType, const)
+class NEOSANCTUM_API UNSCompanionAbilitySet : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	void GiveToAbilitySystem(UAbilitySystemComponent* ASC,
+		FNSCompanionAbilitySet_GameplayAbility* OutGranted,
+		UObject* SourceObject = nullptr) const;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, meta=(TitleProperty=Ability))
+	TArray<FNSCompanionAbilitySet_GameplayAbility> GrantedAbilities;
+};
