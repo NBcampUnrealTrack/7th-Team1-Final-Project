975604d0717c82e83a284ef9d673a4b11020b5ec
Daype <mj1527123@gmail.com>
2026-06-15 16:58:50 +0900

    add(ai) : CompanionAbilitySetType 추가 어빌리티 추적 및 어빌리티 내용보관

---
+++ Source/NeoSanctum/Data/Ability/NSCompanionAbilitySetTypes.h
@@ -0,0 +1,28 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "GameplayAbilitySpecHandle.h"
+#include "ActiveGameplayEffectHandle.h"
+#include "NSCompanionAbilitySetTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FNSCompanionAbilitySet_GameplayAbility
+{
+	GENERATED_BODY()
+	UPROPERTY(EditDefaultsOnly) TSubclassOf<UGameplayAbility> Ability;
+	UPROPERTY(EditDefaultsOnly) int32 AbilityLevel = 1;
+};
+
+USTRUCT(BlueprintType)
+struct FNSCompanionAbilitySet_GrantedHandles
+{
+	GENERATED_BODY()
+	void AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle);
+	void AddGameplayEffectEffectHandle(const FActiveGameplayEffectHandle& Handle);
+	void TakeFromAbilitySystem(UAbilitySystemComponent* ASC);
+	
+protected:
+	UPROPERTY() TArray<FGameplayAbilitySpecHandle> AbilitySpecHandles;
+	UPROPERTY() TArray<FActiveGameplayEffectHandle> GameplayEffectHandles;
+};
