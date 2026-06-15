b432088183f68c16fd580d6f68012de018be4d2a
Daype <mj1527123@gmail.com>
2026-06-15 16:57:34 +0900

    add(ai) : Companion Definition 추가

---
+++ Source/NeoSanctum/Data/AI/NSCompanionDefinition.h
@@ -0,0 +1,35 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "NSCompanionDefinition.generated.h"
+
+class UGameplayEffect;
+class UNSCompanionAbilitySet;
+
+UCLASS(BlueprintType)
+class NEOSANCTUM_API UNSCompanionDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag CompanionTag;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FText DisplayName;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UTexture2D> Icon;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<USkeletalMesh> CompanionMesh;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UNSCompanionAbilitySet> AbilitySet;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UGameplayEffect> DefaultStatsEffect;
+};
