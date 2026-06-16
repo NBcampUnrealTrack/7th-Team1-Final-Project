6f1029dd55a77ec83e1ef27026d86078de5bdd93
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-16 09:54:33 +0900

    add(gas): 쿨다운 적용을 위한 Effect 태그

--- Source/NeoSanctum/Tag/NSGameplayTags_Effect.cpp
+++ Source/NeoSanctum/Tag/NSGameplayTags_Effect.cpp
@@ -19,6 +19,7 @@ namespace NSGameplayTags
 	
 	// Cooldown
 	UE_DEFINE_GAMEPLAY_TAG(Effect_Cooldown_Ranger_ProjectileShot, "Effect.Cooldown.Ranger.ProjectileShot");
+	UE_DEFINE_GAMEPLAY_TAG(Effect_Cooldown_BuffBase, "Effect.Cooldown.BuffBase");
 
 	// Damage SetByCaller
 	UE_DEFINE_GAMEPLAY_TAG(Effect_Damage_Base, "Effect.Damage.Base");
