4b1d88c734ff8f720eb4450b422e6d7a017b1384
Daype <mj1527123@gmail.com>
2026-06-16 19:27:24 +0900

    feat(ai) : 서버 기반 업그레이드 진행 함수 구현

--- Source/NeoSanctum/Character/Component/NSCompanionProgressionComponent.cpp
+++ Source/NeoSanctum/Character/Component/NSCompanionProgressionComponent.cpp
@@ -3,34 +3,43 @@
 
 #include "NSCompanionProgressionComponent.h"
 
+#include "NeoSanctum/AI/Companion/Base/NSBaseCompanionAI.h"
+#include "NeoSanctum/Data/AI/NSCompanionCatalog.h"
+#include "NeoSanctum/Data/AI/NSCompanionDefinition.h"
 
-// Sets default values for this component's properties
-UNSCompanionProgressionComponent::UNSCompanionProgressionComponent()
+
+void UNSCompanionProgressionComponent::ServerTryUpgrade_Implementation(FGameplayTag NodeTag)
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
-}
-
-
-// Called when the game starts
-void UNSCompanionProgressionComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
+	if (!OwnedCompanion || !Catalog || !SelectedCompanionTag.IsValid()) return;
 	
+	// 클릭시 받은 태그를 기반으로 드론의 정의를 캐싱
+	UNSCompanionDefinition* CurrentDefinition = Catalog->FindByTag(SelectedCompanionTag);
+	if (!CurrentDefinition) return;
+	
+	// 캐싱된 정의가 보유한 업그레이드 노드들 순회
+	for (const FNSCompanionUpgradeNode& UpgradeNode : CurrentDefinition->UpgradeNodes)
+	{
+		// 현재 요청된 태그와 다를경우 건너뛰기
+		if (UpgradeNode.NodeTag != NodeTag) continue;
+		
+		// 같다면 해당 노드의 최대 레벨과 Player가 가지고있는 해당 노드의 현재 레벨 리딩
+		const int32 MaxNodeLevel = UpgradeNode.MaxLevel;
+		const int32 CurrentNodeLevel = NodeLevels.FindRef(NodeTag);
+		
+		// 새로 적용할 레벨 계산
+		const int32 NewLevel = CurrentNodeLevel + 1;
+		
+		// 업그레이드 적용 후 레벨이 Max레벨보다 높다면 스킵
+		if (NewLevel > MaxNodeLevel) break;
+		
+		// 노드 레벨 저장
+		NodeLevels.Add(NodeTag, NewLevel);
+		// 선택된 태그의 누적 업그레이드를 찾아서 새로운 값으로 적용
+		CompanionUpgradeCounts.FindOrAdd(SelectedCompanionTag)++;
+		// 소유중인 Companion에 능력치 실제 적용
+		OwnedCompanion->ApplyStatUpgrade(UpgradeNode.NodeTag, NewLevel);
+		break;
+	}
 }
 
-
-// Called every frame
-void UNSCompanionProgressionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-                                                     FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
 
--- Source/NeoSanctum/Character/Component/NSCompanionProgressionComponent.h
+++ Source/NeoSanctum/Character/Component/NSCompanionProgressionComponent.h
@@ -3,25 +3,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Components/ActorComponent.h"
 #include "NSCompanionProgressionComponent.generated.h"
 
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ANSBaseCompanionAI;
+class UNSCompanionCatalog;
+
+UCLASS(ClassGroup=(NEOSANCTUM), meta=(BlueprintSpawnableComponent))
 class NEOSANCTUM_API UNSCompanionProgressionComponent : public UActorComponent
 {
 	GENERATED_BODY()
-
 public:
-	// Sets default values for this component's properties
-	UNSCompanionProgressionComponent();
-
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UNSCompanionCatalog> Catalog;
+	
+	// 업그레이드
+	UFUNCTION(Server, Reliable)
+	void ServerTryUpgrade(FGameplayTag NodeTag);
+	
+	// 해금 판정 게이팅
+	UFUNCTION(BlueprintPure)
+	bool CanSelect(FGameplayTag CompanionTag) const;
+	
+	UFUNCTION(Server, Reliable)
+	void ServerTrySelect(FGameplayTag CompanionTag);
+	
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY()
+	TObjectPtr<ANSBaseCompanionAI> OwnedCompanion;
+	
+	// 세이브 데이터를 위한 진행상태 데이터
+	UPROPERTY()
+	FGameplayTag SelectedCompanionTag; // 선택중인 대상
+	UPROPERTY()
+	TMap<FGameplayTag, int32> NodeLevels; // 노드별 레벨
+	UPROPERTY()
+	TMap<FGameplayTag, int32> CompanionUpgradeCounts; // 누적 업그레이드
 };
