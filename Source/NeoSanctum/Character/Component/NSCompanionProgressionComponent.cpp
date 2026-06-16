d9eb23dbacaa0a731f56d3fc129d8cd8873c3e12
Daype <mj1527123@gmail.com>
2026-06-16 15:59:06 +0900

    add(ai) : 실제 업그레이드 담당 ActorComponent추가

---
+++ Source/NeoSanctum/Character/Component/NSCompanionProgressionComponent.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSCompanionProgressionComponent.h"
+
+
+// Sets default values for this component's properties
+UNSCompanionProgressionComponent::UNSCompanionProgressionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UNSCompanionProgressionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UNSCompanionProgressionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
