83f727abc220a25432934f8864af688c22cadf75
Park WonJong <kidsmon@naver.com>
2026-06-16 10:25:48 +0900

    add(data): 보상 관련 GameplayTag 추가

---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Reward.cpp
@@ -0,0 +1,13 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#include "NSGameplayTags_Reward.h"
+
+namespace NSGameplayTags
+{
+	// Rewards
+	UE_DEFINE_GAMEPLAY_TAG(Reward_Trigger_NormalKill, "Reward.Trigger.NormalKill");
+	UE_DEFINE_GAMEPLAY_TAG(Reward_Trigger_EliteKill, "Reward.Trigger.EliteKill");
+	UE_DEFINE_GAMEPLAY_TAG(Reward_Trigger_BossKill, "Reward.Trigger.BossKill");
+	UE_DEFINE_GAMEPLAY_TAG(Reward_Trigger_Chest, "Reward.Trigger.Chest");
+	UE_DEFINE_GAMEPLAY_TAG(Reward_Trigger_LevelUp, "Reward.Trigger.LevelUp");
+}
---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Reward.h
@@ -0,0 +1,15 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace NSGameplayTags
+{
+	// Rewards
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Reward_Trigger_NormalKill);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Reward_Trigger_EliteKill);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Reward_Trigger_BossKill);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Reward_Trigger_Chest);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Reward_Trigger_LevelUp);
+}
