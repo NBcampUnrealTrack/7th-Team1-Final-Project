2a3167c9d214028ae7d41d506260e3a9f02469f1
Park WonJong <kidsmon@naver.com>
2026-06-16 11:16:51 +0900

    add(data): 증강 보상 풀 데이터 구조 추가

---
+++ Source/NeoSanctum/Data/Reward/NSRewardTypes.h
@@ -0,0 +1,28 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "UObject/PrimaryAssetId.h"
+#include "NSRewardTypes.generated.h"
+
+class UNSAugmentRewardPoolData;
+
+/**
+ * 몬스터 / 보상 트리거에서 사용할 드랍 항목 데이터
+ */
+USTRUCT(BlueprintType)
+struct FNSMonsterDropEntry
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Reward")
+	FGameplayTag RewardTypeTag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Reward", meta = (ClampMin = "0.0"))
+	float Weight = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Reward")
+	TSoftObjectPtr<UNSAugmentRewardPoolData> AugmentRewardPool;																																
+};
