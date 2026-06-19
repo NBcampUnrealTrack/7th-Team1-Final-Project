abb095d6f0fe2d3af1afce31d0c966696fa67cfe
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-19 09:24:59 +0900

    add(gas): Barrier 액터 스켈레톤 코드

---
+++ Source/NeoSanctum/Combat/Weapon/Summon/NSBarrier.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBarrier.h"
+
+ANSBarrier::ANSBarrier()
+{
+}
+
+void ANSBarrier::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/NeoSanctum/Combat/Weapon/Summon/NSBarrier.h
@@ -0,0 +1,19 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NSBarrier.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSBarrier : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANSBarrier();
+
+protected:
+	virtual void BeginPlay() override;
+};
