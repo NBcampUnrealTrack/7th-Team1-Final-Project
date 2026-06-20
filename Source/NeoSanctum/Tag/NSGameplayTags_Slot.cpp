6da90dcadd4ef7d5216d15b9c7f4c0c5979d2b28
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-21 06:57:33 +0900

    add(gas): Skill Slot Tag 및 Skill 쿨타임을 위한 Recharge Effect SetByCaller Tag 추가

---
+++ Source/NeoSanctum/Tag/NSGameplayTags_Slot.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSGameplayTags_Slot.h"
+
+namespace NSGameplayTags
+{
+	// Skill Slot Tag
+	UE_DEFINE_GAMEPLAY_TAG(SkillSlot_Skill1, "SkillSlot.Skill1");
+	UE_DEFINE_GAMEPLAY_TAG(SkillSlot_Skill2, "SkillSlot.Skill2");
+	UE_DEFINE_GAMEPLAY_TAG(SkillSlot_Skill3, "SkillSlot.Skill3");
+}
