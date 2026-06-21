72db0b458bca53bdddc2b84e21650f36ee8a69b9
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-22 01:25:21 +0900

    add(system): Custom Trace Channel 및 Collision Profile을 상황에 맞게 추가
    추가된 CollisionChannel
    - Player : 플레이어 전용
    - EnemyProjectile : Enemy Projectile 전용
    - Enemy : Enemy 자신 전용
    - DestructibleObject : 파괴가능한 오브젝트 전용
    - PlayerConstruct : 플레이어가 소환한 소환물 전용(Turret, Barrier)
    
    추가된 TraceChannel
    - CombatSight :  공격할 대상을 판단하거나 할 때의 시각적 판단
    - WeaponTrace : 실제로 데미지를 입힐 수 있는 히트스캔 혹은 데미지 트레이스
    - ProjectileTrace : 발사체 트레이스
    - ExplosionTrace : 차폐물이 있는 경우를 고려해서 폭발시에 한 번 발사하는 트레이스

---
+++ Source/NeoSanctum/Collision/NSCollisionChannels.h
@@ -0,0 +1,24 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * Custom Trace Channel 모음
+ * 그냥 ECC_GameTraceChannel1 같은 식으로 사용할 수도 있으나
+ * 아무래도 어떤 기능을 하는 Channel인지 명확하지 않아 따로 상수화시켜서 사용할 수 있게 함
+ **/
+namespace NSCollisionChannels
+{
+	constexpr ECollisionChannel Player = ECC_GameTraceChannel1;
+	constexpr ECollisionChannel EnemyProjectile = ECC_GameTraceChannel2;
+	constexpr ECollisionChannel Enemy = ECC_GameTraceChannel3;
+	constexpr ECollisionChannel DestructibleObject = ECC_GameTraceChannel4;
+	constexpr ECollisionChannel PlayerConstruct = ECC_GameTraceChannel5;
+
+	constexpr ECollisionChannel CombatSight = ECC_GameTraceChannel6;
+	constexpr ECollisionChannel WeaponTrace = ECC_GameTraceChannel7;
+	constexpr ECollisionChannel ProjectileTrace = ECC_GameTraceChannel8;
+	constexpr ECollisionChannel ExplosionTrace = ECC_GameTraceChannel9;
+}
