2b1fc9cab106cb6261e2efaf560056c3fa6fc302
ChoiJunHyeok <optimal9744@gmail.com>
2026-06-21 21:23:06 +0900

    feat(ai): 근접 공격 예약 반환 BT Task 구현

---
+++ Source/NeoSanctum/AI/Enemy/Task/NSBTTask_ReleaseMeleeReservation.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBTTask_ReleaseMeleeReservation.h"
+
+#include "NeoSanctum/AI/Enemy/Controller/NSEnemyAIController.h"
+
+UNSBTTask_ReleaseMeleeReservation::UNSBTTask_ReleaseMeleeReservation()
+{
+	NodeName = TEXT("Release Melee Reservation");
+}
+
+EBTNodeResult::Type UNSBTTask_ReleaseMeleeReservation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	ANSEnemyAIController* Controller = Cast<ANSEnemyAIController>(OwnerComp.GetAIOwner());
+
+	if (!Controller)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	Controller->ReleaseMeleeAttackReservation(true);
+	return EBTNodeResult::Succeeded;
+}
