b0c7b8dce3f65669d223f974e751e0d75d057885
ChoiJunHyeok <optimal9744@gmail.com>
2026-06-21 09:35:41 +0900

    feat(ai): 근접 공격 예약 시스템의 컴포넌트 Tick과 기본 예약 설정을 초기화하는 생성자 구현

---
+++ Source/NeoSanctum/Combat/Component/NSMeleeAttackReservationComponent.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSMeleeAttackReservationComponent.h"
+#include "NeoSanctum/Character/Enemy/NSEnemyCharacterBase.h"
+
+UNSMeleeAttackReservationComponent::UNSMeleeAttackReservationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickInterval = 0.2f;
+
+	SetIsReplicatedByDefault(false);
+}
+
