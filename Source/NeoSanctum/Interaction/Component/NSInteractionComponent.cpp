116b12432deea9874d4ad07770062e1f2c93b02c
JongKyu <kongi7064@gmail.com>
2026-06-23 16:44:16 +0900

    refactor(system): 파츠 위젯 문구 변경

--- Source/NeoSanctum/Interaction/Component/NSInteractionComponent.cpp
+++ Source/NeoSanctum/Interaction/Component/NSInteractionComponent.cpp
@@ -239,7 +239,7 @@ void UNSInteractionComponent::ShowPromptFor(AActor* Target)
 
 		if (Cast<ANSDroppedPart>(Target))
 		{
-			Widget->SetPromptText(InteractionKeyText, FText::FromString(TEXT("줍기")));
+			Widget->SetPromptText(InteractionKeyText, FText::FromString(TEXT("교체")));
 			Widget->SetPartName(PromptText);
 		}
 		else
