43a4a4e8b55e5c524768cf4326241b3b21faa2dd
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-28 15:34:00 +0900

    add(system): Player의 AttackFeedback을 위한 정보 Type 생성
    TargetType

---
+++ Source/NeoSanctum/Combat/HitReaction/NSHitFeedbackTypes.h
@@ -0,0 +1,96 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NSHitFeedbackTypes.generated.h"
+
+// 히트 피드백 대상 분류
+UENUM(BlueprintType)
+enum class ENSHitFeedbackTargetType : uint8
+{
+	Any,
+	Enemy,
+	Barrier,
+	DestructibleObject,
+	Turret
+};
+
+// 히트 판정 품질 : 추후에 추가될 사항까지 고려했으나 아직 Critical, Headshot은 로직이 없음
+UENUM(BlueprintType)
+enum class ENSHitFeedbackQuality : uint8
+{
+	Any,
+	Normal,
+	Critical,
+	HeadShot
+};
+
+// 히트에 따른 결과
+UENUM(BlueprintType)
+enum class ENSHitFeedbackOutcome : uint8
+{
+	Any,
+	None,
+	Destroy,
+	Kill
+};
+
+// 크로스헤어 공격 피드백 종류 : 추후에 추가될 사항까지 고려했으나 아직 Critical, Headshot은 로직이 없음
+UENUM(BlueprintType)
+enum class ENSCrosshairAttackFeedbackType : uint8
+{
+	None,
+	NormalAttack,
+	CriticalAttack,
+	HeadShot,
+	ShieldBarrierAttack,
+	Kill,
+	Destroy
+};
+
+// 히트 피드백 판정에 필요한 공통 Context
+USTRUCT(BlueprintType)
+struct NEOSANCTUM_API FNSHitFeedbackContext
+{
+	GENERATED_BODY()
+	
+	// 타겟
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "HitFeedback")
+	ENSHitFeedbackTargetType TargetType = ENSHitFeedbackTargetType::Any;
+	
+	// 히트가 어떤 타입인지 : Normal, Critical, Headshot 구분용
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "HitFeedback")
+	ENSHitFeedbackQuality HitQuality = ENSHitFeedbackQuality::Normal;
+	
+	// 히트의 결과 : Hit결가로 Kill이나 Destroy가 되는 경우
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "HitFeedback")
+	ENSHitFeedbackOutcome Outcome = ENSHitFeedbackOutcome::None;
+	
+	// 타겟 액터
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "HitFeedback")
+	TObjectPtr<AActor> TargetActor = nullptr;
+	
+	// 히트 지점
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "HitFeedback")
+	FVector HitLocation = FVector::ZeroVector;
+	
+	// 타겟이 죽었는지를 판단
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "HitFeedback")
+	bool bTargetDead = false;
+};
+
+// GMS로 크로스헤어 공격 피드백을 전달할 때 사용하는 메시지
+USTRUCT(BlueprintType)
+struct NEOSANCTUM_API FNSCrosshairAttackFeedbackMessage
+{
+	GENERATED_BODY()
+	
+	// 어떤 크로스헤어 피드백을 재생할지
+	UPROPERTY(BlueprintReadOnly, Category = "HitFeedback")
+	ENSCrosshairAttackFeedbackType FeedbackType = ENSCrosshairAttackFeedbackType::None;
+	
+	// HitFeedback 관련 정보 구조체
+	UPROPERTY(BlueprintReadOnly, Category = "HitFeedback")
+	FNSHitFeedbackContext Context;
+};
