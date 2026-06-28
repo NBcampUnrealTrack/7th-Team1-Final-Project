4e9465c6ab4c496276f51ff8cc30864ab1cd82d3
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-28 16:20:41 +0900

    add(ui): Crosshair widget에 HitFeedback 관련 크로스헤어 Image/Animation 추가

--- Source/NeoSanctum/Tag/NSGameplayTags_Message.cpp
+++ Source/NeoSanctum/Tag/NSGameplayTags_Message.cpp
@@ -6,4 +6,5 @@ namespace NSGameplayTags
 {
 	// GMS 쿨다운 상태 변경 채널
 	UE_DEFINE_GAMEPLAY_TAG(Message_UI_SkillCooldown_Changed, "Message.UI.SkillCooldown.Changed");
+	UE_DEFINE_GAMEPLAY_TAG(Message_UI_Crosshair_AttackFeedback, "Message.UI.Crosshair.AttackFeedback");
 }
--- Source/NeoSanctum/Tag/NSGameplayTags_Message.h
+++ Source/NeoSanctum/Tag/NSGameplayTags_Message.h
@@ -8,4 +8,5 @@ namespace NSGameplayTags
 {
 	// GMS 쿨다운 상태 변경 채널
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_UI_SkillCooldown_Changed);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_UI_Crosshair_AttackFeedback);
 }
