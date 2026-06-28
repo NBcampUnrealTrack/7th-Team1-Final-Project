4e82668ae9b042eca58ad647e8d7474b67ed4c04
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-28 15:36:15 +0900

    add(system): PlayerAttackFeedbackData DataTableRow 생성
    TargetType : 어떤 타겟을 때렸는지
    HitQuality : Hit의 타입이 어떤지(Normal인지 Critical인지 등)
    Outcome : Hit의 결과가 어떤지(아무런결과가 없는지 Kill인지 등)
    CrosshairFeedbackType : 어떤 CrossHairFeedback을 재생할 것인지
    Priority : 피드백 우선순위

---
+++ Source/NeoSanctum/Data/Combat/NSPlayerAttackFeedbackData.h
@@ -0,0 +1,30 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "NeoSanctum/Combat/HitReaction/NSHitFeedbackTypes.h"
+#include "NSPlayerAttackFeedbackData.generated.h"
+
+// 플레이어 공격 결과를 크로스헤어 피드백 타입으로 매핑하는 DataTable Row
+USTRUCT(BlueprintType)
+struct NEOSANCTUM_API FNSPlayerAttackFeedbackData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AttackFeedback")
+	ENSHitFeedbackTargetType TargetType = ENSHitFeedbackTargetType::Any;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AttackFeedback")
+	ENSHitFeedbackQuality HitQuality = ENSHitFeedbackQuality::Any;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AttackFeedback")
+	ENSHitFeedbackOutcome Outcome = ENSHitFeedbackOutcome::Any;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AttackFeedback")
+	ENSCrosshairAttackFeedbackType CrosshairFeedbackType = ENSCrosshairAttackFeedbackType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AttackFeedback")
+	int32 Priority = 0;
+};
