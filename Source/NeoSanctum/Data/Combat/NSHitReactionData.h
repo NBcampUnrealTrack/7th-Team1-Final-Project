43fc2eb6249d9946ae68805122ad645a69f44556
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-29 07:31:03 +0900

    add(system): 타겟/상황 등에 따라 어떤 HitReaction를 재생할지에 대한 정보를 담은 DataTableRow

---
+++ Source/NeoSanctum/Data/Combat/NSHitReactionData.h
@@ -0,0 +1,31 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameplayTagContainer.h"
+#include "NeoSanctum/Combat/HitReaction/NSHitFeedbackTypes.h"
+#include "NSHitReactionData.generated.h"
+
+// 월드 피격 리액션 조건을 GameplayCue로 매핑하는 DataTable Row
+USTRUCT(BlueprintType)
+struct NEOSANCTUM_API FNSHitReactionData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HitReaction")
+	ENSHitFeedbackTargetType TargetType = ENSHitFeedbackTargetType::Any;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HitReaction")
+	ENSHitFeedbackQuality HitQuality = ENSHitFeedbackQuality::Any;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HitReaction")
+	ENSHitFeedbackOutcome Outcome = ENSHitFeedbackOutcome::Any;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HitReaction")
+	FGameplayTag GameplayCueTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HitReaction")
+	int32 Priority = 0;
+};
