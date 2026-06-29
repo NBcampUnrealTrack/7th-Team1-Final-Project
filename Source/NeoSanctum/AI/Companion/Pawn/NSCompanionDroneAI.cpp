890bb28bca04073c112d4180bb312c720a7e4fa5
Daype <mj1527123@gmail.com>
2026-06-29 16:03:57 +0900

    refactor(ai) : Base 상속받는 Companion 클래스 생성 기존 빈 클래스 제거 리팩토링

---
+++ Source/NeoSanctum/AI/Companion/Pawn/NSCompanionDroneAI.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSCompanionDroneAI.h"
+
+
+// Sets default values
+ANSCompanionDroneAI::ANSCompanionDroneAI()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSCompanionDroneAI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSCompanionDroneAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ANSCompanionDroneAI::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/NeoSanctum/AI/Companion/Pawn/NSCompanionDroneAI.h
@@ -0,0 +1,28 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NeoSanctum/AI/Base/NSBaseDroneAI.h"
+#include "NSCompanionDroneAI.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSCompanionDroneAI : public ANSBaseDroneAI
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ANSCompanionDroneAI();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
