53812c4043b66aa27682104f22c40729ab11fab6
Daype <mj1527123@gmail.com>
2026-06-30 16:09:31 +0900

    add(ai) : Enemy AttributeSet 추가

---
+++ Source/NeoSanctum/GAS/AttributeSet/NSEnemyAttributeSet.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NSBaseAttributeSet.h"
+#include "NSEnemyAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSEnemyAttributeSet : public UNSBaseAttributeSet
+{
+	GENERATED_BODY()
+};
