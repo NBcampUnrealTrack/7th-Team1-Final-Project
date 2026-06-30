a6a17f680ae3b3fdf5c543f3ed96739f4f60a6f6
Daype <mj1527123@gmail.com>
2026-06-30 18:56:06 +0900

    add(ai) : EnemyDronePawn Class 추가

---
+++ Source/NeoSanctum/AI/Enemy/Pawn/NSEnemyDroneAI.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSEnemyDroneAI.h"
+
+
+// Sets default values
+ANSEnemyDroneAI::ANSEnemyDroneAI()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSEnemyDroneAI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSEnemyDroneAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ANSEnemyDroneAI::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
