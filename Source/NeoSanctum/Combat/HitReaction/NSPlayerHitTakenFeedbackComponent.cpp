93648466cf4f09bac419c4be361d1a2554f6c627
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-06-30 16:40:25 +0900

    feat(system): HitTakenFeedbackComponent에서 상태태그를 GMS로 만들어 Broadcast하기 위한 로직 추가

--- Source/NeoSanctum/Combat/HitReaction/NSPlayerHitTakenFeedbackComponent.cpp
+++ Source/NeoSanctum/Combat/HitReaction/NSPlayerHitTakenFeedbackComponent.cpp
@@ -2,13 +2,30 @@
 
 #include "NSPlayerHitTakenFeedbackComponent.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 #include "GameFramework/GameplayMessageSubsystem.h"
 #include "GameFramework/Pawn.h"
 #include "NeoSanctum/Tag/NSGameplayTags_Message.h"
+#include "NeoSanctum/Tag/NSGameplayTags_State.h"
 
 UNSPlayerHitTakenFeedbackComponent::UNSPlayerHitTakenFeedbackComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UNSPlayerHitTakenFeedbackComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TryBindShieldRechargingTagEvent();
+}
+
+void UNSPlayerHitTakenFeedbackComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UnbindShieldRechargingTagEvent();
+
+	Super::EndPlay(EndPlayReason);
 }
 
 void UNSPlayerHitTakenFeedbackComponent::HandleHitTakenFeedback(
@@ -19,11 +36,80 @@ void UNSPlayerHitTakenFeedbackComponent::HandleHitTakenFeedback(
 		return;
 	}
 
+	TryBindShieldRechargingTagEvent();
+
 	FNSHitTakenFeedbackMessage Message;
 	Message.Context = Context;
 
 	UGameplayMessageSubsystem::Get(this).BroadcastMessage(
 		NSGameplayTags::Message_UI_HitTakenFeedback,
+		Message);
+}
+
+void UNSPlayerHitTakenFeedbackComponent::TryBindShieldRechargingTagEvent()
+{
+	if (!ShouldPlayLocalFeedback() || CachedASC.IsValid())
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* ASC =
+		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetOwner());
+	if (!ASC)
+	{
+		return;
+	}
+
+	CachedASC = ASC;
+	ShieldRechargingTagDelegateHandle =
+		ASC->RegisterGameplayTagEvent(
+			NSGameplayTags::State_Shield_Recharging,
+			EGameplayTagEventType::NewOrRemoved).AddUObject(
+				this,
+				&ThisClass::HandleShieldRechargingTagChanged);
+
+	BroadcastHitTakenFeedbackState(
+		ENSHitTakenFeedbackStateType::ShieldRecharging,
+		ASC->HasMatchingGameplayTag(NSGameplayTags::State_Shield_Recharging));
+}
+
+void UNSPlayerHitTakenFeedbackComponent::UnbindShieldRechargingTagEvent()
+{
+	if (UAbilitySystemComponent* ASC = CachedASC.Get())
+	{
+		ASC->RegisterGameplayTagEvent(
+			NSGameplayTags::State_Shield_Recharging,
+			EGameplayTagEventType::NewOrRemoved).Remove(ShieldRechargingTagDelegateHandle);
+	}
+
+	CachedASC.Reset();
+	ShieldRechargingTagDelegateHandle.Reset();
+}
+
+void UNSPlayerHitTakenFeedbackComponent::HandleShieldRechargingTagChanged(
+	const FGameplayTag Tag,
+	const int32 NewCount)
+{
+	BroadcastHitTakenFeedbackState(
+		ENSHitTakenFeedbackStateType::ShieldRecharging,
+		NewCount > 0);
+}
+
+void UNSPlayerHitTakenFeedbackComponent::BroadcastHitTakenFeedbackState(
+	const ENSHitTakenFeedbackStateType StateType,
+	const bool bActive) const
+{
+	if (!ShouldPlayLocalFeedback())
+	{
+		return;
+	}
+
+	FNSHitTakenFeedbackStateMessage Message;
+	Message.StateType = StateType;
+	Message.bActive = bActive;
+
+	UGameplayMessageSubsystem::Get(this).BroadcastMessage(
+		NSGameplayTags::Message_UI_HitTakenFeedback_State,
 		Message);
 }
 
