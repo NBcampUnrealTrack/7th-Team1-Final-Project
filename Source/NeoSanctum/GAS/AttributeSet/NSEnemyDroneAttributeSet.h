c18d8c3e6810c9b90b3f6b019ba5229f404d0346
Daype <mj1527123@gmail.com>
2026-06-30 18:55:46 +0900

    add(ai) : EnemyDroneAttributeSet 추가

---
+++ Source/NeoSanctum/GAS/AttributeSet/NSEnemyDroneAttributeSet.h
@@ -0,0 +1,29 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NSBaseAttributeSet.h"
+#include "NSEnemyAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+UCLASS()
+class NEOSANCTUM_API UNSEnemyAttributeSet : public UNSBaseAttributeSet
+{
+	GENERATED_BODY()
+public:
+	UNSEnemyAttributeSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Health, Category="Enemy|Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UNSEnemyAttributeSet, Health);
+};
