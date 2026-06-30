44fd03daf92b18569a015fc729f230f3f2e62396
Daype <mj1527123@gmail.com>
2026-06-30 15:51:01 +0900

    add(ai) : BaseDroneDefinition 추가

---
+++ Source/NeoSanctum/Data/AI/NSDroneDefinition.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "NSDroneDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSDroneDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
