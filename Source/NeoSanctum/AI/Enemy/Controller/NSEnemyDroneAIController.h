07734c117f00f94b6c23385fd774cf3c62ab94ae
Daype <mj1527123@gmail.com>
2026-07-01 14:30:55 +0900

    add(ai) : EnemyDroneAIController 추가

---
+++ Source/NeoSanctum/AI/Enemy/Controller/NSEnemyDroneAIController.h
@@ -0,0 +1,25 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "NSEnemyDroneAIController.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSEnemyDroneAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ANSEnemyDroneAIController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
