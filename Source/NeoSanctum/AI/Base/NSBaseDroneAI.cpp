c2eadcb986fde370e97f02d21d434fe351ba1442
Daype <mj1527123@gmail.com>
2026-07-02 16:42:07 +0900

    refactor(ai) : 플라잉 무브먼트 컴포넌트화로 인한  구조 리팩토링

--- Source/NeoSanctum/AI/Base/NSBaseDroneAI.cpp
+++ Source/NeoSanctum/AI/Base/NSBaseDroneAI.cpp
@@ -8,6 +8,7 @@
 #include "Engine/AssetManager.h"
 #include "Engine/StreamableManager.h"
 #include "AIController.h"
+#include "NeoSanctum/AI/Components/NSFlyingLocomotionComponent.h"
 #include "NeoSanctum/Character/Player/NSPlayerCharacterBase.h"
 #include "NeoSanctum/Data/AI/NSCompanionAbilitySet.h"
 #include "NeoSanctum/Data/AI/NSBaseDroneDefinition.h"
@@ -28,11 +29,15 @@ ANSBaseDroneAI::ANSBaseDroneAI()
 	SkeletalMeshComponent->SetupAttachment(SphereComponent);
 	SkeletalMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	
+	// 무브먼트 컴포넌트는 여전히 폰이 소유 (로코모션 컴포넌트는 참조만 함)
 	FloatingPawnMovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>("FloatingPawnMovement");
 	FloatingPawnMovementComponent->MaxSpeed = 1000.f;
 	FloatingPawnMovementComponent->Acceleration = 4000.f;
 	FloatingPawnMovementComponent->Deceleration = 4000.f;
 	FloatingPawnMovementComponent->TurningBoost = 8.f;
+	
+	// 비행 로코모션 컴포넌트 생성 (회전/고도/스티어링 로직 통합)
+	LocomotionComponent = CreateDefaultSubobject<UNSFlyingLocomotionComponent>("FlyingLocomotion");
 	
 	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
@@ -47,21 +52,22 @@ UAbilitySystemComponent* ANSBaseDroneAI::GetAbilitySystemComponent() const
 	return AbilitySystemComponent;
 }
 
+void ANSBaseDroneAI::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+	// 로코모션 컴포넌트에 오너/무브먼트 참조 주입 (스폰 시 1회)
+	if (IsValid(LocomotionComponent))
+	{
+		LocomotionComponent->InitializeLocomotion(this, FloatingPawnMovementComponent);
+	}
+}
+
 void ANSBaseDroneAI::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
-	
-	if (HasAuthority())
-	{
-		DroneAIRotate(DeltaSeconds);
-
-		GroundSampleAccumulator += DeltaSeconds;
-		if (GroundSampleAccumulator >= GroundSampleInterval)  // 예: 0.1초
-		{
-			MaintainAltitude(GroundSampleAccumulator);
-			GroundSampleAccumulator = 0.f;
-		}
-	}
+	// 움직임 관련 로직은 전부 LocomotionComponent가 자체 Tick에서 처리
+	// 폰 레벨의 추가 로직이 필요하면 여기에 작성
 }
 
 void ANSBaseDroneAI::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -88,24 +94,27 @@ void ANSBaseDroneAI::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	InitSteeringDirections();
-	
 	InitAbilityActorInfo();
 }
 
 void ANSBaseDroneAI::MoveTowards(const FVector& TargetLocation)
 {
-	FVector TargetPosition = TargetLocation - GetActorLocation();
-	TargetPosition.Z = 0.f;
+	// 서버 권한 확인 및 로코모션 컴포넌트 유효성 확인 후 위임
+	if (!HasAuthority() || !IsValid(LocomotionComponent)) return;
 	
-	if (TargetPosition.SizeSquared() < FMath::Square(ArrivalRadius)) return;
+	LocomotionComponent->RequestMoveTowards(TargetLocation);
+}
+
+void ANSBaseDroneAI::SetCurrentEnemy(AActor* InEnemy)
+{
+	// 폰 상태 갱신
+	CurrentEnemy = InEnemy;
 	
-	FVector TargetDirection = TargetPosition.GetSafeNormal();  
-	BuildInterestMap(TargetDirection);  
-	BuildDangerMap();  
-	FVector SteeringDirection = ChooseSteeringDirection();  
-  
-	AddMovementInput(SteeringDirection, 1.0f);
+	// 로코모션 컴포넌트에 회전 타겟 전파 (nullptr 허용 - velocity 회전 복귀)
+	if (IsValid(LocomotionComponent))
+	{
+		LocomotionComponent->SetRotationTarget(InEnemy);
+	}
 }
 
 void ANSBaseDroneAI::SetPendingDefinition(const UNSBaseDroneDefinition* InDefinition)
@@ -119,14 +128,14 @@ void ANSBaseDroneAI::InitAbilityActorInfo()
 {
 	checkf(AbilitySystemComponent, TEXT("Can't Found ASC %s"), *GetName());
 	
-	AbilitySystemComponent->InitAbilityActorInfo(this,this);
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 }
 
 void ANSBaseDroneAI::InitializeDefaultStats()
 {
 	if (!HasAuthority()) return;
 	
-	if (!AbilitySystemComponent || !DefaultStatsEffect)	return;
+	if (!AbilitySystemComponent || !DefaultStatsEffect) return;
 	
 	FGameplayEffectContextHandle ContextHandle =
 		AbilitySystemComponent->MakeEffectContext();
@@ -234,275 +243,7 @@ void ANSBaseDroneAI::OnRep_CurrentDefinition()
 	ApplyDroneVisual(CurrentDefinition);
 }
 
-void ANSBaseDroneAI::MaintainAltitude(float DeltaSeconds)
-{
-	// 고도 가장 높은 값
-	float OutZ;
-	
-	// 지형 추종 샘플을 순회하며 가장 높은 지형 위치를 반환
-	if(SampleHighestGround(OutZ))
-	{
-		float RawTarget = OutZ + Altitude;
-		// 이미 유효한 지형 정보를 가지고 있는지 확인 첫감지
-		if (!bHasValidGround)
-		{
-			SmoothedTargetHeight = RawTarget;
-			bHasValidGround = true;
-		}
-		else
-		{
-			// Clamp를 통해 변화량 제한 급격한 이동 방지
-			float DesiredPoint = RawTarget - SmoothedTargetHeight;
-			SmoothedTargetHeight += FMath::Clamp(DesiredPoint, -MaxDescendSpeed * DeltaSeconds, MaxClimbSpeed * DeltaSeconds);
-		}
-		
-		// 떨림 방지 데드존 적은값 이동을 위해 드론이 흔들리는것을 방지
-		const float DesiredMoveDis = SmoothedTargetHeight - GetActorLocation().Z;
-		if (FMath::Abs(DesiredMoveDis) > AltitudeDeadZone)
-		{
-			// Clamp를 통해 입력강도 값 -1~1 사이 값으로 변환 후 적용
-			const float InputZ = FMath::Clamp(DesiredMoveDis / AltitudeCorrectionRange, -1.f, 1.f);
-			AddMovementInput(FVector::UpVector, InputZ);
-		}
-	}
-	
-	/*
-	const FVector End = Start - FVector(0.f,0.f,GroundTraceDistance);
-	
-	FHitResult Hit;
-	FCollisionQueryParams CollisionParams;
-	CollisionParams.AddIgnoredActor(this);
-	
-	if (GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECollisionChannel::ECC_Visibility, CollisionParams))
-	{
-		const float DesiredHeight = Hit.ImpactPoint.Z + Altitude;
-		const float DesiredMoveDistance = DesiredHeight - Start.Z;
-		
-		if (FMath::Abs(DesiredMoveDistance) > AltitudeDeadZone)
-		{
-			const float InputZ = FMath::Clamp(DesiredMoveDistance / AltitudeCorrectionRange, -1.f, 1.f);
-			AddMovementInput(FVector::UpVector, InputZ);
-		}
-	}
-	*/
-}
-
-bool ANSBaseDroneAI::TraceGroundAt(const FVector& WorldXY, float& OutZ) const
-{
-	// 시작위치 종료위치 설정
-	const FVector StartWorldLocation = WorldXY;
-	const FVector EndWorldLocation = WorldXY - FVector(0.f, 0.f, GroundTraceDistance);
-	
-	// 트레이스 정보 입력
-	FHitResult Hit;
-	FCollisionQueryParams CollisionParams;
-	CollisionParams.AddIgnoredActor(this);
-	
-	if (GetWorld()->LineTraceSingleByChannel(Hit, StartWorldLocation, EndWorldLocation, ECC_Visibility, CollisionParams))
-	{
-		OutZ = Hit.ImpactPoint.Z;
-		return true;
-	}
-	return false;
-}
-
-bool ANSBaseDroneAI::SampleHighestGround(float& OutGroundZ) const
-{
-	if (!FloatingPawnMovementComponent) return false;
-	
-	// 샘플 포인트 여러개 벡터로 배열
-	TArray<FVector> SamplePoints;
-	// 지형 정보를 받아올 멤버변수 지형 샘플 갯수 + 드론현재위치 1 + 이동 방향 예측위치 1 만큼 공간확보
-	SamplePoints.Reset(GroundSampleCount + 2);
-        
-	const FVector DroneLocation = GetActorLocation();
-	SamplePoints.Add(DroneLocation);
-	
-	// 드론의 이동 방향 예측
-	FVector ForwardXY = FloatingPawnMovementComponent->Velocity.GetSafeNormal2D();
-	
-	if (ForwardXY.IsNearlyZero())
-	{
-		ForwardXY = GetActorForwardVector().GetSafeNormal2D();
-	}
-	
-	const FVector LookAheadPoint = DroneLocation + ForwardXY * GroundLookAheadDistance;
-	SamplePoints.Add(LookAheadPoint);
-	
-	// 360도 원 기준으로 지형 샘플 만큼 나누기
-	const float AngleStep = 360.f / GroundSampleCount;
-	
-	// 나온 각 각도에 지형 샘플 포인트 배치
-	for (int32 i = 0; i < GroundSampleCount; ++i)
-	{
-		const float Angle = i * AngleStep;
-        const FVector Dir = FRotator(0.f, Angle, 0.f).Vector();
-		const FVector SamplePoint = DroneLocation + Dir * GroundSampleRadius;
-		SamplePoints.Add(SamplePoint);
-	}
-	
-	// 처음 들어오는 값을 무조건 갱신하기위해 가장 낮은 값 대입
-	float HighestZ = TNumericLimits<float>::Lowest();
-	bool bFound = false;
-	
-	// 존재하는 지형 샘플 마다 순회하며 아래쪽으로 트레이스 발사 최대 값 갱신
-	for (const FVector& Point : SamplePoints)
-	{
-		float HitZ;
-		if (TraceGroundAt(Point, HitZ))
-		{
-			bFound = true;
-			HighestZ = FMath::Max(HighestZ, HitZ);
-		}
-	}
-	
-	// 순회 후 가장 높은 지형을 반환 및 bool 값 반환
-	OutGroundZ = HighestZ;
-	return bFound;
-}
-
 void ANSBaseDroneAI::InitializeFromData()
 {
-	
+	// 서브클래스에서 오버라이드
 }
-
-void ANSBaseDroneAI::DroneAIRotate(float DeltaSeconds)
-{
-	if (!FloatingPawnMovementComponent) return;
-	
-	if (!HasAuthority()) return;
-	
-	const bool bHasEnemy = (GetCurrentEnemy() != nullptr);
-	
-	FRotator Desired;
-	
-	if (bHasEnemy)
-	{
-		FVector ToTarget = CurrentEnemy->GetActorLocation() - GetActorLocation();
-		ToTarget.Z = 0.f;
-		if (ToTarget.SizeSquared() < FMath::Square(CombatYawInterpSpeed)) return;
-		Desired = FRotator(0.f,ToTarget.Rotation().Yaw,0.f);
-	}
-	else
-	{
-		FVector Vel = FloatingPawnMovementComponent->Velocity;
-		Vel.Z = 0.f;
-		if (Vel.SizeSquared() < FMath::Square(MinSpeedToRotate)) return;
-		Desired = FRotator(0.f,Vel.Rotation().Yaw,0.f);
-	}
-	
-	const float InterpSpeed = GetCurrentEnemy() ? CombatYawInterpSpeed : YawInterpSpeed;
-	const FRotator NewRot = FMath::RInterpTo(GetActorRotation(), Desired,DeltaSeconds,InterpSpeed);
-	SetActorRotation(NewRot);
-}
-
-#pragma region 회피기능
-
-void ANSBaseDroneAI::InitSteeringDirections()
-{
-	float AngleStep = 360.0f / NumSteeringDirections;
-	
-	for (int32 i = 0; i < NumSteeringDirections; ++i)
-	{
-		float Angle = i * AngleStep;
-		
-		FRotator Rotation = FRotator(0.f,Angle,0.f);
-		FVector Direction = Rotation.Vector();
-		
-		SteeringDirections.Add(Direction);
-	}
-}
-
-void ANSBaseDroneAI::BuildInterestMap(const FVector& DesiredDirection)
-{
-	InterestMap.Reset(SteeringDirections.Num());
-	
-	for (const FVector& Direction : SteeringDirections)
-	{
-		float Interest = FVector::DotProduct(Direction,DesiredDirection);
-		InterestMap.Add(Interest);
-	}
-	
-}
-
-void ANSBaseDroneAI::BuildDangerMap()
-{
-	DangerMap.Reset(SteeringDirections.Num());
-	
-	for (const FVector& Direction : SteeringDirections)
-	{
-		const FVector Start = GetActorLocation();
-		const FVector End = Start + Direction * AvoidanceTraceDistance;
-		
-		FHitResult Hit;
-		FCollisionQueryParams CollisionParams;
-		CollisionParams.AddIgnoredActor(this);
-		CollisionParams.bTraceComplex = false;
-		
-		float Danger = 0.f;
-	
-		if (GetWorld()->SweepSingleByChannel(
-			Hit,
-			Start,
-			End,
-			FQuat::Identity,
-			ECollisionChannel::ECC_Visibility,
-			FCollisionShape::MakeSphere(AvoidanceTraceRadius),
-			CollisionParams))
-		{
-			if (!IsWalkableSurface(Hit.Normal))
-			{
-				Danger = 1.f - FMath::Clamp(Hit.Distance / AvoidanceTraceDistance, 0.f, 1.f);
-			}
-			else
-			{
-				Danger = 0.f;
-			}
-		}
-		
-		DangerMap.Add(Danger);
-	}
-}
-
-bool ANSBaseDroneAI::IsWalkableSurface(const FVector& SurfaceNormal) const
-{
-	float SurfaceAndUpVectorDot = FVector::DotProduct(SurfaceNormal,FVector::UpVector);
-	float MaxSlopeRadians = FMath::DegreesToRadians(MaxWalkableSlopeAngle);
-	float MaxSlopeCos = FMath::Cos(MaxSlopeRadians);
-	
-	if (SurfaceAndUpVectorDot >= MaxSlopeCos)
-	{
-		return true;
-	}
-	
-	return false;
-}
-
-FVector ANSBaseDroneAI::ChooseSteeringDirection() const
-{
-	float BestInterest = TNumericLimits<float>::Lowest();
-	int32 BestIndex = INDEX_NONE;
-	
-	for (int32 i = 0; i < NumSteeringDirections; ++i)
-	{
-		if (DangerMap[i] > DangerThreshold)
-		{
-			continue;
-		}
-		
-		if (InterestMap[i] > BestInterest)
-		{
-			BestInterest = InterestMap[i];
-			BestIndex = i;
-		}
-	}
-	
-	if (BestIndex == INDEX_NONE)
-	{
-		return FVector::ZeroVector;
-	}
-	
-	return SteeringDirections[BestIndex];
-}
-
-#pragma endregion
--- Source/NeoSanctum/AI/Base/NSBaseDroneAI.h
+++ Source/NeoSanctum/AI/Base/NSBaseDroneAI.h
@@ -16,6 +16,7 @@ class UGameplayEffect;
 class USphereComponent;
 class USkeletalMeshComponent;
 class UFloatingPawnMovement;
+class UNSFlyingLocomotionComponent;
 class AAIController;
 
 UCLASS()
@@ -29,214 +30,119 @@ public:
 	ANSBaseDroneAI();
 
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
-	
+
 #pragma region DroneTeamId
-	
 public:
 	virtual FGenericTeamId GetGenericTeamId() const override
 	{
 		return FGenericTeamId(static_cast<uint8>(TeamId));
 	}
-	
+
 protected:
 	ETeamId TeamId = ETeamId::Player;
-	
+
 #pragma endregion
+
 public:
-	USkeletalMeshComponent* GetSkeletalMeshComponent() const {return SkeletalMeshComponent;}
-	
+	USkeletalMeshComponent* GetSkeletalMeshComponent() const { return SkeletalMeshComponent; }
+
 	// @민재 : 멀티 관련
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-	
+
 	virtual void Tick(float DeltaSeconds) override;
 	virtual void BeginPlay() override;
 	virtual void PossessedBy(AController* NewController) override;
-	
-	// @민재 : 드론 움직임 함수 BT연동
+	// @민재 : 컴포넌트 초기화 연결 지점 (Sphere/Mesh/FloatingPawnMovement/Locomotion 모두 유효)
+	virtual void PostInitializeComponents() override;
+
+	// @민재 : 드론 움직임 함수 BT 연동. 실제 로직은 LocomotionComponent에 위임
 	UFUNCTION()
-	void MoveTowards(const FVector& TargetLocation);
-	
+	void MoveTowards(
+		const FVector& TargetLocation);                  // 목표 지점 (XY 평면)
+
 protected:
-	// @민재 : 고도유지 이동관련 함수
-	void MaintainAltitude(float DeltaSeconds);
-	
-	// @민재 : 회전기능 함수
-	void DroneAIRotate(float DeltaSeconds);
-	
-	// @민재 : 회피기능 함수
-	void InitSteeringDirections();
-	
-	void BuildInterestMap(const FVector& DesiredDirection);
-	
-	void BuildDangerMap();
-	
-	bool IsWalkableSurface(const FVector& SurfaceNormal) const;
-	
-	FVector ChooseSteeringDirection() const;
-	
-	// @민재 : 지형추적 기능
-	bool TraceGroundAt(const FVector& WorldXY, float& OutZ) const; 
-	
-	bool SampleHighestGround(float& OutGroundZ) const;
-	
+	// @민재 : 데이터 기반 초기화 훅. 서브클래스에서 오버라이드하여 Definition 로드/적용
 	virtual void InitializeFromData();
+
 protected:
 	// @민재 : 컴포넌트 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<USphereComponent> SphereComponent;
-	
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
-	
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UFloatingPawnMovement> FloatingPawnMovementComponent;
-	
-	// @민재 : 드론 회전관련 변수
-	UPROPERTY(EditAnywhere, Category="DroneAI|Rotation")
-	float YawInterpSpeed = 5.f;
-	
-	UPROPERTY(EditAnywhere, Category="DroneAI|Rotation")
-	float CombatYawInterpSpeed = 10.f;
-	
-	UPROPERTY(EditAnywhere, Category="DroneAI|Rotation")
-	float MinSpeedToRotate = 50.f;
-	
-	// @민재 : 도착거리 판정
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Movement")
-	float ArrivalRadius = 60.f;
-	
-#pragma region Altitude
-	
-	// @민재 : 드론 고도 유지위한 변수
-	UPROPERTY(EditAnywhere, Category="DroneAI|Altitude")
-	float Altitude = 300.f;
-	
-	UPROPERTY(EditAnywhere, Category="DroneAI|Altitude")
-	float GroundTraceDistance = 2000.f;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	float AltitudeDeadZone = 20.f;
- 
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	float AltitudeCorrectionRange = 200.f;
-	
-	// @민재 : 지형추적 기능
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	float GroundSampleRadius = 150.f;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	int32 GroundSampleCount = 4;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	float GroundLookAheadDistance = 200.f;
-	
-	UPROPERTY(VisibleAnywhere, Category = "DroneAI|Altitude")
-	float GroundSampleAccumulator = 0.f;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	float GroundSampleInterval = 0.1f;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	float MaxClimbSpeed = 600.f;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Altitude")
-	float MaxDescendSpeed = 150.f;
-	
-	float SmoothedTargetHeight = 0.f;
-	bool bHasValidGround = false;
-	
-#pragma endregion 
-	
-#pragma region CustomAvoid
-	// @민재 : 커스텀 회피 기능 관련
-	UPROPERTY(EditAnywhere, Category="DroneAI|Avoidance")
-	float AvoidanceTraceDistance = 400.f;
-	
-	UPROPERTY(EditAnywhere, Category="DroneAI|Avoidance")
-	float AvoidanceTraceRadius = 100.f;
-	
-	UPROPERTY(EditAnywhere, Category="DroneAI|Avoidance")
-	int32 NumSteeringDirections = 16;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Avoidance")
-	float DangerThreshold = 0.1f;
-	
-	UPROPERTY(EditAnywhere, Category = "DroneAI|Avoidance")
-	float MaxWalkableSlopeAngle = 50.f;
- 
-	TArray<FVector> SteeringDirections;
-	
-	TArray<float> InterestMap;
-	
-	TArray<float> DangerMap;
-	
-#pragma endregion 
-	
+
+	// @민재 : 비행 로코모션 (회전/고도/스티어링/회피 통합)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Locomotion")
+	TObjectPtr<UNSFlyingLocomotionComponent> LocomotionComponent;
+
 #pragma region CachedData
 	// @민재 : 캐싱 데이터
-
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "AI|CachedData")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="AI|CachedData")
 	TObjectPtr<AAIController> CachedAIController;
-	
+
 public:
-	void SetCurrentEnemy(AActor* InEnemy) {CurrentEnemy = InEnemy;}
-	AActor* GetCurrentEnemy() const {return CurrentEnemy.Get();}
+	// @민재 : 현재 적 지정. 로코모션 컴포넌트의 회전 타겟도 함께 동기화
+	void SetCurrentEnemy(
+		AActor* InEnemy);                                // 새 타겟 (nullptr이면 해제)
+
+	AActor* GetCurrentEnemy() const { return CurrentEnemy.Get(); }
+
 protected:
 	UPROPERTY(VisibleAnywhere, Category="GAS|WeakPtr")
 	TWeakObjectPtr<AActor> CurrentEnemy;
-	
-#pragma endregion
-	
-#pragma region CompanionGAS
-	
-public:
-	FORCEINLINE UAbilitySystemComponent* GetDroneAbilitySystemComponent() const {return AbilitySystemComponent;}
 
-	
+#pragma endregion
+
+#pragma region CompanionGAS
+public:
+	FORCEINLINE UAbilitySystemComponent* GetDroneAbilitySystemComponent() const { return AbilitySystemComponent; }
+
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GAS")
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
 	UPROPERTY(EditDefaultsOnly, Category="GAS|Init")
 	TSubclassOf<UGameplayEffect> DefaultStatsEffect;
-	
+
 	UPROPERTY(EditDefaultsOnly, Category="GAS|Init")
 	TArray<TSubclassOf<UGameplayAbility>> DefaultAbilities;
-	
+
 protected:
 	void InitAbilityActorInfo();
 	void InitializeDefaultStats();
 	void GiveDefaultAbilities();
-	
+
 	bool bDefaultAbilitiesGranted = false;
-	
+
 	// @ 민재 : 어빌리티에서 Pawn이 지정하고있는 Enemy접근
-	
+
 #pragma endregion
 
 #pragma region DataDriven
-	
 public:
 	void SetPendingDefinition(const UNSBaseDroneDefinition* InDefinition);
-	
+
 	void ApplyDroneDefinition(const UNSBaseDroneDefinition* NewDefinition);
-	
+
 	void ApplyDroneVisual(const UNSBaseDroneDefinition* NewDefinition);
-	
+
 	UFUNCTION()
 	void OnRep_CurrentDefinition();
-	
+
 protected:
 	// @민재 : 스폰엑터 디퍼드에서 PS로부터 정보를 전달 받으면 될것같음
 	// 현재 드론이 보유중인 어빌리티 정보
-	UPROPERTY() 
+	UPROPERTY()
 	FNSCompanionAbilitySet_GrantedHandles CurrentAbilityHandles;
-	
+
 	// 현재 드론의 타입 정보
 	UPROPERTY(ReplicatedUsing = OnRep_CurrentDefinition)
 	TObjectPtr<const UNSBaseDroneDefinition> CurrentDefinition;
-	
+
 #pragma endregion
-	
 };
