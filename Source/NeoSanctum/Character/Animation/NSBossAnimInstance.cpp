e727dc66ee8b1ef44d9888af45de05c8b108b2ce
ChoiJunHyeok <optimal9744@gmail.com>
2026-07-02 11:51:02 +0900

    feat(character): 보스 몬스터의 공통 AnimInstance 추가
    - 공통이 되는 사망, 그로기 상태만 다룸

---
+++ Source/NeoSanctum/Character/Animation/NSBossAnimInstance.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSBossAnimInstance.h"
+
+#include "GameFramework/Pawn.h"
+#include "NeoSanctum/Combat/Component/NSEnemyStateComponent.h"
+
+void UNSBossAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	CacheOwner();
+	UpdateState();
+}
+
+void UNSBossAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!IsValid(OwnerPawn) || !IsValid(StateComponent) || !EnemyAgent.GetObject())
+	{
+		CacheOwner();
+	}
+
+	UpdateState();
+}
+
+void UNSBossAnimInstance::CacheOwner()
+{
+	OwnerPawn = TryGetPawnOwner();
+
+	EnemyAgent.SetObject(nullptr);
+	EnemyAgent.SetInterface(nullptr);
+	StateComponent = nullptr;
+
+	if (!IsValid(OwnerPawn))
+	{
+		return;
+	}
+
+	if (INSEnemyAgent* NewEnemyAgent = Cast<INSEnemyAgent>(OwnerPawn))
+	{
+		EnemyAgent.SetObject(OwnerPawn);
+		EnemyAgent.SetInterface(NewEnemyAgent);
+	}
+
+	StateComponent = OwnerPawn->FindComponentByClass<UNSEnemyStateComponent>();
+}
+
+void UNSBossAnimInstance::UpdateState()
+{
+	if (!IsValid(StateComponent))
+	{
+		bIsDead = false;
+		bIsHitReacting = false;
+		return;
+	}
+
+	bIsDead = StateComponent->IsDead();
+	bIsHitReacting = StateComponent->IsHitReacting();
+}
---
+++ Source/NeoSanctum/Character/Animation/NSBossAnimInstance.h
@@ -0,0 +1,53 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NeoSanctum/AI/Enemy/Interface/NSEnemyAgent.h"
+#include "NSBossAnimInstance.generated.h"
+
+class APawn;
+class UNSEnemyStateComponent;
+
+/**
+ * Boss Pawn 계열 AnimInstance의 공통 부모 클래스입니다.
+ * CharacterMovement나 특정 보스 이동 방식에 의존하지 않고, 공통 상태만 관리합니다.
+ */
+UCLASS()
+class NEOSANCTUM_API UNSBossAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	// AnimInstance를 소유한 Boss Pawn을 캐싱하는 함수
+	void CacheOwner();
+
+	// StateComponent에서 애니메이션 상태 플래그를 갱신하는 함수
+	void UpdateState();
+
+protected:
+	// 이 AnimInstance를 소유한 Pawn
+	UPROPERTY(BlueprintReadOnly, Category = "Boss|Owner")
+	TObjectPtr<APawn> OwnerPawn;
+
+	// Character/Pawn을 공통 Enemy로 다루기 위한 인터페이스 참조
+	UPROPERTY(BlueprintReadOnly, Category = "Boss|Owner")
+	TScriptInterface<INSEnemyAgent> EnemyAgent;
+
+	// 사망, 피격 경직 같은 공통 Enemy 상태를 읽기 위한 컴포넌트
+	UPROPERTY(BlueprintReadOnly, Category = "Boss|State")
+	TObjectPtr<UNSEnemyStateComponent> StateComponent;
+
+	// Boss가 사망 상태인지 애니메이션 그래프에서 사용하기 위한 값
+	UPROPERTY(BlueprintReadOnly, Category = "Boss|State")
+	bool bIsDead = false;
+
+	// Boss가 피격 경직 상태인지 애니메이션 그래프에서 사용하기 위한 값
+	UPROPERTY(BlueprintReadOnly, Category = "Boss|State")
+	bool bIsHitReacting = false;
+};
