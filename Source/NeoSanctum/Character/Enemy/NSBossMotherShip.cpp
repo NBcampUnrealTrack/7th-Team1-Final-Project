96fa0074c01338035830369eba319a4e577ac61a
Daype <mj1527123@gmail.com>
2026-07-03 10:37:35 +0900

    add(ai) : 보스 MotherShip 추가

---
+++ Source/NeoSanctum/Character/Enemy/NSBossMotherShip.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBossMotherShip.h"
+
+
+// Sets default values
+ANSBossMotherShip::ANSBossMotherShip()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSBossMotherShip::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSBossMotherShip::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ANSBossMotherShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
