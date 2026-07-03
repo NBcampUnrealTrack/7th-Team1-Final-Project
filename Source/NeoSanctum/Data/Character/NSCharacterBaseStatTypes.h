11fa36a828a8408ec80c4c3e272c0bf105d7c082
Park WonJong <kidsmon@naver.com>
2026-07-03 12:53:42 +0900

    feat(data): 캐릭터 기본 스탯 DataTable Row 구조체 추가

---
+++ Source/NeoSanctum/Data/Character/NSCharacterBaseStatTypes.h
@@ -0,0 +1,69 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameplayTagContainer.h"
+#include "NSCharacterBaseStatTypes.generated.h"
+
+/**
+ * 캐릭터 하나의 초기 Attribute 값을 정의하는 DataTable Row.
+ * 하나의 Row가 하나의 캐릭터를 담당하며, RowName은 CharacterTag와 동일해야 합니다.
+ */
+USTRUCT(BlueprintType)
+struct FNSCharacterBaseStatRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	// UNSCharacterData::CharacterTag와 동일해야 함
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character", meta = (Categories = "Character"))
+	FGameplayTag CharacterTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MaxHealth = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float BaseDamage = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float Defense = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MoveSpeed = 600.0f;
+
+	// 치명타 확률(%) / 20 = 20%
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float CritChance = 20.0f;
+
+	// 치명타 피해 배율(%) / 150 = 기본 데미지의 150%
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float CritDamage = 150.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MaxShield = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float ShieldRechargeRate = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float ShieldRechargeCooldown = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MaxDashCount = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float DashRegenRate = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MaxAmmo = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MaxSkill1Count = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MaxSkill2Count = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NS|Character|Attribute")
+	float MaxSkill3Count = 0.0f;
+};
