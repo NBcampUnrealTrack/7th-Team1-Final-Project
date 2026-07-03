dadb3e6ab63aa40db1b0a3c9430ecea66f3ffb5d
Daype <mj1527123@gmail.com>
2026-07-03 16:52:05 +0900

    add(ai) : 보스제어 드론 풀 매니저 추가

---
+++ Source/NeoSanctum/AI/Manager/NSDronePoolManager.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "NSDronePoolManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSDronePoolManager : public UObject
+{
+	GENERATED_BODY()
+};
