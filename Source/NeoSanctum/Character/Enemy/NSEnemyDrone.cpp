26c374a8fcf8d4817a4db9130dc1d14eacffaa0c
Daype <mj1527123@gmail.com>
2026-07-03 10:45:16 +0900

    add(ai) : EnemyDrone 클래스 추가

---
+++ Source/NeoSanctum/Character/Enemy/NSEnemyDrone.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSEnemyDrone.h"
+
+
+// Sets default values
+ANSEnemyDrone::ANSEnemyDrone()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSEnemyDrone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSEnemyDrone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ANSEnemyDrone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
