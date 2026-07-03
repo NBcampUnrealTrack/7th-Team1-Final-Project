fe51d6316f455b28b0dc66c96c3a744059bd91f6
Daype <mj1527123@gmail.com>
2026-07-03 10:59:07 +0900

    add(ai) : MotherShip 패턴 어빌리티 잡몹소환 추가

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_BossSpawnDrone.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_EnemyAttackBase.h"
+#include "GA_BossSpawnDrone.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_BossSpawnDrone : public UGA_EnemyAttackBase
+{
+	GENERATED_BODY()
+};
