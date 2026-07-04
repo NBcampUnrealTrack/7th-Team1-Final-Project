9bfd58f482f7c2ad287405ef4477776e79ab850d
ChoiJunHyeok <optimal9744@gmail.com>
2026-07-04 23:20:44 +0900

    refactor(ai): 각 몬스터의 Blackboard 키 일괄 관리
    관리 목적:
    - 각 기능이 어떤 Blackboard 키를 사용하는지 빠르게 확인
    - 하드 코딩으로 인한 오타 실수 방지

---
+++ Source/NeoSanctum/Type/NSBBTypes.cpp
@@ -0,0 +1,48 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSBBTypes.h"
+
+namespace NSBB
+{
+	namespace Target
+	{
+		const FName TargetActor = TEXT("TargetActor");
+		const FName AttackActor = TEXT("AttackActor");
+		const FName TargetLastKnownLocation = TEXT("TargetLastKnownLocation");
+		const FName HasTargetLineOfSight = TEXT("bHasTargetLineOfSight");
+	}
+
+	namespace Combat
+	{
+		const FName CanAttack = TEXT("bCanAttack");
+		const FName IsAttacking = TEXT("bIsAttacking");
+		const FName IsHitReacting = TEXT("bIsHitReacting");
+	}
+
+	namespace Phase
+	{
+		const FName PhasePatternLocked = TEXT("bPhasePatternLocked");
+		const FName CurrentPhaseId = TEXT("CurrentPhaseId");
+	}
+
+	namespace Movement
+	{
+		const FName ShouldRetreat = TEXT("bShouldRetreat");
+		const FName RetreatLocation = TEXT("RetreatLocation");
+		const FName PatrolLocation = TEXT("PatrolLocation");
+	}
+
+	namespace Melee
+	{
+		const FName HasAttackReservation = TEXT("bHasMeleeAttackReservation");
+		const FName CanApproachTarget = TEXT("bCanApproachMeleeTarget");
+		const FName EQSQuery = TEXT("MeleeEQSQuery");
+		const FName ApproachLocation = TEXT("MeleeApproachLocation");
+		const FName EQSNeedsRefresh = TEXT("bMeleeEQSNeedsRefresh");
+	}
+
+	namespace Boss
+	{
+		const FName CurrentModeTag = TEXT("CurrentModeTag");
+	}
+}
