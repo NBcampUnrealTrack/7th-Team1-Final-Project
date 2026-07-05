a42c9229073e98a6261af6fc938a2294f78198dd
YongHo <skymo6872@gmail.com>
2026-07-05 20:32:41 +0900

    feat(system): 스테이지 페이즈 분류 추가

--- Source/NeoSanctum/Core/GameFlow/NSRunFlowType.h
+++ Source/NeoSanctum/Core/GameFlow/NSRunFlowType.h
@@ -9,6 +9,7 @@
  * GameFlow에서 사용하기 위한 Enum
  * 런 종료시의 상태나 투표에 사용
  */
+ 
 UENUM(BlueprintType)
 enum class ENSRunChoice : uint8
 {
@@ -22,4 +23,15 @@ enum class ENSRunEndPhase : uint8
 	None,
 	Voting,
 	Result
+};
+
+UENUM(BlueprintType)
+enum class ENSStagePhase : uint8
+{
+	// 스테이지 목표 수행 중
+	Objective	UMETA(DisplayName = "Objective"),
+    // 목표 달성, 보스 진입 볼륨 활성
+    BossReady   UMETA(DisplayName = "Boss Ready"),
+    // 전원 텔레포트 완료, 보스 전투
+    BossFight   UMETA(DisplayName = "Boss Fight")
 };
