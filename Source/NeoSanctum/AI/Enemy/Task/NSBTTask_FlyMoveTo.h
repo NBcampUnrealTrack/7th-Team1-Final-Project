b01be07e72f72a08c0658d44e1e2fdaae9e1352e
Daype <mj1527123@gmail.com>
2026-07-05 20:25:37 +0900

    add(ai) : 보스드론 및 잡몹드론 전용 공중이동 테스크 추가

---
+++ Source/NeoSanctum/AI/Enemy/Task/NSBTTask_FlyMoveTo.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "NSBTTask_FlyMoveTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSBTTask_FlyMoveTo : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+};
