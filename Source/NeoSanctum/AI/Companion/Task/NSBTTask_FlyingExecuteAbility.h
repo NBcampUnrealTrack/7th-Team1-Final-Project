89358f5d6216fb2227cc6dc40772b06dff00f68e
Daype <mj1527123@gmail.com>
2026-07-05 23:33:31 +0900

    add(ai) : 적 비행 공격태스크 추가

---
+++ Source/NeoSanctum/AI/Companion/Task/NSBTTask_FlyingExecuteAbility.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "NSBTTask_FlyingExecuteAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UNSBTTask_FlyingExecuteAbility : public UBTTaskNode
+{
+	GENERATED_BODY()
+};
