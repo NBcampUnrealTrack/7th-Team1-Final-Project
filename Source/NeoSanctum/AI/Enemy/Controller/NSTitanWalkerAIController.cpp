fa5da8a933c17e89729e671a095d1370ac345c78
ChoiJunHyeok <optimal9744@gmail.com>
2026-07-05 22:24:54 +0900

    feat(ai): Stage 1 보스 AI Controller 추가

---
+++ Source/NeoSanctum/AI/Enemy/Controller/NSTitanWalkerAIController.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#include "NSTitanWalkerAIController.h"
+
+#include "GameFramework/Pawn.h"
+#include "NeoSanctum/Combat/Component/NSBossAbilityExecutorComponent.h"
+
+ANSTitanWalkerAIController::ANSTitanWalkerAIController()
+{
+}
+
+bool ANSTitanWalkerAIController::IsBossAttackInProgress() const
+{
+	if (Super::IsBossAttackInProgress())
+	{
+		return true;
+	}
+
+	const UNSBossAbilityExecutorComponent* ExecutorComponent = GetBossAbilityExecutorComponent();
+
+	return ExecutorComponent && ExecutorComponent->IsExecuting();
+}
+
+UNSBossAbilityExecutorComponent* ANSTitanWalkerAIController::GetBossAbilityExecutorComponent() const
+{
+	const APawn* ControlledPawn = GetPawn();
+
+	return ControlledPawn
+		       ? ControlledPawn->FindComponentByClass<UNSBossAbilityExecutorComponent>()
+		       : nullptr;
+}
