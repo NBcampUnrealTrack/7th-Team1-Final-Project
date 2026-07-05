0a1be979160d351d561ec14e1ecb43c163e15b9e
Daype <mj1527123@gmail.com>
2026-07-06 08:56:27 +0900

    refactor(ai) : 제어장치 tick 리팩토링

--- Source/NeoSanctum/Interaction/Prop/NSBossControlDevice.cpp
+++ Source/NeoSanctum/Interaction/Prop/NSBossControlDevice.cpp
@@ -7,8 +7,7 @@
 // Sets default values
 ANSBossControlDevice::ANSBossControlDevice()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 void ANSBossControlDevice::OnServerDestroyed(const FVector& Origin)
