26c0da446f5bcb2a55d62f37d6fd32bd25886fd2
Daype <mj1527123@gmail.com>
2026-07-05 13:43:36 +0900

    add(ai) : 파괴가능한 오브젝트 베이스 상속 보스 무적제어장치 추가

---
+++ Source/NeoSanctum/Interaction/Prop/NSBossaControlDevice.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBossaControlDevice.h"
+
+
+// Sets default values
+ANSBossaControlDevice::ANSBossaControlDevice()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSBossaControlDevice::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSBossaControlDevice::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/NeoSanctum/Interaction/Prop/NSBossaControlDevice.h
@@ -0,0 +1,25 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NSDestructibleObjectBase.h"
+#include "NSBossaControlDevice.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSBossaControlDevice : public ANSDestructibleObjectBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ANSBossaControlDevice();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
