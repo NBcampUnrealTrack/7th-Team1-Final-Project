39512b8021a5c7b7c9e5a27616e4f6d2397824a9
Daype <mj1527123@gmail.com>
2026-07-05 18:33:44 +0900

    add(gas) : 마더쉽 보스 2페이즈 연출 어빌리티 추가

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_MotherShipPhase2Transition.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_MotherShipPhase2Transition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_MotherShipPhase2Transition : public UGameplayAbility
+{
+	GENERATED_BODY()
+};
