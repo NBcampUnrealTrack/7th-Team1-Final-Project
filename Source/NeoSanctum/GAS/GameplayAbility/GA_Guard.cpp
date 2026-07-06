0b7f0545852e55d23b346f38cfdd5f77772e7907
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-07-07 07:06:27 +0900

    add(gas): GA_Guard Ability추가
    몽타주에서부터 Event를 받아서 해당하는 시점에 Barrier Actor를 특정 소켓에 스폰하는 Ability.
    Engineer의 Barrier 스폰 로직과 동일한 면이 있어서 BarrierBase를 상속받았음.

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_Guard.cpp
@@ -0,0 +1,328 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#include "GA_Guard.h"
+
+#include "AbilitySystemComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Animation/AnimInstance.h"
+#include "NeoSanctum/Combat/Weapon/Summon/NSBarrierBase.h"
+#include "NeoSanctum/Tag/NSGameplayTags_Ability.h"
+#include "NeoSanctum/Tag/NSGameplayTags_State.h"
+
+UGA_Guard::UGA_Guard()
+{
+	FGameplayTagContainer AssetTags = GetAssetTags();
+	AssetTags.AddTag(NSGameplayTags::Ability_Vanguard_Guard);
+	SetAssetTags(AssetTags);
+	SkillAbilityTag = NSGameplayTags::Ability_Vanguard_Guard;
+
+	ActivationPolicy = ENSAbilityActivationPolicy::OnInputTriggered;
+	ActivationBlockedTags.AddTag(NSGameplayTags::State_Vanguard_Guarding);
+	ActivationBlockedTags.AddTag(NSGameplayTags::State_Vanguard_Attacking);
+	ActivationBlockedTags.AddTag(NSGameplayTags::State_Dashing);
+}
+
+void UGA_Guard::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!ActorInfo || !ActorInfo->AvatarActor.IsValid())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	if (!ActorInfo->AbilitySystemComponent.IsValid())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	if (!HasValidBarrierConfig())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	// Barrier 생성에 필요한 스탯은 활성화 시점에 고정
+	if (!TryResolveBarrierStats(CachedBarrierRadius, CachedBarrierDuration))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	// Guard 유지 상태 적용 후 몽타주 이벤트를 기다림
+	AddGuardStateTags();
+	ApplyGuardMoveSpeedEffect();
+	StartGuardEventTask();
+
+	if (!PlayGuardMontage())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+	}
+}
+
+void UGA_Guard::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
+{
+	// Ability 종료 시 활성 Task와 지속 효과를 정리
+	if (GuardMontageTask)
+	{
+		GuardMontageTask->EndTask();
+		GuardMontageTask = nullptr;
+	}
+
+	if (GuardEventTask)
+	{
+		GuardEventTask->EndTask();
+		GuardEventTask = nullptr;
+	}
+
+	RemoveGuardMoveSpeedEffect();
+	RemoveGuardStateTags();
+
+	if (bDestroyBarrierOnEnd)
+	{
+		DestroyActiveGuardBarrier();
+	}
+	else
+	{
+		ActiveGuardBarrier = nullptr;
+	}
+
+	CachedBarrierRadius = 0.0f;
+	CachedBarrierDuration = 0.0f;
+	bGuardEndingFromInputRelease = false;
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UGA_Guard::InputReleased(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
+
+	// 입력 해제는 한 번만 처리
+	if (bGuardEndingFromInputRelease)
+	{
+		return;
+	}
+
+	// 종료 섹션이 있으면 몽타주 종료 연출을 먼저 재생
+	bGuardEndingFromInputRelease = true;
+	if (TryJumpToGuardEndSection())
+	{
+		return;
+	}
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
+
+void UGA_Guard::OnGuardEventReceived(FGameplayEventData Payload)
+{
+	// Notify 시점에 Barrier 생성
+	SpawnGuardBarrier();
+}
+
+void UGA_Guard::OnGuardMontageCompleted()
+{
+	// End 섹션까지 끝나면 Ability 종료
+	EndAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true, false);
+}
+
+void UGA_Guard::OnGuardMontageInterrupted()
+{
+	// 중단된 경우 취소 종료로 처리
+	EndAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true, true);
+}
+
+void UGA_Guard::StartGuardEventTask()
+{
+	GuardEventTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this,
+		NSGameplayTags::Event_Vanguard_Guard,
+		nullptr,
+		false,
+		true
+	);
+
+	if (!GuardEventTask)
+	{
+		return;
+	}
+
+	// 이벤트 바인딩
+	GuardEventTask->EventReceived.AddDynamic(this, &ThisClass::OnGuardEventReceived);
+	GuardEventTask->ReadyForActivation();
+}
+
+bool UGA_Guard::PlayGuardMontage()
+{
+	if (!GuardMontage)
+	{
+		return false;
+	}
+
+	const float MontagePlayRate = FMath::Max(GuardMontagePlayRate, 0.01f);
+	GuardMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+		this,
+		NAME_None,
+		GuardMontage,
+		MontagePlayRate,
+		NAME_None,
+		true
+	);
+
+	if (!GuardMontageTask)
+	{
+		return false;
+	}
+
+	// BlendOut은 End 섹션 전환 중에도 발생할 수 있으므로 완료 처리하지 않음
+	GuardMontageTask->OnCompleted.AddDynamic(this, &ThisClass::OnGuardMontageCompleted);
+	GuardMontageTask->OnInterrupted.AddDynamic(this, &ThisClass::OnGuardMontageInterrupted);
+	GuardMontageTask->OnCancelled.AddDynamic(this, &ThisClass::OnGuardMontageInterrupted);
+	GuardMontageTask->ReadyForActivation();
+
+	return true;
+}
+
+bool UGA_Guard::TryJumpToGuardEndSection() const
+{
+	const FGameplayAbilityActorInfo* ActorInfo = GetCurrentActorInfo();
+	UAnimInstance* AnimInstance = ActorInfo ? ActorInfo->GetAnimInstance() : nullptr;
+	if (!AnimInstance || !GuardMontage || GuardEndSectionName.IsNone())
+	{
+		return false;
+	}
+
+	// 현재 Guard 몽타주가 재생 중일 때만 섹션 전환
+	if (!AnimInstance->Montage_IsPlaying(GuardMontage))
+	{
+		return false;
+	}
+
+	AnimInstance->Montage_JumpToSection(GuardEndSectionName, GuardMontage);
+	return true;
+}
+
+bool UGA_Guard::TryResolveBarrierStats(float& OutBarrierRadius, float& OutBarrierDuration) const
+{
+	return TryGetBarrierRadius(OutBarrierRadius) && TryGetBarrierDuration(OutBarrierDuration);
+}
+
+void UGA_Guard::SpawnGuardBarrier()
+{
+	// Guard 중 Barrier는 하나만 유지
+	if (IsValid(ActiveGuardBarrier))
+	{
+		return;
+	}
+
+	// Barrier Health 등 SetByCaller 값은 스폰 직전에 만들어둠
+	RebuildSetByCallerMagnitudes();
+	ActiveGuardBarrier = SpawnBarrierActor(
+		GetCurrentActorInfo(),
+		CachedBarrierRadius,
+		CachedBarrierDuration,
+		GetSetByCallerMagnitudes()
+	);
+
+	if (!IsValid(ActiveGuardBarrier))
+	{
+		EndAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true, true);
+	}
+}
+
+void UGA_Guard::AddGuardStateTags()
+{
+	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
+	{
+		// 중복 추가를 피하기 위해 태그 카운트를 1로 고정
+		ASC->SetLooseGameplayTagCount(NSGameplayTags::State_Vanguard_Guarding, 1);
+		ASC->SetLooseGameplayTagCount(NSGameplayTags::State_Vanguard_BarrierAttackWindow, 1);
+		bGuardStateTagsAdded = true;
+	}
+}
+
+void UGA_Guard::RemoveGuardStateTags()
+{
+	if (!bGuardStateTagsAdded)
+	{
+		return;
+	}
+
+	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
+	{
+		// RemoveLooseGameplayTag Error를 해결하기 위해 카운트를 명시적으로 0으로 설정
+		ASC->SetLooseGameplayTagCount(NSGameplayTags::State_Vanguard_Guarding, 0);
+		ASC->SetLooseGameplayTagCount(NSGameplayTags::State_Vanguard_BarrierAttackWindow, 0);
+	}
+
+	bGuardStateTagsAdded = false;
+}
+
+void UGA_Guard::ApplyGuardMoveSpeedEffect()
+{
+	if (!GuardMoveSpeedEffectClass || GuardMoveSpeedEffectHandle.IsValid())
+	{
+		return;
+	}
+
+	// Guard 중 이동속도 감소 효과는 Ability 종료 시 직접 제거함
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	if (!ASC)
+	{
+		return;
+	}
+
+	FGameplayEffectSpecHandle SpecHandle =
+		MakeOutgoingGameplayEffectSpec(GuardMoveSpeedEffectClass, GetAbilityLevel());
+
+	if (!SpecHandle.IsValid() || !SpecHandle.Data.IsValid())
+	{
+		return;
+	}
+
+	GuardMoveSpeedEffectHandle = ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+}
+
+void UGA_Guard::RemoveGuardMoveSpeedEffect()
+{
+	if (!GuardMoveSpeedEffectHandle.IsValid())
+	{
+		return;
+	}
+
+	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
+	{
+		ASC->RemoveActiveGameplayEffect(GuardMoveSpeedEffectHandle);
+	}
+
+	GuardMoveSpeedEffectHandle.Invalidate();
+}
+
+void UGA_Guard::DestroyActiveGuardBarrier()
+{
+	if (IsValid(ActiveGuardBarrier) && ActiveGuardBarrier->HasAuthority())
+	{
+		ActiveGuardBarrier->Destroy();
+	}
+
+	ActiveGuardBarrier = nullptr;
+}
---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_Guard.h
@@ -0,0 +1,135 @@
+// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_BarrierBase.h"
+#include "GA_Guard.generated.h"
+
+class UAbilityTask_PlayMontageAndWait;
+class UAbilityTask_WaitGameplayEvent;
+class UAnimMontage;
+class UGameplayEffect;
+
+UCLASS()
+class NEOSANCTUM_API UGA_Guard : public UGA_BarrierBase
+{
+	GENERATED_BODY()
+
+public:
+	UGA_Guard();
+
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+
+	virtual void InputReleased(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo
+	) override;
+
+private:
+	// 몽타주 Notify로 전달된 Guard 이벤트 수신
+	UFUNCTION()
+	void OnGuardEventReceived(FGameplayEventData Payload);
+
+	// Guard 몽타주 정상 종료 처리
+	UFUNCTION()
+	void OnGuardMontageCompleted();
+
+	// Guard 몽타주 중단 처리
+	UFUNCTION()
+	void OnGuardMontageInterrupted();
+
+	// Barrier 스폰 이벤트 대기
+	void StartGuardEventTask();
+
+	// Guard 몽타주 재생
+	bool PlayGuardMontage();
+
+	// 입력 해제 시 종료 섹션으로 전환
+	bool TryJumpToGuardEndSection() const;
+
+	// Barrier 스탯 조회
+	bool TryResolveBarrierStats(float& OutBarrierRadius, float& OutBarrierDuration) const;
+
+	// Guard Barrier 스폰
+	void SpawnGuardBarrier();
+
+	// Guard 상태태그 부여
+	void AddGuardStateTags();
+
+	// Guard 상태태그 제거
+	void RemoveGuardStateTags();
+
+	// 이동속도 감소 GE 적용
+	void ApplyGuardMoveSpeedEffect();
+
+	// 이동속도 감소 GE 제거
+	void RemoveGuardMoveSpeedEffect();
+
+	// 활성 Guard Barrier 파괴
+	void DestroyActiveGuardBarrier();
+
+protected:
+	// Guard 몽타주
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Vanguard|Guard|Animation")
+	TObjectPtr<UAnimMontage> GuardMontage;
+
+	// Guard 몽타주 재생 속도
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Vanguard|Guard|Animation", meta = (ClampMin = "0.01"))
+	float GuardMontagePlayRate = 1.0f;
+
+	// 입력 해제 시 이동할 종료 섹션
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Vanguard|Guard|Animation")
+	FName GuardEndSectionName = TEXT("End");
+
+	// Guard 중 적용할 이동속도 감소 GE
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Vanguard|Guard")
+	TSubclassOf<UGameplayEffect> GuardMoveSpeedEffectClass;
+
+	// 종료 시 Barrier를 함께 제거할지 여부
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Vanguard|Guard")
+	bool bDestroyBarrierOnEnd = true;
+
+private:
+	// Guard 몽타주 Task
+	UPROPERTY(Transient)
+	TObjectPtr<UAbilityTask_PlayMontageAndWait> GuardMontageTask;
+
+	// Guard 이벤트 대기 Task
+	UPROPERTY(Transient)
+	TObjectPtr<UAbilityTask_WaitGameplayEvent> GuardEventTask;
+
+	// 현재 Guard Barrier
+	UPROPERTY(Transient)
+	TObjectPtr<ANSBarrierBase> ActiveGuardBarrier;
+
+	// 이동속도 감소 GE 핸들
+	FActiveGameplayEffectHandle GuardMoveSpeedEffectHandle;
+
+	// 활성화 시점의 Barrier 반지름
+	float CachedBarrierRadius = 0.0f;
+
+	// 활성화 시점의 Barrier 지속시간
+	float CachedBarrierDuration = 0.0f;
+
+	// Guard 상태태그 추가 여부
+	bool bGuardStateTagsAdded = false;
+
+	// 입력 해제 종료 처리 중인지 여부
+	bool bGuardEndingFromInputRelease = false;
+};
