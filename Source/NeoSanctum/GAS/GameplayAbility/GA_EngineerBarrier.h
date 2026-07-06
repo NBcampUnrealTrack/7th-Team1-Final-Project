ac24a16820e143d96e5719f2faf6c714ed29d582
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-07-06 19:51:05 +0900

    refactor(gas): EngineerBarrier 스킬에서 GA_BarrierBase로 공통로직을 부분을 분리
    GA_BarrierBase로 Barrier를 소환하는 공통로직을 분리해서 추후에 Vanguard의 Guard 스킬과 공통베이스를 상속받아 사용할 수 있게 함

--- Source/NeoSanctum/GAS/GameplayAbility/GA_EngineerBarrier.h
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_EngineerBarrier.h
@@ -3,33 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GA_SkillBase.h"
-#include "NeoSanctum/Data/Combat/NSCombatStatTypes.h"
+#include "GA_BarrierBase.h"
 #include "GA_EngineerBarrier.generated.h"
-
-class ANSBarrier;
-class UGameplayEffect;
-
-USTRUCT(BlueprintType)
-struct FNSBarrierAbilityConfig
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Barrier")
-	TSubclassOf<ANSBarrier> BarrierClass;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Barrier|Attribute")
-	TSubclassOf<UGameplayEffect> InitialAttributeEffectClass;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Barrier|SetByCaller")
-	TArray<FNSSetByCallerFromCombatStat> SetByCallerMappings;
-};
 
 /**
  * 
  */
 UCLASS()
-class NEOSANCTUM_API UGA_EngineerBarrier : public UGA_SkillBase
+class NEOSANCTUM_API UGA_EngineerBarrier : public UGA_BarrierBase
 {
 	GENERATED_BODY()
 	
@@ -43,43 +24,4 @@ protected:
 		const FGameplayAbilityActivationInfo ActivationInfo,
 		const FGameplayEventData* TriggerEventData
 	) override;
-
-	virtual void EndAbility(
-		const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo,
-		const FGameplayAbilityActivationInfo ActivationInfo,
-		bool bReplicateEndAbility,
-		bool bWasCancelled
-	) override;
-	
-private:
-	bool TryGetBarrierRadius(float& OutBarrierRadius) const;
-	bool TryGetBarrierDuration(float& OutBarrierDuration) const;
-	void RebuildSetByCallerMagnitudes();
-	void SpawnBarrierActor(
-		const FGameplayAbilityActorInfo* ActorInfo,
-		float BarrierRadius,
-		float BarrierDuration,
-		const TArray<FNSSetByCallerMagnitude>& InSetByCallerMagnitudes
-	);
-	
-protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Engineer|Barrier")
-	FNSBarrierAbilityConfig BarrierAbilityConfig;
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Engineer|Barrier")
-	float MinimumBarrierRadius = 150.0f;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Engineer|Barrier")
-	FName AttachSocketName = NAME_None;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Engineer|Barrier")
-	FTransform AttachRelativeTransform = FTransform::Identity;
-
-private:
-	UPROPERTY(Transient)
-	TArray<FNSSetByCallerMagnitude> SetByCallerMagnitudes;
-
-	UPROPERTY(Transient)
-	TObjectPtr<ANSBarrier> ActiveBarrier;
 };
