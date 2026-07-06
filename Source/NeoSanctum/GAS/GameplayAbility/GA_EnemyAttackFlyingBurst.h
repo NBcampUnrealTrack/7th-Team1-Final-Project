a61dfb673c6601115cf2df485482b1541211f63c
Daype <mj1527123@gmail.com>
2026-07-06 12:52:06 +0900

    add(ai) : 적 보스드론 및 일반드론 발사 어빌리티 추가

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_EnemyAttackFlyingBurst.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_EnemyAttackBase.h"
+#include "GA_EnemyAttackFlyingBurst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_EnemyAttackFlyingBurst : public UGA_EnemyAttackBase
+{
+	GENERATED_BODY()
+};
