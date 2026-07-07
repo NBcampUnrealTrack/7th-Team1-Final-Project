2e01c4541fb772c73240c2253537b3495f06dc3d
Daype <mj1527123@gmail.com>
2026-07-07 17:18:13 +0900

    add(ai) : 보스드론 2페이즈 공격패턴 폭격 GA 및 범위탐지 박스 추가

---
+++ Source/NeoSanctum/AI/Enemy/HelperActor/NSBossArenaBounds.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBossArenaBounds.h"
+
+
+// Sets default values
+ANSBossArenaBounds::ANSBossArenaBounds()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSBossArenaBounds::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSBossArenaBounds::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_MotherShipBombingRun.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_EnemyAttackBase.h"
+#include "GA_MotherShipBombingRun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_MotherShipBombingRun : public UGA_EnemyAttackBase
+{
+	GENERATED_BODY()
+};
