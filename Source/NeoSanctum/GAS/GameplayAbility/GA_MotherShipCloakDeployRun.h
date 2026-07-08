938f70668175f46d5311bacda4b24ca6487e309a
Daype <mj1527123@gmail.com>
2026-07-08 20:52:13 +0900

    add(ai) : 클로킹 드랍쉽 패턴 추가

---
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_MotherShipCloakDeployRun.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_EnemyAttackBase.h"
+#include "GA_MotherShipCloakDeployRun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEOSANCTUM_API UGA_MotherShipCloakDeployRun : public UGA_EnemyAttackBase
+{
+	GENERATED_BODY()
+};
