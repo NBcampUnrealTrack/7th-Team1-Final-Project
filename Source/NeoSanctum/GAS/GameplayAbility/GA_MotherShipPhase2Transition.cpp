61a72f226eea0f1bb6e5a671e34e926bf7f76b24
Daype <mj1527123@gmail.com>
2026-07-13 03:39:36 +0900

    refactor(ai) : 2페이즈 진입 연출 태그관리 리팩토링

--- Source/NeoSanctum/GAS/GameplayAbility/GA_MotherShipPhase2Transition.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_MotherShipPhase2Transition.cpp
@@ -21,6 +21,7 @@ void UGA_MotherShipPhase2Transition::ActivateAbility(const FGameplayAbilitySpecH
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 	
+	// 보스 몬스터 캐스팅
 	ANSBossMotherShip* MotherShip = Cast<ANSBossMotherShip>(GetAvatarActorFromActorInfo());
 	if (!MotherShip)
 	{
@@ -28,29 +29,51 @@ void UGA_MotherShipPhase2Transition::ActivateAbility(const FGameplayAbilitySpecH
 		return;
 	}
 	
+	// 2페이즈 연출 시작 
 	MotherShip->BeginPhase2Transition();
 	
-	// 연출 대기: 몽타주 있으면 재생, 없으면 고정 시간 대기 → 끝나면 OnTransitionSequenceFinished
+	// 몽타주가 존재한다면
 	if (TransitionMontage)
 	{
+		// 등록된 몽타주로 Phase2 몽타주태스크 만듦
 		UAbilityTask_PlayMontageAndWait* MontageTask =
 			UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
 				this, TEXT("Phase2TransitionMontage"), TransitionMontage);
-
-		// 완료/중단/취소 어느 쪽이든 마무리(보스가 무적에 갇히지 않게 전부 같은 핸들러로)
+		
+		// 만든 몽타주 태스크에 함수 바인딩 몽타주가 취소/스킵/재생완료 3가지 모든 경우에도 이벤트스킵방지
 		MontageTask->OnCompleted.AddDynamic(this, &ThisClass::OnTransitionSequenceFinished);
 		MontageTask->OnInterrupted.AddDynamic(this, &ThisClass::OnTransitionSequenceFinished);
 		MontageTask->OnCancelled.AddDynamic(this, &ThisClass::OnTransitionSequenceFinished);
+		
+		// 몽타주 준비 완료 호출
 		MontageTask->ReadyForActivation();
 	}
-	else
+	else // 몽타주가 없다면
 	{
+		// 일정시간동안 대기시키는 대기 테스크 제작
 		UAbilityTask_WaitDelay* WaitTask =
 			UAbilityTask_WaitDelay::WaitDelay(this, TransitionDuration);
-
+		
+		// 대기시간이 전부다 끝나면 이벤트 작동하게 바인딩
 		WaitTask->OnFinish.AddDynamic(this, &ThisClass::OnTransitionSequenceFinished);
 		WaitTask->ReadyForActivation();
 	}
+}
+
+// 엔진이 EndAbility를 강제 호출할경우 보스가 무적 및 이동잠금에 갇히는 버그가 발생 할 수도 있음 가능성 매우 낮음
+void UGA_MotherShipPhase2Transition::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	// 보스 몬스터 캐스팅
+	if (ANSBossMotherShip* MotherShip = Cast<ANSBossMotherShip>(GetAvatarActorFromActorInfo()))
+	{
+		// 숨겼던 2페이즈 태그 주입 함수 호출
+		MotherShip->CommitDeferredPhase2Tag();
+	}
+	
+	// 부모 EndAbility호출
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
 void UGA_MotherShipPhase2Transition::OnTransitionSequenceFinished()
