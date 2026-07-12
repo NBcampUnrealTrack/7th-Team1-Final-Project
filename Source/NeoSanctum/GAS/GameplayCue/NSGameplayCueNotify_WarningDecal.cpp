2a7345eb8a7efb2841155a8ea66e29b41ffbadce
Daype <mj1527123@gmail.com>
2026-07-13 03:35:48 +0900

    feat(ai) : 데칼 구현

--- Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_WarningDecal.cpp
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_WarningDecal.cpp
@@ -1,26 +1,71 @@
-﻿// Copyright 2026 One Team. All rights reserved.
+﻿#include "NSGameplayCueNotify_WarningDecal.h"
+
+#include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NeoSanctum/AI/Enemy/HelperActor/NSBossArenaBounds.h"
 
 
-#include "NSGameplayCueNotify_WarningDecal.h"
-
-
-// Sets default values
 ANSGameplayCueNotify_WarningDecal::ANSGameplayCueNotify_WarningDecal()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	bAutoDestroyOnRemove = true;
+	bAllowMultipleOnActiveEvents = false;
+	bAllowMultipleWhileActiveEvents = false;
 }
 
-// Called when the game starts or when spawned
-void ANSGameplayCueNotify_WarningDecal::BeginPlay()
+bool ANSGameplayCueNotify_WarningDecal::OnActive_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters)
 {
-	Super::BeginPlay();
+	Super::OnActive_Implementation(MyTarget, Parameters);
+
+	if (!DecalMaterial)
+	{
+		return true;
+	}
+
+	FRotator FinalRotation = DecalRotation;   // 기본: 바닥 투영(pitch -90 등)
 	
+	if (!Parameters.Normal.IsNearlyZero())
+	{
+		FinalRotation.Yaw = Parameters.Normal.Rotation().Yaw;
+	}
+
+	// 기본값(256)은 폴백. 아레나가 실려오면 존 실측 크기로 덮어씀
+	FVector FinalDecalSize = DecalSize;
+	if (const ANSBossArenaBounds* Arena = Cast<ANSBossArenaBounds>(Parameters.SourceObject.Get()))
+	{
+		const int32 ZoneCount = FMath::RoundToInt(Parameters.NormalizedMagnitude);
+		if (ZoneCount > 0)
+		{
+			// ZoneExtent: X=진행축 half, Y=룸폭 half, Z=높이 half (footprint엔 Z 불필요)
+			const FVector ZoneExtent = Arena->GetZoneBoxExtent(ZoneCount, 0.f);
+
+			// 데칼 로컬축: X=투영 깊이, Y/Z=바닥 footprint 반경.
+			// DecalSize.X(투영 깊이)는 기존값 유지, Y/Z에 존 수평 half-extent 매핑
+			FinalDecalSize = FVector(DecalSize.X, ZoneExtent.Y, ZoneExtent.X);
+		}
+	}
+
+	SpawnedDecal = UGameplayStatics::SpawnDecalAtLocation(
+		this,
+		DecalMaterial,
+		FinalDecalSize,          // ← DecalSize 대신 계산값
+		Parameters.Location,
+		FinalRotation,
+		0.f);
+
+	return true;
 }
 
-// Called every frame
-void ANSGameplayCueNotify_WarningDecal::Tick(float DeltaTime)
+bool ANSGameplayCueNotify_WarningDecal::OnRemove_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters)
 {
-	Super::Tick(DeltaTime);
-}
+	if (IsValid(SpawnedDecal))
+	{
+		SpawnedDecal->DestroyComponent();
+		SpawnedDecal = nullptr;
+	}
 
+	return Super::OnRemove_Implementation(MyTarget, Parameters);
+}
--- Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_WarningDecal.h
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_WarningDecal.h
@@ -1,25 +1,46 @@
-﻿// Copyright 2026 One Team. All rights reserved.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameplayCueNotify_Actor.h"
 #include "NSGameplayCueNotify_WarningDecal.generated.h"
 
-UCLASS()
+class UDecalComponent;
+class UMaterialInterface;
+
+/**
+ * 지속형 경고 데칼 GameplayCue
+ * OnActive(AddGameplayCue) 시 Parameters.Location에 데칼 스폰, OnRemove 시 정리
+ */
+UCLASS(Blueprintable)
 class NEOSANCTUM_API ANSGameplayCueNotify_WarningDecal : public AGameplayCueNotify_Actor
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
 	ANSGameplayCueNotify_WarningDecal();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	virtual bool OnActive_Implementation(
+		AActor* MyTarget,
+		const FGameplayCueParameters& Parameters) override;
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	virtual bool OnRemove_Implementation(
+		AActor* MyTarget,
+		const FGameplayCueParameters& Parameters) override;
+
+protected:
+	// 경고 데칼 머티리얼 (존별로 다르게 하려면 태그별 큐 BP에서 지정)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WarningDecal")
+	TObjectPtr<UMaterialInterface> DecalMaterial;
+
+	// 데칼 크기 (X=투영 깊이, Y/Z=바닥 위 반경)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WarningDecal")
+	FVector DecalSize = FVector(256.f, 256.f, 256.f);
+
+	// 바닥 투영 회전 (기본: 아래로 투영)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WarningDecal")
+	FRotator DecalRotation = FRotator(-90.f, 0.f, 0.f);
+
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<UDecalComponent> SpawnedDecal;
 };
