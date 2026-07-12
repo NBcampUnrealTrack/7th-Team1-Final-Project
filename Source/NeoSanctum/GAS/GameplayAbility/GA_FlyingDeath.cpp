1ad84f3790ac0703fcaf200180eeee792b9cb7a2
Daype <mj1527123@gmail.com>
2026-07-13 03:40:52 +0900

    feat(ai) : 드론 사망연출 구현

--- Source/NeoSanctum/GAS/GameplayAbility/GA_FlyingDeath.cpp
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_FlyingDeath.cpp
@@ -1,4 +1,28 @@
-﻿// Copyright 2026 One Team. All rights reserved.
+﻿#include "GA_FlyingDeath.h"
 
+#include "NeoSanctum/Character/Enemy/NSEnemyPawnBase.h"
+#include "NeoSanctum/AI/Components/NSFlyingLocomotionComponent.h"
 
-#include "GA_FlyingDeath.h"
+void UGA_FlyingDeath::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	// 부모(몽타주/디졸브/콜리전 처리) 전에 고도유지 끄고 낙하 시작
+	if (UNSFlyingLocomotionComponent* Flying = GetAvatarFlyingLocomotion())
+	{
+		Flying->StartDeathFall();
+	}
+
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+UNSFlyingLocomotionComponent* UGA_FlyingDeath::GetAvatarFlyingLocomotion() const
+{
+	if (ANSEnemyPawnBase* EnemyPawn = Cast<ANSEnemyPawnBase>(GetAvatarActorFromActorInfo()))
+	{
+		return EnemyPawn->GetFlyingLocomotion();
+	}
+	return nullptr;
+}
--- Source/NeoSanctum/GAS/GameplayAbility/GA_FlyingDeath.h
+++ Source/NeoSanctum/GAS/GameplayAbility/GA_FlyingDeath.h
@@ -1,16 +1,23 @@
-﻿// Copyright 2026 One Team. All rights reserved.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GA_EnemyDeath.h"
 #include "GA_FlyingDeath.generated.h"
 
-/**
- * 
- */
+class UNSFlyingLocomotionComponent;
+
 UCLASS()
 class NEOSANCTUM_API UGA_FlyingDeath : public UGA_EnemyDeath
 {
 	GENERATED_BODY()
+
+public:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+private:
+	UNSFlyingLocomotionComponent* GetAvatarFlyingLocomotion() const;
 };
