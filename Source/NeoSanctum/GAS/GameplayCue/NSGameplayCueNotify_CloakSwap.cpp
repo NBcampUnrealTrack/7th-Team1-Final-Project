664b263caa37452045355a7be22e3139a4d54250
Daype <mj1527123@gmail.com>
2026-07-13 16:46:37 +0900

    add(ai) : 굴절 클로킹 머터리얼 스왑 큐 추가

---
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_CloakSwap.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSGameplayCueNotify_CloakSwap.h"
+
+
+// Sets default values
+ANSGameplayCueNotify_CloakSwap::ANSGameplayCueNotify_CloakSwap()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSGameplayCueNotify_CloakSwap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSGameplayCueNotify_CloakSwap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
