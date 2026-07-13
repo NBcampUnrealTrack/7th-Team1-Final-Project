a027c8c8eafce27bab4bdeba71a1036cd3520891
Daype <mj1527123@gmail.com>
2026-07-13 16:46:37 +0900

    add(ai) : 굴절 클로킹 머터리얼 스왑 큐 추가

---
+++ Source/NeoSanctum/GAS/GameplayCue/NSGameplayCueNotify_CloakSwap.h
@@ -0,0 +1,25 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Actor.h"
+#include "NSGameplayCueNotify_CloakSwap.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSGameplayCueNotify_CloakSwap : public AGameplayCueNotify_Actor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ANSGameplayCueNotify_CloakSwap();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
