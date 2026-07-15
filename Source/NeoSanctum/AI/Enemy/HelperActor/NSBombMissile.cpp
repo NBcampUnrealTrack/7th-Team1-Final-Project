111d39ee5bf59526f0b9de1d5929657ed2183eb4
Daype <mj1527123@gmail.com>
2026-07-15 17:26:12 +0900

    add(ai) : 마더쉽 BombingRun 투하용 미사일 추가

---
+++ Source/NeoSanctum/AI/Enemy/HelperActor/NSBombMissile.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+
+#include "NSBombMissile.h"
+
+
+// Sets default values
+ANSBombMissile::ANSBombMissile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANSBombMissile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANSBombMissile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/NeoSanctum/AI/Enemy/HelperActor/NSBombMissile.h
@@ -0,0 +1,25 @@
+﻿// Copyright 2026 One Team. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NSBombMissile.generated.h"
+
+UCLASS()
+class NEOSANCTUM_API ANSBombMissile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ANSBombMissile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
