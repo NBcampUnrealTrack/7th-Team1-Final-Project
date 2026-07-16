2f57538461555a2c45c98b02c4095b577dc7dd79
Joung_Juhyeon <wjdwngus2056@gmail.com>
2026-07-16 17:13:00 +0900

    Revert "del(gas): Guard Ability 관련 클래스 및 태그 제거"
    This reverts commit aa322f79e6533869f351309acf7a653fd65aa583.

--- Source/NeoSanctum/Tag/NSGameplayTags_Ability.h
+++ Source/NeoSanctum/Tag/NSGameplayTags_Ability.h
@@ -27,6 +27,7 @@ namespace NSGameplayTags
 	// Vanguard Ability
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Vanguard_BaseAttack);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Vanguard_Flicker);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Vanguard_Guard);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Vanguard_ThrowBarrierField);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Vanguard_VanguardBuff);
 	
@@ -41,6 +42,7 @@ namespace NSGameplayTags
 
 	// Vanguard Ability Event Tags
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Vanguard_Hit);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Vanguard_Guard);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Vanguard_ComboWindowOpened);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Vanguard_DashAttackRecoverStarted);
 	
